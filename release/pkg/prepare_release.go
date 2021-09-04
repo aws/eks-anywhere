5ca4043ed20fd8fcb4dcd42adecdb1711b9f6381
Abhay Krishna <arnchlm@amazon.com>
2021-09-03 20:00:38 -0700

    Prepend git tag for CLI tools image for release (#83)

--- release/pkg/prepare_release.go
+++ release/pkg/prepare_release.go
@@ -81,7 +81,7 @@ func (r *ReleaseConfig) PrepareBundleRelease(sourceClients *SourceClients) (map[
 	}
 	fmt.Println("Artifacts download complete")
 
-	err = renameArtifacts(sourceClients, artifactsTable)
+	err = r.renameArtifacts(sourceClients, artifactsTable)
 	if err != nil {
 		return nil, errors.Cause(err)
 	}
@@ -103,7 +103,7 @@ func (r *ReleaseConfig) PrepareEksARelease(sourceClients *SourceClients) (map[st
 	}
 	fmt.Println("Artifacts download complete")
 
-	err = renameArtifacts(sourceClients, artifactsTable)
+	err = r.renameArtifacts(sourceClients, artifactsTable)
 	if err != nil {
 		return nil, errors.Cause(err)
 	}
@@ -112,7 +112,7 @@ func (r *ReleaseConfig) PrepareEksARelease(sourceClients *SourceClients) (map[st
 	return artifactsTable, nil
 }
 
-func renameArtifacts(sourceClients *SourceClients, artifacts map[string][]Artifact) error {
+func (r *ReleaseConfig) renameArtifacts(sourceClients *SourceClients, artifacts map[string][]Artifact) error {
 	fmt.Println("============================================================")
 	fmt.Println("                 Renaming Artifacts                         ")
 	fmt.Println("============================================================")
@@ -159,7 +159,7 @@ func renameArtifacts(sourceClients *SourceClients, artifacts map[string][]Artifa
 					if err != nil {
 						return errors.Cause(err)
 					}
-					regex := fmt.Sprintf("public.ecr.aws.*%s.*", imageTagOverride.Repository)
+					regex := fmt.Sprintf("%s/%s.*", r.SourceContainerRegistry, imageTagOverride.Repository)
 					compiledRegex, err := regexp.Compile(regex)
 					if err != nil {
 						return errors.Cause(err)
