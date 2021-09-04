f408cf2e254905664bb37c84edaffe8052af5c41
Rajashree Mandaogane <rajashree.28m@gmail.com>
2021-09-03 21:24:35 -0700

    Revert "Don't provide ssh key for e2e tests (#72)"
    This reverts commit 23da2a384a7426b5db865b9667644e165e25df0b.

--- test/framework/vsphere.go
+++ test/framework/vsphere.go
@@ -41,6 +41,7 @@ var requiredEnvVars = []string{
 	vsphereNetworkVar,
 	vsphereResourcePoolVar,
 	vsphereServerVar,
+	vsphereSshAuthorizedKeyVar,
 	vsphereTemplateUbuntu118Var,
 	vsphereTemplateUbuntu119Var,
 	vsphereTemplateUbuntu120Var,
