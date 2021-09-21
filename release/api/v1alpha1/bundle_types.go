3e6c36ff3a7817731a2b027149047b3e06d1a9db
danbudris <budris@amazon.com>
2021-09-21 12:39:32 -0400

    add eksa-diagnostic-collector to eks-a-tools bundle

--- release/api/v1alpha1/bundle_types.go
+++ release/api/v1alpha1/bundle_types.go
@@ -192,9 +192,10 @@ type FluxBundle struct {
 }
 
 type EksaBundle struct {
-	CliTools          Image    `json:"cliTools"`
-	ClusterController Image    `json:"clusterController"`
-	Components        Manifest `json:"components"`
+	CliTools            Image    `json:"cliTools"`
+	ClusterController   Image    `json:"clusterController"`
+	DiagnosticCollector Image    `json:"diagnosticCollector"`
+	Components          Manifest `json:"components"`
 }
 
 type EtcdadmBootstrapBundle struct {
