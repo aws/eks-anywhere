7f051ed8bc931fda439ee004bba2b085afce6ace
Guillermo Gaston <gaslor@amazon.com>
2021-09-28 20:07:55 +0000

    Move skip ip check option to method argument in provider factory

--- pkg/providers/factory/providerfactory_test.go
+++ pkg/providers/factory/providerfactory_test.go
@@ -97,7 +97,7 @@ func TestProviderFactoryBuildProvider(t *testing.T) {
 				VSphereKubectlClient: vsphereMocks.NewMockProviderKubectlClient(mockCtrl),
 				Writer:               mockswriter.NewMockFileWriter(mockCtrl),
 			}
-			got, err := p.BuildProvider(tt.args.clusterConfigFileName, tt.args.clusterConfig)
+			got, err := p.BuildProvider(tt.args.clusterConfigFileName, tt.args.clusterConfig, false)
 			if err == nil {
 				if got.Name() != tt.want.kind || got.Version(clusterSpec) != tt.want.version {
 					t.Errorf("BuildProvider() got = %v, want %v", got, tt.want)
