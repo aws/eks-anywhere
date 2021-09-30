caf069bae04df614abb1742c1f7a7be270cca3d5
Pankti Shah <pjshah@amazon.com>
2021-09-29 18:40:24 -0700

    set cp count to 3 and added e2e tests for kubernetes version 1.21 for stacked etcd

--- test/e2e/stackedetcd_test.go
+++ test/e2e/stackedetcd_test.go
@@ -20,7 +20,7 @@ func TestVSphereKubernetes120StackedEtcdBottlerocket(t *testing.T) {
 	test := framework.NewE2ETest(t,
 		framework.NewVSphere(t, framework.WithBottleRocket120()),
 		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube120)),
-		framework.WithClusterFiller(api.WithControlPlaneCount(1)),
+		framework.WithClusterFiller(api.WithControlPlaneCount(3)),
 		framework.WithClusterFiller(api.WithStackedEtcdTopology()))
 	runStackedEtcdFlow(test)
 }
@@ -29,7 +29,25 @@ func TestVSphereKubernetes120StackedEtcdUbuntu(t *testing.T) {
 	test := framework.NewE2ETest(t,
 		framework.NewVSphere(t, framework.WithUbuntu120()),
 		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube120)),
-		framework.WithClusterFiller(api.WithControlPlaneCount(1)),
+		framework.WithClusterFiller(api.WithControlPlaneCount(3)),
+		framework.WithClusterFiller(api.WithStackedEtcdTopology()))
+	runStackedEtcdFlow(test)
+}
+
+func TestVSphereKubernetes121StackedEtcdBottlerocket(t *testing.T) {
+	test := framework.NewE2ETest(t,
+		framework.NewVSphere(t, framework.WithBottleRocket121()),
+		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube121)),
+		framework.WithClusterFiller(api.WithControlPlaneCount(3)),
+		framework.WithClusterFiller(api.WithStackedEtcdTopology()))
+	runStackedEtcdFlow(test)
+}
+
+func TestVSphereKubernetes121StackedEtcdUbuntu(t *testing.T) {
+	test := framework.NewE2ETest(t,
+		framework.NewVSphere(t, framework.WithUbuntu121()),
+		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube121)),
+		framework.WithClusterFiller(api.WithControlPlaneCount(3)),
 		framework.WithClusterFiller(api.WithStackedEtcdTopology()))
 	runStackedEtcdFlow(test)
 }
@@ -38,16 +56,5 @@ func TestDockerKubernetesStackedEtcd(t *testing.T) {
 	test := framework.NewE2ETest(t,
 		framework.NewDocker(t),
 		framework.WithClusterFiller(api.WithStackedEtcdTopology()))
-	runStackedEtcdFlow(test)
-}
-
-func TestVSphereKubernetes120StackedEtcdBottlerocket3CP2Workers(t *testing.T) {
-	test := framework.NewE2ETest(t,
-		framework.NewVSphere(t, framework.WithBottleRocket120()),
-		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube120)),
-		framework.WithClusterFiller(api.WithStackedEtcdTopology()),
-		framework.WithClusterFiller(api.WithControlPlaneCount(3)),
-		framework.WithClusterFiller(api.WithWorkerNodeCount(2)),
-	)
 	runStackedEtcdFlow(test)
 }
