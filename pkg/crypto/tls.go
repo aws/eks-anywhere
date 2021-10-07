5cd59c6cec03f6869947a026e28a96596b228857
Abhinav Pandey <abhinavmpandey08@gmail.com>
2021-10-07 12:30:57 -0700

    remove getBaseCertFromUrl function

--- pkg/crypto/tls.go
+++ pkg/crypto/tls.go
@@ -24,23 +24,6 @@ func NewTlsValidator(cert, url string) TlsValidator {
 	}
 }
 
-// getBaseCertFromUrl connects to the url and retrieves the base certificate
-func (tv *DefaultTlsValidator) getBaseCertFromUrl() (*x509.Certificate, error) {
-	conf := &tls.Config{
-		// This is needed so to make a connection to the url even if it is using self-signed certs
-		InsecureSkipVerify: true,
-	}
-
-	conn, err := tls.Dial("tcp", tv.url+":443", conf)
-	if err != nil {
-		return nil, err
-	}
-	defer conn.Close()
-
-	// ConnectionState().PeerCertificates returns the entire certificate chain but we only need the base
-	return conn.ConnectionState().PeerCertificates[0], nil
-}
-
 // HasSelfSignedCert determines whether the url is using self-signed certs or not
 func (tv *DefaultTlsValidator) HasSelfSignedCert() (bool, error) {
 	conf := &tls.Config{
@@ -60,11 +43,6 @@ func (tv *DefaultTlsValidator) HasSelfSignedCert() (bool, error) {
 
 // ValidateCert parses the cert, ensures that the cert format is valid and verifies that the cert is valid for the url
 func (tv *DefaultTlsValidator) ValidateCert() error {
-	cert, err := tv.getBaseCertFromUrl()
-	if err != nil {
-		return err
-	}
-
 	// Validates that the cert format is valid
 	block, _ := pem.Decode([]byte(tv.cert))
 	if block == nil {
@@ -73,11 +51,6 @@ func (tv *DefaultTlsValidator) ValidateCert() error {
 	providedCert, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
 		return fmt.Errorf("failed to parse certificate: %v", err)
-	}
-
-	// Validates that the provided cert matches the one returned by the url
-	if !cert.Equal(providedCert) {
-		return fmt.Errorf("certificate mismatch error: provided certificate does not match certificate received from %s", tv.url)
 	}
 
 	roots := x509.NewCertPool()
