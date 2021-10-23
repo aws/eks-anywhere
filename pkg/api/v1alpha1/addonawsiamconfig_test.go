1a21f7571d24fdbd65a2abdbd20f0cbda2a2055d
Aravind Ramalingam <ramaliar@amazon.com>
2021-10-22 18:03:49 -0700

    Adding more unit test coverage to addonawsiamconfig

--- pkg/api/v1alpha1/addonawsiamconfig_test.go
+++ pkg/api/v1alpha1/addonawsiamconfig_test.go
@@ -7,8 +7,12 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestGetAndValidateAWSIamConfig(t *testing.T) {
-	c := &Cluster{}
+func TestGetAndValidateAddOnAWSIamConfig(t *testing.T) {
+	c := &Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			ClusterName: "eksa-unit-test-cluster",
+		},
+	}
 	tests := []struct {
 		testName              string
 		fileName              string
@@ -21,6 +25,104 @@ func TestGetAndValidateAWSIamConfig(t *testing.T) {
 			fileName:              "testdata/fake_file.yaml",
 			wantAddOnAWSIamConfig: nil,
 			wantErr:               true,
+		},
+		{
+			testName:              "invalid ref name",
+			fileName:              "testdata/cluster_1_21_awsiam_invalid_refname.yaml",
+			refName:               "eksa-unit-test",
+			wantAddOnAWSIamConfig: nil,
+			wantErr:               true,
+		},
+		{
+			testName:              "invalid namespace",
+			fileName:              "testdata/cluster_1_21_awsiam_invalid_namespace.yaml",
+			refName:               "eksa-unit-test",
+			wantAddOnAWSIamConfig: nil,
+			wantErr:               true,
+		},
+		{
+			testName:              "invalid AddOnAWSIamConfig no aws region",
+			fileName:              "testdata/cluster_1_21_awsiam_no_awsregion.yaml",
+			refName:               "eksa-unit-test",
+			wantAddOnAWSIamConfig: nil,
+			wantErr:               true,
+		},
+		{
+			testName:              "invalid AddOnAWSIamConfig no aws region",
+			fileName:              "testdata/cluster_1_21_awsiam_no_backendmode.yaml",
+			refName:               "eksa-unit-test",
+			wantAddOnAWSIamConfig: nil,
+			wantErr:               true,
+		},
+		{
+			testName:              "invalid AddOnAWSIamConfig unsupported MountedFile",
+			fileName:              "testdata/cluster_1_21_awsiam_unsupported_mountedfile.yaml",
+			refName:               "eksa-unit-test",
+			wantAddOnAWSIamConfig: nil,
+			wantErr:               true,
+		},
+		{
+			testName:              "invalid AddOnAWSIamConfig no arn",
+			fileName:              "testdata/cluster_1_21_awsiam_no_arn.yaml",
+			refName:               "eksa-unit-test",
+			wantAddOnAWSIamConfig: nil,
+			wantErr:               true,
+		},
+		{
+			testName: "valid AddOnAWSIamConfig default cluster id",
+			fileName: "testdata/cluster_1_21_awsiam_no_clusterid.yaml",
+			refName:  "eksa-unit-test",
+			wantAddOnAWSIamConfig: &AddOnAWSIamConfig{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "AddOnAWSIamConfig",
+					APIVersion: SchemeBuilder.GroupVersion.String(),
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "eksa-unit-test",
+				},
+				Spec: AddOnAWSIamConfigSpec{
+					AWSRegion:   "test-region",
+					BackendMode: []string{"mode1", "mode2"},
+					ClusterID:   "eksa-unit-test-cluster",
+					MapRoles: []MapRoles{
+						{
+							RoleARN:  "test-role-arn",
+							Username: "test",
+							Groups:   []string{"group1", "group2"},
+						},
+					},
+					MapUsers: []MapUsers{
+						{
+							UserARN:  "test-user-arn",
+							Username: "test",
+							Groups:   []string{"group1", "group2"},
+						},
+					},
+					Partition: "aws",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			testName: "valid AddOnAWSIamConfig no mapping eksconfigmap backend",
+			fileName: "testdata/cluster_1_21_awsiam_no_mapping_eksconfigmap.yaml",
+			refName:  "eksa-unit-test",
+			wantAddOnAWSIamConfig: &AddOnAWSIamConfig{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "AddOnAWSIamConfig",
+					APIVersion: SchemeBuilder.GroupVersion.String(),
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "eksa-unit-test",
+				},
+				Spec: AddOnAWSIamConfigSpec{
+					AWSRegion:   "test-region",
+					BackendMode: []string{"EKSConfigMap"},
+					ClusterID:   "eksa-unit-test",
+					Partition:   "aws",
+				},
+			},
+			wantErr: false,
 		},
 		{
 			testName: "valid AddOnAWSIamConfig",
@@ -38,7 +140,21 @@ func TestGetAndValidateAWSIamConfig(t *testing.T) {
 					AWSRegion:   "test-region",
 					BackendMode: []string{"mode1", "mode2"},
 					ClusterID:   "eksa-unit-test",
-					Partition:   "aws",
+					MapRoles: []MapRoles{
+						{
+							RoleARN:  "test-role-arn",
+							Username: "test",
+							Groups:   []string{"group1", "group2"},
+						},
+					},
+					MapUsers: []MapUsers{
+						{
+							UserARN:  "test-user-arn",
+							Username: "test",
+							Groups:   []string{"group1", "group2"},
+						},
+					},
+					Partition: "aws",
 				},
 			},
 			wantErr: false,
