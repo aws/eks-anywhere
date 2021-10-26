bf88b0f380e2da2693808e1fedcd277c5c4e15ac
Jiayi Wang <jiayiwang7@yahoo.com>
2021-10-26 11:12:39 -0400

    Move load kubeconfig logic to separate file

--- pkg/cluster/fetch.go
+++ pkg/cluster/fetch.go
@@ -3,12 +3,8 @@ package cluster
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
-
-	"sigs.k8s.io/yaml"
 
 	"github.com/aws/eks-anywhere/pkg/api/v1alpha1"
-	"github.com/aws/eks-anywhere/pkg/types"
 	v1alpha1release "github.com/aws/eks-anywhere/release/api/v1alpha1"
 )
 
@@ -30,33 +26,4 @@ func GetBundlesForCluster(ctx context.Context, cluster *v1alpha1.Cluster, fetch 
 	}
 
 	return bundles, nil
-}
-
-type kubeConfigCluster struct {
-	Name string `json:"name"`
-}
-
-type kubeConfigYAML struct {
-	Clusters []*kubeConfigCluster `json:"clusters"`
-}
-
-func LoadManagement(kubeconfig string) (*types.Cluster, error) {
-	if kubeconfig == "" {
-		return nil, nil
-	}
-	kubeConfigBytes, err := ioutil.ReadFile(kubeconfig)
-	if err != nil {
-		return nil, err
-	}
-	kc := &kubeConfigYAML{}
-	kc.Clusters = []*kubeConfigCluster{}
-	err = yaml.Unmarshal(kubeConfigBytes, &kc)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing kubeconfig file: %v", err)
-	}
-	return &types.Cluster{
-		Name:               kc.Clusters[0].Name,
-		KubeconfigFile:     kubeconfig,
-		ExistingManagement: true,
-	}, nil
 }
---
+++ pkg/cluster/load.go
@@ -0,0 +1,38 @@
+package cluster
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/aws/eks-anywhere/pkg/types"
+	"sigs.k8s.io/yaml"
+)
+
+type kubeConfigCluster struct {
+	Name string `json:"name"`
+}
+
+type kubeConfigYAML struct {
+	Clusters []*kubeConfigCluster `json:"clusters"`
+}
+
+func LoadManagement(kubeconfig string) (*types.Cluster, error) {
+	if kubeconfig == "" {
+		return nil, nil
+	}
+	kubeConfigBytes, err := ioutil.ReadFile(kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+	kc := &kubeConfigYAML{}
+	kc.Clusters = []*kubeConfigCluster{}
+	err = yaml.Unmarshal(kubeConfigBytes, &kc)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing kubeconfig file: %v", err)
+	}
+	return &types.Cluster{
+		Name:               kc.Clusters[0].Name,
+		KubeconfigFile:     kubeconfig,
+		ExistingManagement: true,
+	}, nil
+}
