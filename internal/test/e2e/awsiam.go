859814901096e34103e2330e43b670a1771ba351
Aravind Ramalingam <ramaliar@amazon.com>
2021-11-02 13:59:18 -0700

    Adding e2e tests for aws-iam-authenticator

---
+++ internal/test/e2e/awsiam.go
@@ -0,0 +1,26 @@
+package e2e
+
+import (
+	"os"
+	"regexp"
+
+	"github.com/aws/eks-anywhere/pkg/logger"
+	e2etests "github.com/aws/eks-anywhere/test/framework"
+)
+
+func (e *E2ESession) setupAwsIam(testRegex string) error {
+	re := regexp.MustCompile(`^.*AWSIamAuth.*$`)
+	if !re.MatchString(testRegex) {
+		logger.V(2).Info("Not running AWSIamAuth tests, skipping Env variable setup")
+		return nil
+	}
+
+	requiredEnvVars := e2etests.RequiredAWSIamEnvVars()
+	for _, eVar := range requiredEnvVars {
+		if val, ok := os.LookupEnv(eVar); ok {
+			e.testEnvVars[eVar] = val
+		}
+	}
+
+	return nil
+}
