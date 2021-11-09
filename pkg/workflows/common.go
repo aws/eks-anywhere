f9233f12d02efacb2403ee22d5ce572bff9b8431
danbudris <budris@amazon.com>
2021-11-09 11:13:32 -0500

    decompose Diagnostics task into WC and MG diag tasks

--- pkg/workflows/common.go
+++ pkg/workflows/common.go
@@ -7,15 +7,48 @@ import (
 	"github.com/aws/eks-anywhere/pkg/task"
 )
 
-type CollectDiagnosticsTask struct{}
+type CollectDiagnosticsTask struct {
+	*CollectWorkloadClusterDiagnosticsTask
+	*CollectMgmtClusterDiagnosticsTask
+}
+
+type CollectWorkloadClusterDiagnosticsTask struct{}
+
+type CollectMgmtClusterDiagnosticsTask struct{}
+
+// CollectDiagnosticsTask implementation
 
 func (s *CollectDiagnosticsTask) Run(ctx context.Context, commandContext *task.CommandContext) task.Task {
 	logger.Info("collecting cluster diagnostics")
-	_ = commandContext.ClusterManager.SaveLogsManagementCluster(ctx, commandContext.BootstrapCluster)
-	_ = commandContext.ClusterManager.SaveLogsWorkloadCluster(ctx, commandContext.Provider, commandContext.ClusterSpec, commandContext.WorkloadCluster)
+	_ = s.CollectMgmtClusterDiagnosticsTask.Run(ctx, commandContext)
+	_ = s.CollectWorkloadClusterDiagnosticsTask.Run(ctx, commandContext)
 	return nil
 }
 
 func (s *CollectDiagnosticsTask) Name() string {
 	return "collect-cluster-diagnostics"
+}
+
+// CollectWorkloadClusterDiagnosticsTask implementation
+
+func (s *CollectWorkloadClusterDiagnosticsTask) Run(ctx context.Context, commandContext *task.CommandContext) task.Task {
+	logger.Info("collecting workload cluster diagnostics")
+	_ = commandContext.ClusterManager.SaveLogsWorkloadCluster(ctx, commandContext.Provider, commandContext.ClusterSpec, commandContext.WorkloadCluster)
+	return nil
+}
+
+func (s *CollectWorkloadClusterDiagnosticsTask) Name() string {
+	return "collect-workload-cluster-diagnostics"
+}
+
+// CollectMgmtClusterDiagnosticsTask implementation
+
+func (s *CollectMgmtClusterDiagnosticsTask) Run(ctx context.Context, commandContext *task.CommandContext) task.Task {
+	logger.Info("collecting management cluster diagnostics")
+	_ = commandContext.ClusterManager.SaveLogsManagementCluster(ctx, commandContext.BootstrapCluster)
+	return nil
+}
+
+func (s *CollectMgmtClusterDiagnosticsTask) Name() string {
+	return "collect-management-cluster-diagnostics"
 }
