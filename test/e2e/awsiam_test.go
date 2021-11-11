73bb84e11a5c208ffac444a27a5dc449e19b74df
Aravind Ramalingam <60027164+pokearu@users.noreply.github.com>
2021-11-11 15:17:31 -0800

    Identity provider aws iam auth e2e tests (#556)
    * Adding framework support for aws-iam-authenticator tests
    
    * Refactored gzip file download
    
    * Adding e2e tests for aws-iam-authenticator
    
    * Enabling feature gates activation from e2e run command
    
    * Refactoring changes based on framework updates, reverting e2e --gates flag
    
    * Refactor IamAuth test validations to use existing kubectl
    
    * Checking env PATH before updating

---
+++ test/e2e/awsiam_test.go
@@ -0,0 +1,77 @@
+// +build e2e
+
+package e2e
+
+import (
+	"testing"
+
+	"github.com/aws/eks-anywhere/internal/pkg/api"
+	"github.com/aws/eks-anywhere/pkg/api/v1alpha1"
+	"github.com/aws/eks-anywhere/test/framework"
+)
+
+func runAWSIamAuthFlow(test *framework.ClusterE2ETest) {
+	test.GenerateClusterConfig()
+	test.CreateCluster()
+	test.ValidateAWSIamAuth()
+	test.StopIfFailed()
+	test.DeleteCluster()
+}
+
+func TestDockerKubernetes120AWSIamAuth(t *testing.T) {
+	test := framework.NewClusterE2ETest(t,
+		framework.NewDocker(t),
+		framework.WithAWSIam(),
+		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube120)),
+	)
+	runAWSIamAuthFlow(test)
+}
+
+func TestDockerKubernetes121AWSIamAuth(t *testing.T) {
+	test := framework.NewClusterE2ETest(t,
+		framework.NewDocker(t),
+		framework.WithAWSIam(),
+		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube121)),
+	)
+	runAWSIamAuthFlow(test)
+}
+
+func TestVSphereKubernetes120AWSIamAuth(t *testing.T) {
+	test := framework.NewClusterE2ETest(
+		t,
+		framework.NewVSphere(t, framework.WithUbuntu120()),
+		framework.WithAWSIam(),
+		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube120)),
+	)
+	runAWSIamAuthFlow(test)
+}
+
+func TestVSphereKubernetes121AWSIamAuth(t *testing.T) {
+	test := framework.NewClusterE2ETest(
+		t,
+		framework.NewVSphere(t, framework.WithUbuntu121()),
+		framework.WithAWSIam(),
+		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube121)),
+	)
+	runAWSIamAuthFlow(test)
+}
+
+func TestVSphereKubernetes120BottleRocketAWSIamAuth(t *testing.T) {
+	test := framework.NewClusterE2ETest(
+		t,
+		framework.NewVSphere(t, framework.WithBottleRocket120()),
+		framework.WithAWSIam(),
+		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube120)),
+	)
+	runAWSIamAuthFlow(test)
+}
+
+func TestVSphereKubernetes121BottleRocketAWSIamAuth(t *testing.T) {
+	test := framework.NewClusterE2ETest(
+		t,
+		framework.NewVSphere(t, framework.WithBottleRocket121()),
+		framework.WithAWSIam(),
+		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube121)),
+	)
+	runAWSIamAuthFlow(test)
+}
