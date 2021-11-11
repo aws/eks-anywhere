aa4921a89175533eeeab51d136e803b14cbeeab2
Jiayi Wang <jiayiwang7@yahoo.com>
2021-11-10 19:15:19 -0500

    Only check flux clusterConfigPath exists when bootstrapping management cluster

--- pkg/addonmanager/addonclients/fluxaddonclient.go
+++ pkg/addonmanager/addonclients/fluxaddonclient.go
@@ -647,12 +647,13 @@ func (fc *fluxForCluster) validateLocalConfigPathDoesNotExist() error {
 }
 
 func (fc *fluxForCluster) validateRemoteConfigPathDoesNotExist(ctx context.Context) error {
-	if exists, err := fc.gitOpts.Git.PathExists(ctx, fc.owner(), fc.repository(), fc.branch(), fc.path()); err != nil {
-		return fmt.Errorf("failed validating remote flux config path: %s", err)
-	} else if exists {
-		return fmt.Errorf("flux path %s already exists in remote repository", fc.path())
+	if fc.clusterSpec.IsSelfManaged() {
+		if exists, err := fc.gitOpts.Git.PathExists(ctx, fc.owner(), fc.repository(), fc.branch(), fc.path()); err != nil {
+			return fmt.Errorf("failed validating remote flux config path: %s", err)
+		} else if exists {
+			return fmt.Errorf("flux path %s already exists in remote repository", fc.path())
+		}
 	}
-
 	return nil
 }
 
