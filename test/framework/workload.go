55d7b1578b6711dea944e33af9b54d1f6aa0c7e2
danbudris <budris@amazon.com>
2021-11-19 10:20:42 -0500

    add opts to workload cluster command execution functions

--- test/framework/workload.go
+++ test/framework/workload.go
@@ -7,14 +7,17 @@ type WorkloadCluster struct {
 
 type WorkloadClusters map[string]*WorkloadCluster
 
-func (w *WorkloadCluster) CreateCluster() {
-	w.createCluster(nil, withKubeconfig(w.managementClusterKubeconfigFile()))
+func (w *WorkloadCluster) CreateCluster(opts ...CommandOpt) {
+	opts = append(opts, withKubeconfig(w.managementClusterKubeconfigFile()))
+	w.createCluster(opts...)
 }
 
-func (w *WorkloadCluster) UpgradeCluster(opts ...ClusterE2ETestOpt) {
-	w.upgradeCluster(opts, []CommandOpt{withKubeconfig(w.managementClusterKubeconfigFile())}...)
+func (w *WorkloadCluster) UpgradeCluster(clusterOpts []ClusterE2ETestOpt, commandOpts ...CommandOpt) {
+	commandOpts = append(commandOpts, withKubeconfig(w.managementClusterKubeconfigFile()))
+	w.upgradeCluster(clusterOpts, commandOpts...)
 }
 
-func (w *WorkloadCluster) DeleteCluster() {
-	w.deleteCluster(nil, withKubeconfig(w.managementClusterKubeconfigFile()))
+func (w *WorkloadCluster) DeleteCluster(opts ...CommandOpt) {
+	opts = append(opts, withKubeconfig(w.managementClusterKubeconfigFile()))
+	w.deleteCluster(opts...)
 }
