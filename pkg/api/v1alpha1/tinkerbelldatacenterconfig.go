0fbbae89d7acdfc549f89a67922857e30fe6137e
Abhinav Pandey <abhinavmpandey08@gmail.com>
2021-11-30 13:23:50 -0500

    Add basic provider skeleton for tinkerbell

---
+++ pkg/api/v1alpha1/tinkerbelldatacenterconfig.go
@@ -0,0 +1,42 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const TinkerbellDatacenterKind = "TinkerbellDatacenterConfig"
+
+// Used for generating yaml for generate clusterconfig command
+func NewTinkerbellDatacenterConfigGenerate(clusterName string) *TinkerbellDatacenterConfigGenerate {
+	return &TinkerbellDatacenterConfigGenerate{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       TinkerbellDatacenterKind,
+			APIVersion: SchemeBuilder.GroupVersion.String(),
+		},
+		ObjectMeta: ObjectMeta{
+			Name: clusterName,
+		},
+		Spec: TinkerbellDatacenterConfigSpec{},
+	}
+}
+
+func (c *TinkerbellDatacenterConfigGenerate) APIVersion() string {
+	return c.TypeMeta.APIVersion
+}
+
+func (c *TinkerbellDatacenterConfigGenerate) Kind() string {
+	return c.TypeMeta.Kind
+}
+
+func (c *TinkerbellDatacenterConfigGenerate) Name() string {
+	return c.ObjectMeta.Name
+}
+
+func GetTinkerbellDatacenterConfig(fileName string) (*TinkerbellDatacenterConfig, error) {
+	var clusterConfig TinkerbellDatacenterConfig
+	err := ParseClusterConfig(fileName, &clusterConfig)
+	if err != nil {
+		return nil, err
+	}
+	return &clusterConfig, nil
+}
