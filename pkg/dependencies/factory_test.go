66c907b9e2c12c2240b17e21970edaac79e8133f
Guillermo Gaston <gaslor@amazon.com>
2021-12-08 23:52:13 +0100

    Fix typo
    Co-authored-by: Abhay Krishna <arnchlm@amazon.com>

--- pkg/dependencies/factory_test.go
+++ pkg/dependencies/factory_test.go
@@ -43,7 +43,7 @@ func TestFactoryBuildWithProvider(t *testing.T) {
 
 	tt.Expect(err).To(BeNil())
 	tt.Expect(deps.Provider).NotTo(BeNil())
-	tt.Expect(deps.DockerClient).To(BeNil(), "it only only builds deps for vsphere")
+	tt.Expect(deps.DockerClient).To(BeNil(), "it only builds deps for vsphere")
 }
 
 func TestFactoryBuildWithClusterManager(t *testing.T) {
