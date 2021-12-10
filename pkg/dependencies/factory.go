b69d4a96b18f5a3a8b719303242c9338987ddae3
Daniel Budris <budris@amazon.com>
2021-12-09 22:03:59 -0500

    add nil check to dependency closer to avoid segfault (#802)

--- pkg/dependencies/factory.go
+++ pkg/dependencies/factory.go
@@ -47,8 +47,10 @@ type Dependencies struct {
 func (d *Dependencies) Close(ctx context.Context) error {
 	closers := []types.Closer{d.Govc, d.DockerClient, d.Kubectl, d.Kind, d.Clusterctl, d.Flux, d.Troubleshoot}
 	for _, c := range closers {
-		if err := c.Close(ctx); err != nil {
-			return err
+		if c != nil {
+			if err := c.Close(ctx); err != nil {
+				return err
+			}
 		}
 	}
 
