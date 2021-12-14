ced0dc9aac1df4fefe5c2b2318b2c2086377a02a
Guillermo Gaston <gaslor@amazon.com>
2021-12-14 17:02:47 +0000

    Added APIReader and helper method for envtest to create namespace for tests and clean it up automatically

--- internal/test/envtest/environment.go
+++ internal/test/envtest/environment.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"fmt"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	etcdv1 "github.com/mrajashree/etcdadm-controller/api/v1alpha3"
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/kubernetes/scheme"
@@ -17,6 +21,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	releasev1 "github.com/aws/eks-anywhere/release/api/v1alpha1"
 )
@@ -42,7 +47,10 @@ type Environment struct {
 	scheme  *runtime.Scheme
 	client  client.Client
 	env     *envtest.Environment
-	cancelF context.CancelFunc
+	manager manager.Manager
+	// apiReader is a non cached client (only for reads), helpful when testing the actual state of objects
+	apiReader client.Reader
+	cancelF   context.CancelFunc
 }
 
 type EnvironmentOpt func(ctx context.Context, e *Environment)
@@ -106,7 +114,6 @@ func newEnvironment(ctx context.Context) (*Environment, error) {
 
 	cfg, err := testEnv.Start()
 	if err != nil {
-		_ = testEnv.Stop()
 		return nil, err
 	}
 
@@ -123,6 +130,7 @@ func newEnvironment(ctx context.Context) (*Environment, error) {
 	if err != nil {
 		return nil, err
 	}
+	env.manager = mgr
 
 	go func() {
 		err = mgr.Start(ctx)
@@ -133,6 +141,7 @@ func newEnvironment(ctx context.Context) (*Environment, error) {
 	}
 
 	env.client = mgr.GetClient()
+	env.apiReader = mgr.GetAPIReader()
 
 	return env, nil
 }
@@ -145,4 +154,33 @@ func (e *Environment) stop() error {
 
 func (e *Environment) Client() client.Client {
 	return e.client
+}
+
+// APIReader returns a non cached reader client
+func (e *Environment) APIReader() client.Reader {
+	return e.apiReader
+}
+
+func (e *Environment) CreateNamespaceForTest(ctx context.Context, t *testing.T) string {
+	t.Helper()
+	name := strings.ReplaceAll(t.Name(), "/", "-")
+	name = strings.ReplaceAll(name, "_", "-")
+	name = strings.ToLower(name)
+	namespace := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
+
+	if err := e.client.Create(ctx, namespace); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		if err := e.client.Delete(ctx, namespace); err != nil && !apierrors.IsNotFound(err) {
+			t.Fatal(err)
+		}
+	})
+
+	return namespace.Name
 }
