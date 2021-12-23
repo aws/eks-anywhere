93e5ebe5be71c5f1c76b4bdcbbd7e11445067490
Guillermo Gaston <gaslor@amazon.com>
2021-12-23 15:44:59 +0100

    Add cilium templater (#865)

---
+++ pkg/networking/cilium/templater.go
@@ -0,0 +1,92 @@
+package cilium
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/eks-anywhere/pkg/cluster"
+)
+
+type Helm interface {
+	Template(ctx context.Context, ociURI, version, namespace string, values interface{}) ([]byte, error)
+}
+
+type Templater struct {
+	helm Helm
+}
+
+func NewTemplater(helm Helm) *Templater {
+	return &Templater{
+		helm: helm,
+	}
+}
+
+func (c *Templater) GenerateUpgradePreflightManifest(ctx context.Context, spec *cluster.Spec) ([]byte, error) {
+	v := templateValues(spec)
+	v.set(true, "preflight", "enabled")
+	v.set(false, "agent")
+	v.set(false, "operator", "enabled")
+
+	manifest, err := c.helm.Template(ctx, "placeholder", spec.VersionsBundle.Cilium.Version, namespace, v) // TODO: use real oci uri
+	if err != nil {
+		return nil, fmt.Errorf("failed generating cilium upgrade preflight manifest: %v", err)
+	}
+
+	return manifest, nil
+}
+
+func (c *Templater) GenerateManifest(ctx context.Context, spec *cluster.Spec) ([]byte, error) {
+	v := templateValues(spec)
+
+	manifest, err := c.helm.Template(ctx, "placeholder", spec.VersionsBundle.Cilium.Version, namespace, v) // TODO: use real oci uri
+	if err != nil {
+		return nil, fmt.Errorf("failed generating cilium manifest: %v", err)
+	}
+
+	return manifest, nil
+}
+
+type values map[string]interface{}
+
+func (c values) set(value interface{}, path ...string) {
+	element := c
+	for _, p := range path[:len(path)-1] {
+		e, ok := element[p]
+		if !ok {
+			e = values{}
+			element[p] = e
+		}
+		element = e.(values)
+	}
+	element[path[len(path)-1]] = value
+}
+
+func templateValues(spec *cluster.Spec) values {
+	return values{
+		"cni": values{
+			"chainingMode": "portmap",
+		},
+		"ipam": values{
+			"mode": "kubernetes",
+		},
+		"identityAllocationMode": "crd",
+		"prometheus": values{
+			"enabled": true,
+		},
+		"rollOutCiliumPods": true,
+		"tunnel":            "geneve",
+		"image": values{
+			"repository": spec.VersionsBundle.Cilium.Cilium.Image(),
+			"tag":        spec.VersionsBundle.Cilium.Cilium.Tag(),
+		},
+		"operator": values{
+			"image": values{
+				"repository": spec.VersionsBundle.Cilium.Operator.Image(),
+				"tag":        spec.VersionsBundle.Cilium.Operator.Tag(),
+			},
+			"prometheus": values{
+				"enabled": true,
+			},
+		},
+	}
+}
---
+++ pkg/networking/cilium/templater_test.go
@@ -0,0 +1,173 @@
+package cilium_test
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/gomega"
+
+	"github.com/aws/eks-anywhere/internal/test"
+	"github.com/aws/eks-anywhere/pkg/cluster"
+	"github.com/aws/eks-anywhere/pkg/networking/cilium"
+	"github.com/aws/eks-anywhere/pkg/networking/cilium/mocks"
+)
+
+type templaterTest struct {
+	*WithT
+	ctx                     context.Context
+	t                       *cilium.Templater
+	h                       *mocks.MockHelm
+	manifest                []byte
+	uri, version, namespace string
+	spec                    *cluster.Spec
+}
+
+func newtemplaterTest(t *testing.T) *templaterTest {
+	ctrl := gomock.NewController(t)
+	h := mocks.NewMockHelm(ctrl)
+	return &templaterTest{
+		WithT:     NewWithT(t),
+		ctx:       context.Background(),
+		h:         h,
+		t:         cilium.NewTemplater(h),
+		manifest:  []byte("manifestContent"),
+		uri:       "placeholder",
+		version:   "v1.9.11-eksa.1",
+		namespace: "kube-system",
+		spec: test.NewClusterSpec(func(s *cluster.Spec) {
+			s.VersionsBundle.Cilium.Version = "v1.9.11-eksa.1"
+			s.VersionsBundle.Cilium.Cilium.URI = "public.ecr.aws/isovalent/cilium:v1.9.11-eksa.1"
+			s.VersionsBundle.Cilium.Operator.URI = "public.ecr.aws/isovalent/operator:v1.9.11-eksa.1"
+		}),
+	}
+}
+
+func (t *templaterTest) expectHelmTemplateWith(wantValues gomock.Matcher) *gomock.Call {
+	return t.h.EXPECT().Template(t.ctx, t.uri, t.version, t.namespace, wantValues)
+}
+
+func eqMap(m map[string]interface{}) gomock.Matcher {
+	return &mapMatcher{m: m}
+}
+
+// mapMacher implements a gomock matcher for maps
+// transforms any map or struct into a map[string]interface{} and uses DeepEqual to compare
+type mapMatcher struct {
+	m map[string]interface{}
+}
+
+func (e *mapMatcher) Matches(x interface{}) bool {
+	xJson, err := json.Marshal(x)
+	if err != nil {
+		return false
+	}
+	xMap := &map[string]interface{}{}
+	err = json.Unmarshal(xJson, xMap)
+	if err != nil {
+		return false
+	}
+
+	return reflect.DeepEqual(e.m, *xMap)
+}
+
+func (e *mapMatcher) String() string {
+	return fmt.Sprintf("matches map %v", e.m)
+}
+
+func TestTemplaterGenerateUpgradePreflightManifestSuccess(t *testing.T) {
+	wantValues := map[string]interface{}{
+		"cni": map[string]interface{}{
+			"chainingMode": "portmap",
+		},
+		"ipam": map[string]interface{}{
+			"mode": "kubernetes",
+		},
+		"identityAllocationMode": "crd",
+		"prometheus": map[string]interface{}{
+			"enabled": true,
+		},
+		"rollOutCiliumPods": true,
+		"tunnel":            "geneve",
+		"image": map[string]interface{}{
+			"repository": "public.ecr.aws/isovalent/cilium",
+			"tag":        "v1.9.11-eksa.1",
+		},
+		"operator": map[string]interface{}{
+			"image": map[string]interface{}{
+				"repository": "public.ecr.aws/isovalent/operator",
+				"tag":        "v1.9.11-eksa.1",
+			},
+			"prometheus": map[string]interface{}{
+				"enabled": true,
+			},
+			"enabled": false,
+		},
+		"preflight": map[string]interface{}{
+			"enabled": true,
+		},
+		"agent": false,
+	}
+
+	tt := newtemplaterTest(t)
+	tt.expectHelmTemplateWith(eqMap(wantValues)).Return(tt.manifest, nil)
+
+	tt.Expect(tt.t.GenerateUpgradePreflightManifest(tt.ctx, tt.spec)).To(Equal(tt.manifest), "templater.GenerateUpgradePreflightManifest() should return right manifest")
+}
+
+func TestTemplaterGenerateUpgradePreflightManifestError(t *testing.T) {
+	tt := newtemplaterTest(t)
+	tt.expectHelmTemplateWith(gomock.Any()).Return(nil, errors.New("error from helm")) // Using any because we only want to test the returned error
+
+	_, err := tt.t.GenerateUpgradePreflightManifest(tt.ctx, tt.spec)
+	tt.Expect(err).To(HaveOccurred(), "templater.GenerateUpgradePreflightManifest() should fail")
+	tt.Expect(err).To(MatchError(ContainSubstring("error from helm")))
+}
+
+func TestTemplaterGenerateManifestSuccess(t *testing.T) {
+	wantValues := map[string]interface{}{
+		"cni": map[string]interface{}{
+			"chainingMode": "portmap",
+		},
+		"ipam": map[string]interface{}{
+			"mode": "kubernetes",
+		},
+		"identityAllocationMode": "crd",
+		"prometheus": map[string]interface{}{
+			"enabled": true,
+		},
+		"rollOutCiliumPods": true,
+		"tunnel":            "geneve",
+		"image": map[string]interface{}{
+			"repository": "public.ecr.aws/isovalent/cilium",
+			"tag":        "v1.9.11-eksa.1",
+		},
+		"operator": map[string]interface{}{
+			"image": map[string]interface{}{
+				"repository": "public.ecr.aws/isovalent/operator",
+				"tag":        "v1.9.11-eksa.1",
+			},
+			"prometheus": map[string]interface{}{
+				"enabled": true,
+			},
+		},
+	}
+
+	tt := newtemplaterTest(t)
+	tt.expectHelmTemplateWith(eqMap(wantValues)).Return(tt.manifest, nil)
+
+	tt.Expect(tt.t.GenerateManifest(tt.ctx, tt.spec)).To(Equal(tt.manifest), "templater.GenerateManifest() should return right manifest")
+}
+
+func TestTemplaterGenerateManifestError(t *testing.T) {
+	tt := newtemplaterTest(t)
+	tt.expectHelmTemplateWith(gomock.Any()).Return(nil, errors.New("error from helm")) // Using any because we only want to test the returned error
+
+	_, err := tt.t.GenerateManifest(tt.ctx, tt.spec)
+	tt.Expect(err).To(HaveOccurred(), "templater.GenerateManifest() should fail")
+	tt.Expect(err).To(MatchError(ContainSubstring("error from helm")))
+}
