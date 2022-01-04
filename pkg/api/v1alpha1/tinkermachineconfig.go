13c87e0ed90917a918dd73648e904d289b1ce6f4
Pankti Shah <pjshah@amazon.com>
2022-01-04 15:11:59 -0800

    added default templateoverride value inside clusterconfig file for tinkerbell

--- pkg/api/v1alpha1/tinkermachineconfig.go
+++ pkg/api/v1alpha1/tinkermachineconfig.go
@@ -9,7 +9,74 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-const TinkerbellMachineConfigKind = "TinkerbellMachineConfig"
+const (
+	TinkerbellMachineConfigKind = "TinkerbellMachineConfig"
+	TinkerbellDefaultTemplate   = `
+version: "0.1"
+name: %s
+global_timeout: 6000
+tasks:
+  - name: "%s"
+    worker: "{{.device_1}}"
+    volumes:
+      - /dev:/dev
+      - /dev/console:/dev/console
+      - /lib/firmware:/lib/firmware:ro
+    actions:
+      - name: "stream-image"
+        image: oci2disk:v1.0.0
+        timeout: 360
+        environment:
+          IMG_URL: %s
+          DEST_DISK: %s
+          COMPRESSED: true
+      - name: "add-tink-cloud-init-config"
+        image: writefile:v1.0.0
+        timeout: 90
+        environment:
+          DEST_DISK: %s
+          FS_TYPE: ext4
+          DEST_PATH: /etc/cloud/cloud.cfg.d/10_tinkerbell.cfg
+          UID: 0
+          GID: 0
+          MODE: 0600
+          DIRMODE: 0700
+          CONTENTS: |
+            datasource:
+              Ec2:
+                metadata_urls: ["%s"]
+                strict_id: false
+            system_info:
+              default_user:
+                name: tink
+                groups: [wheel, adm]
+                sudo: ["ALL=(ALL) NOPASSWD:ALL"]
+                shell: /bin/bash
+            manage_etc_hosts: localhost
+            warnings:
+              dsid_missing_source: off
+      - name: "add-tink-cloud-init-ds-config"
+        image: writefile:v1.0.0
+        timeout: 90
+        environment:
+          DEST_DISK: %s
+          FS_TYPE: ext4
+          DEST_PATH: /etc/cloud/ds-identify.cfg
+          UID: 0
+          GID: 0
+          MODE: 0600
+          DIRMODE: 0700
+          CONTENTS: |
+            datasource: Ec2
+      - name: "kexec-image"
+        image: kexec:v1.0.0
+        timeout: 90
+        pid: host
+        environment:
+          BLOCK_DEVICE: %s
+          FS_TYPE: ext4
+`
+)
 
 // Used for generating yaml for generate clusterconfig command
 func NewTinkerbellMachineConfigGenerate(name string) *TinkerbellMachineConfigGenerate {
@@ -27,6 +94,7 @@ func NewTinkerbellMachineConfigGenerate(name string) *TinkerbellMachineConfigGen
 				Name:              "ec2-user",
 				SshAuthorizedKeys: []string{"ssh-rsa AAAA..."},
 			}},
+			TemplateOverride: TinkerbellDefaultTemplate,
 		},
 	}
 }
