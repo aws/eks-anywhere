f15a5916691511c2ebc18c2d404e5b2e9d9136d4
Abhinav Pandey <abhinavmpandey08@gmail.com>
2022-01-31 13:44:53 -0800

    Add tink cli to executables (#1067)

---
+++ pkg/executables/tink.go
@@ -0,0 +1,39 @@
+package executables
+
+import (
+	"context"
+	"fmt"
+)
+
+const (
+	tinkPath                   = "tink"
+	TinkerbellCertUrlKey       = "TINKERBELL_CERT_URL"
+	TinkerbellGrpcAuthorityKey = "TINKERBELL_GRPC_AUTHORITY"
+)
+
+type Tink struct {
+	Executable
+	tinkerbellCertUrl       string
+	tinkerbellGrpcAuthority string
+	envMap                  map[string]string
+}
+
+func NewTink(executable Executable, tinkerbellCertUrl, tinkerbellGrpcAuthority string) *Tink {
+	return &Tink{
+		Executable:              executable,
+		tinkerbellCertUrl:       tinkerbellCertUrl,
+		tinkerbellGrpcAuthority: tinkerbellGrpcAuthority,
+		envMap: map[string]string{
+			TinkerbellCertUrlKey:       tinkerbellCertUrl,
+			TinkerbellGrpcAuthorityKey: tinkerbellGrpcAuthority,
+		},
+	}
+}
+
+func (t *Tink) PushHardware(ctx context.Context, hardware []byte) error {
+	params := []string{"hardware", "push"}
+	if _, err := t.Command(ctx, params...).WithStdIn(hardware).WithEnvVars(t.envMap).Run(); err != nil {
+		return fmt.Errorf("error pushing hardware: %v", err)
+	}
+	return nil
+}
