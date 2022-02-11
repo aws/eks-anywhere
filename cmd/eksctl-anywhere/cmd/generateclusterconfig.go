a72c08563b455810ae091c180a49cf84c42683d6
Max Dribinsky <maxdrib@users.noreply.github.com>
2022-02-10 19:12:52 -0500

    Adding cloudstack cluster config and CRD's (#1076)
    * Adding cloudstack eksa object definitions and webhooks
    
    * Minor refactoring of types/webhooks
    
    * Adding descriptions for cloudstack config specs and removing some unused attributes
    
    * Renaming CloudStackDeployment to CloudStackDatacenter
    
    * Removing ManagementEndpoint to fix unit tests
    
    * Fixing linting by running gofumpt
    
    * Deleting old redundant cloudstackdeployment config file
    
    * Aligning attributes supporting uuid/name and adding validation on certSANs attribute
    
    * Refactoring ambiguous cloudstack resources to explicitly set type of id/name
    
    * Removing OSFamily attribute for CloudStack
    
    * Removing certSANs attribute since it's only supported for 1 provider
    
    * Removing details attribute from CloudStackMachineConfig
    
    * Adding ability to generate cluster config behind feature gate
    
    * Addressing nit

--- cmd/eksctl-anywhere/cmd/generateclusterconfig.go
+++ cmd/eksctl-anywhere/cmd/generateclusterconfig.go
@@ -114,6 +114,46 @@ func generateClusterConfig(clusterName string) error {
 			return fmt.Errorf("error outputting yaml: %v", err)
 		}
 		machineGroupYaml = append(machineGroupYaml, cpMcYaml, workerMcYaml, etcdMcYaml)
+	case constants.CloudStackProviderName:
+		if !features.IsActive(features.CloudStackProvider()) {
+			return fmt.Errorf("The cloudstack infrastructure provider is still in development!")
+		}
+		clusterConfigOpts = append(clusterConfigOpts, v1alpha1.WithClusterEndpoint())
+		datacenterConfig := v1alpha1.NewCloudStackDatacenterConfigGenerate(clusterName)
+		clusterConfigOpts = append(clusterConfigOpts, v1alpha1.WithDatacenterRef(datacenterConfig))
+		clusterConfigOpts = append(clusterConfigOpts,
+			v1alpha1.ControlPlaneConfigCount(2),
+			v1alpha1.ExternalETCDConfigCount(3),
+			v1alpha1.WorkerNodeConfigCount(2),
+		)
+		dcyaml, err := yaml.Marshal(datacenterConfig)
+		if err != nil {
+			return fmt.Errorf("error outputting yaml: %v", err)
+		}
+		datacenterYaml = dcyaml
+		// need to default control plane config name to something different from the cluster name based on assumption
+		// in controller code
+		cpMachineConfig := v1alpha1.NewCloudStackMachineConfigGenerate(clusterName + "-cp")
+		workerMachineConfig := v1alpha1.NewCloudStackMachineConfigGenerate(clusterName)
+		etcdMachineConfig := v1alpha1.NewCloudStackMachineConfigGenerate(fmt.Sprintf("%s-etcd", clusterName))
+		clusterConfigOpts = append(clusterConfigOpts,
+			v1alpha1.WithCPMachineGroupRef(cpMachineConfig),
+			v1alpha1.WithWorkerMachineGroupRef(workerMachineConfig),
+			v1alpha1.WithEtcdMachineGroupRef(etcdMachineConfig),
+		)
+		cpMcYaml, err := yaml.Marshal(cpMachineConfig)
+		if err != nil {
+			return fmt.Errorf("error outputting yaml: %v", err)
+		}
+		workerMcYaml, err := yaml.Marshal(workerMachineConfig)
+		if err != nil {
+			return fmt.Errorf("error outputting yaml: %v", err)
+		}
+		etcdMcYaml, err := yaml.Marshal(etcdMachineConfig)
+		if err != nil {
+			return fmt.Errorf("error outputting yaml: %v", err)
+		}
+		machineGroupYaml = append(machineGroupYaml, cpMcYaml, workerMcYaml, etcdMcYaml)
 	case constants.TinkerbellProviderName:
 		if features.IsActive(features.TinkerbellProvider()) {
 			clusterConfigOpts = append(clusterConfigOpts, v1alpha1.WithClusterEndpoint())
---
+++ pkg/api/v1alpha1/cloudstackdatacenterconfig.go
@@ -0,0 +1,54 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const CloudStackDatacenterKind = "CloudStackDatacenterConfig"
+
+// Used for generating yaml for generate clusterconfig command
+func NewCloudStackDatacenterConfigGenerate(clusterName string) *CloudStackDatacenterConfigGenerate {
+	return &CloudStackDatacenterConfigGenerate{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       CloudStackDatacenterKind,
+			APIVersion: SchemeBuilder.GroupVersion.String(),
+		},
+		ObjectMeta: ObjectMeta{
+			Name: clusterName,
+		},
+		Spec: CloudStackDatacenterConfigSpec{
+			Domain: "domain1",
+			Zone: CloudStackResourceRef{
+				Value: "zone1",
+				Type:  "name",
+			},
+			Network: CloudStackResourceRef{
+				Value: "net1",
+				Type:  "name",
+			},
+			Account:  "admin",
+			Insecure: false,
+		},
+	}
+}
+
+func (c *CloudStackDatacenterConfigGenerate) APIVersion() string {
+	return c.TypeMeta.APIVersion
+}
+
+func (c *CloudStackDatacenterConfigGenerate) Kind() string {
+	return c.TypeMeta.Kind
+}
+
+func (c *CloudStackDatacenterConfigGenerate) Name() string {
+	return c.ObjectMeta.Name
+}
+
+func GetCloudStackDatacenterConfig(fileName string) (*CloudStackDatacenterConfig, error) {
+	var clusterConfig CloudStackDatacenterConfig
+	err := ParseClusterConfig(fileName, &clusterConfig)
+	if err != nil {
+		return nil, err
+	}
+	return &clusterConfig, nil
+}
