eed0567128db6fef720ed7a4472fe4d471c41c6a
Aravind Ramalingam <60027164+pokearu@users.noreply.github.com>
2022-02-16 18:37:53 -0800

    Bmc Secret validations for hardware config (#1230)
    * Adding input validations for BMC in hardware config
    
    * Adding input validation for Secrets in hardware config
    
    * Adding mark pass to validations
    
    * Adding extra validations to hardware, BMC
    
    * Renaming variables in func

--- pkg/hardware/config.go
+++ pkg/hardware/config.go
@@ -12,6 +12,8 @@ import (
 	"sigs.k8s.io/yaml"
 
 	"github.com/aws/eks-anywhere/pkg/api/v1alpha1"
+	"github.com/aws/eks-anywhere/pkg/constants"
+	"github.com/aws/eks-anywhere/pkg/networkutils"
 )
 
 type HardwareConfig struct {
@@ -67,9 +69,17 @@ func (hc *HardwareConfig) setHardwareConfigFromFile(hardwareFileName string) err
 	return nil
 }
 
-func (hc *HardwareConfig) ValidateBmcRefMapping() error {
+func (hc *HardwareConfig) ValidateHardware() error {
 	bmcRefMap := hc.initBmcRefMap()
 	for _, hw := range hc.hardwareList {
+		if hw.Name == "" {
+			return fmt.Errorf("hardware name is required")
+		}
+
+		if hw.Spec.ID == "" {
+			return fmt.Errorf("hardware: %s ID is required", hw.Name)
+		}
+
 		if hw.Spec.BmcRef == "" {
 			return fmt.Errorf("bmcRef not present in hardware %s", hw.Name)
 		}
@@ -88,6 +98,74 @@ func (hc *HardwareConfig) ValidateBmcRefMapping() error {
 	return nil
 }
 
+func (hc *HardwareConfig) ValidateBMC() error {
+	secretRefMap := hc.initSecretRefMap()
+	bmcIpMap := make(map[string]struct{}, len(hc.bmcList))
+	for _, bmc := range hc.bmcList {
+		if bmc.Name == "" {
+			return fmt.Errorf("bmc name is required")
+		}
+
+		if bmc.Spec.AuthSecretRef.Name == "" {
+			return fmt.Errorf("authSecretRef name required for bmc %s", bmc.Name)
+		}
+
+		if bmc.Spec.AuthSecretRef.Namespace != constants.EksaSystemNamespace {
+			return fmt.Errorf("invalid authSecretRef namespace: %s for bmc %s", bmc.Spec.AuthSecretRef.Namespace, bmc.Name)
+		}
+
+		if _, ok := secretRefMap[bmc.Spec.AuthSecretRef.Name]; !ok {
+			return fmt.Errorf("bmc authSecretRef: %s not present in hardware config", bmc.Spec.AuthSecretRef.String())
+		}
+
+		if _, ok := bmcIpMap[bmc.Spec.Host]; ok {
+			return fmt.Errorf("duplicate host IP: %s for bmc %s", bmc.Spec.Host, bmc.Name)
+		} else {
+			bmcIpMap[bmc.Spec.Host] = struct{}{}
+		}
+
+		if err := networkutils.ValidateIP(bmc.Spec.Host); err != nil {
+			return fmt.Errorf("bmc host IP: %v", err)
+		}
+
+		if bmc.Spec.Vendor == "" {
+			return fmt.Errorf("bmc: %s vendor is required", bmc.Name)
+		}
+	}
+
+	return nil
+}
+
+func (hc *HardwareConfig) ValidateBmcSecretRefs() error {
+	for _, s := range hc.secretList {
+		if s.Name == "" {
+			return fmt.Errorf("secret name is required")
+		}
+		if s.Namespace != constants.EksaSystemNamespace {
+			return fmt.Errorf("invalid secret namespace: %s for secret: %s expected: %s", s.Namespace, s.Name, constants.EksaSystemNamespace)
+		}
+		dUsr, dOk := s.Data["username"]
+		sdUsr, sdOk := s.StringData["username"]
+		if !dOk && !sdOk {
+			return fmt.Errorf("secret: %s must contain key username", s.Name)
+		}
+		if (dOk && len(dUsr) == 0) || (sdOk && sdUsr == "") {
+			return fmt.Errorf("username can not be empty for secret: %s", s.Name)
+		}
+
+		dPwd, dOk := s.Data["password"]
+		sdPwd, sdOk := s.StringData["password"]
+		if !dOk && !sdOk {
+			return fmt.Errorf("secret: %s must contain key password", s.Name)
+		}
+		if (dOk && len(dPwd) == 0) || (sdOk && sdPwd == "") {
+			return fmt.Errorf("password can not be empty for secret: %s", s.Name)
+		}
+	}
+
+	return nil
+}
+
 func (hc *HardwareConfig) initBmcRefMap() map[string]*tinkv1alpha1.Hardware {
 	bmcRefMap := make(map[string]*tinkv1alpha1.Hardware, len(hc.bmcList))
 	for _, bmc := range hc.bmcList {
@@ -95,4 +173,13 @@ func (hc *HardwareConfig) initBmcRefMap() map[string]*tinkv1alpha1.Hardware {
 	}
 
 	return bmcRefMap
+}
+
+func (hc *HardwareConfig) initSecretRefMap() map[string]struct{} {
+	secretRefMap := make(map[string]struct{}, len(hc.secretList))
+	for _, s := range hc.secretList {
+		secretRefMap[s.Name] = struct{}{}
+	}
+
+	return secretRefMap
 }
