e3ea26b1e07590db0218a5c97822b3a56d73c8f8
Daniel Budris <budris@amazon.com>
2022-02-22 15:57:54 -0500

    Control Plane Taints: end-to-end tests; account for applied taints (#1254)
    * cp taints e2e tests; worker and cp validation methods
    
    * remove unused GetKubeadmConfigTemplate method from kubectl exectuable
    
    * validate control plane taints in taints e2e tests
    
    * add control plane taint tolerations to IAM auth configuraiton
    
    * add custom control plane taint tolerations to vsphere-csi-controller
    
    * account for default CP taint
    
    * Update test/framework/workerNodes.go
    
    * Update pkg/awsiamauth/config/aws-iam-authenticator.yaml
    
    Co-authored-by: Vivek Koppuru <vivek.koppuru@gmail.com>
    
    * set taints feature flag to true on e2e test
    
    Co-authored-by: Vivek Koppuru <vivek.koppuru@gmail.com>

--- test/e2e/taints_test.go
+++ test/e2e/taints_test.go
@@ -5,18 +5,26 @@ package e2e
 import (
 	"testing"
 
+	corev1 "k8s.io/api/core/v1"
+
 	"github.com/aws/eks-anywhere/internal/pkg/api"
 	"github.com/aws/eks-anywhere/pkg/api/v1alpha1"
 	"github.com/aws/eks-anywhere/test/framework"
 )
 
+const worker0 = "worker-0"
+const worker1 = "worker-1"
+const worker2 = "worker-2"
+
 func runTaintsUpgradeFlow(test *framework.ClusterE2ETest, updateVersion v1alpha1.KubernetesVersion, clusterOpts ...framework.ClusterE2ETestOpt) {
 	test.GenerateClusterConfig()
 	test.CreateCluster()
-	test.VaidateWorkerNodeTaints()
+	test.ValidateWorkerNodes(framework.ValidateWorkerNodeTaints)
+	test.ValidateControlPlaneNodes(framework.ValidateControlPlaneTaints)
 	test.UpgradeCluster(clusterOpts)
 	test.ValidateCluster(updateVersion)
-	test.VaidateWorkerNodeTaints()
+	test.ValidateWorkerNodes(framework.ValidateWorkerNodeTaints)
+	test.ValidateControlPlaneNodes(framework.ValidateControlPlaneTaints)
 	test.StopIfFailed()
 	test.DeleteCluster()
 }
@@ -26,12 +34,63 @@ func runTaintsUpgradeFlow(test *framework.ClusterE2ETest, updateVersion v1alpha1
 // remove a taint from a node
 // add a taint to a node which already has another taint
 // add a taint to a node which had no taints
-func TestVSphereKubernetes121TaintsWorkerNodeGroups(t *testing.T) {
-	worker0 := "worker-0"
-	worker1 := "worker-1"
-	worker2 := "worker-2"
+func TestVSphereKubernetes121Taints(t *testing.T) {
+	provider := ubuntu121ProviderWithTaints(t)
 
-	provider := framework.NewVSphere(t,
+	test := framework.NewClusterE2ETest(
+		t,
+		provider,
+		framework.WithClusterFiller(
+			api.WithKubernetesVersion(v1alpha1.Kube121),
+			api.WithExternalEtcdTopology(1),
+			api.WithControlPlaneCount(1),
+			api.RemoveAllWorkerNodeGroups(), // This gives us a blank slate
+		),
+		framework.WithEnvVar("TAINTS_SUPPORT", "true"),
+	)
+
+	runTaintsUpgradeFlow(
+		test,
+		v1alpha1.Kube121,
+		framework.WithClusterUpgrade(
+			api.WithWorkerNodeGroup(worker0, api.WithTaint(framework.NoExecuteTaint())),
+			api.WithWorkerNodeGroup(worker1, api.WithTaint(framework.NoExecuteTaint())),
+			api.WithWorkerNodeGroup(worker2, api.WithNoTaints()),
+			api.WithControlPlaneTaints([]corev1.Taint{framework.PreferNoScheduleTaint()}),
+		),
+	)
+}
+
+func TestVSphereKubernetes121TaintsBottlerocket(t *testing.T) {
+	provider := bottlerocket121ProviderWithTaints(t)
+
+	test := framework.NewClusterE2ETest(
+		t,
+		provider,
+		framework.WithClusterFiller(
+			api.WithKubernetesVersion(v1alpha1.Kube121),
+			api.WithExternalEtcdTopology(1),
+			api.WithControlPlaneCount(1),
+			api.RemoveAllWorkerNodeGroups(), // This gives us a blank slate
+		),
+		framework.WithEnvVar("TAINTS_SUPPORT", "true"),
+	)
+
+	runTaintsUpgradeFlow(
+		test,
+		v1alpha1.Kube121,
+		framework.WithClusterUpgrade(
+			api.WithWorkerNodeGroup(worker0, api.WithTaint(framework.NoExecuteTaint())),
+			api.WithWorkerNodeGroup(worker1, api.WithTaint(framework.NoExecuteTaint())),
+			api.WithWorkerNodeGroup(worker2, api.WithNoTaints()),
+			api.WithControlPlaneTaints([]corev1.Taint{framework.PreferNoScheduleTaint()}),
+		),
+	)
+}
+
+
+func ubuntu121ProviderWithTaints(t *testing.T) *framework.VSphere {
+	return framework.NewVSphere(t,
 		framework.WithVSphereWorkerNodeGroup(
 			worker0,
 			framework.NoScheduleWorkerNodeGroup(worker0, 2),
@@ -46,24 +105,22 @@ func TestVSphereKubernetes121TaintsWorkerNodeGroups(t *testing.T) {
 		),
 		framework.WithUbuntu121(),
 	)
-	test := framework.NewClusterE2ETest(
-		t,
-		provider,
-		framework.WithClusterFiller(
-			api.WithKubernetesVersion(v1alpha1.Kube121),
-			api.WithExternalEtcdTopology(1),
-			api.WithControlPlaneCount(1),
-			api.RemoveAllWorkerNodeGroups(), // This gives us a blank slate
-		),
-	)
+}
 
-	runTaintsUpgradeFlow(
-		test,
-		v1alpha1.Kube121,
-		framework.WithClusterUpgrade(
-			api.WithWorkerNodeGroup(worker0, api.WithTaint(framework.NoExecuteTaint())),
-			api.WithWorkerNodeGroup(worker1, api.WithTaint(framework.NoExecuteTaint())),
-			api.WithWorkerNodeGroup(worker2, api.WithNoTaints()),
+func bottlerocket121ProviderWithTaints(t *testing.T) *framework.VSphere {
+	return framework.NewVSphere(t,
+		framework.WithVSphereWorkerNodeGroup(
+			worker0,
+			framework.NoScheduleWorkerNodeGroup(worker0, 2),
 		),
+		framework.WithVSphereWorkerNodeGroup(
+			worker1,
+			framework.WithWorkerNodeGroup(worker1, api.WithCount(1)),
+		),
+		framework.WithVSphereWorkerNodeGroup(
+			worker2,
+			framework.PreferNoScheduleWorkerNodeGroup(worker2, 1),
+		),
+		framework.WithBottleRocket121(),
 	)
 }
