b879564b0de26613c408c565a8fb368716d72e9a
Daniel Budris <budris@amazon.com>
2022-02-23 11:09:55 -0500

    Backport taints tests (#1275)
    * End to end tests for taints (#1224)
    
    * add GetNodes method to kubectl exe
    
    * add basic test for taints upgrade
    
    * move taint generation methods to framework
    
    * refactor test to deduce node from wngc and compare taints
    
    * move methods to framework, remove uneeded check'
    
    * account for multiple machineSets on cluster after upgrade
    
    * Update internal/pkg/api/cluster.go
    
    Co-authored-by: Guillermo Gaston <guillermogastonlorente@gmail.com>
    
    * Update internal/pkg/api/cluster.go
    
    Co-authored-by: Guillermo Gaston <guillermogastonlorente@gmail.com>
    
    * Update test/framework/taints.go
    
    Co-authored-by: Guillermo Gaston <guillermogastonlorente@gmail.com>
    
    Co-authored-by: Guillermo Gaston <guillermogastonlorente@gmail.com>
    
    * Control Plane Taints: end-to-end tests; account for applied taints (#1254)
    
    * cp taints e2e tests; worker and cp validation methods
    
    * remove unused GetKubeadmConfigTemplate method from kubectl exectuable
    
    * validate control plane taints in taints e2e tests
    
    * add control plane taint tolerations to IAM auth configuraiton
    
    * add custom control plane taint tolerations to vsphere-csi-controller
    
    * account for default CP taint
    
    * Update test/framework/workerNodes.go
    
    * Update pkg/awsiamauth/config/aws-iam-authenticator.yaml
    
    Co-authored-by: Vivek Koppuru <vivek.koppuru@gmail.com>
    
    * set taints feature flag to true on e2e test
    
    Co-authored-by: Vivek Koppuru <vivek.koppuru@gmail.com>
    
    * remove feature flag set in tests
    
    Co-authored-by: Guillermo Gaston <guillermogastonlorente@gmail.com>
    Co-authored-by: Vivek Koppuru <vivek.koppuru@gmail.com>

--- pkg/executables/kubectl.go
+++ pkg/executables/kubectl.go
@@ -293,6 +293,30 @@ func (k *Kubectl) ListCluster(ctx context.Context) error {
 	return nil
 }
 
+func (k *Kubectl) GetNodes(ctx context.Context, kubeconfig string) ([]corev1.Node, error) {
+	params := []string{"get", "nodes", "-o", "json", "--kubeconfig", kubeconfig}
+	stdOut, err := k.Execute(ctx, params...)
+	if err != nil {
+		return nil, fmt.Errorf("error getting nodes: %v", err)
+	}
+	response := &corev1.NodeList{}
+	err = json.Unmarshal(stdOut.Bytes(), response)
+
+	return response.Items, err
+}
+
+func (k *Kubectl) GetControlPlaneNodes(ctx context.Context, kubeconfig string) ([]corev1.Node, error) {
+	params := []string{"get", "nodes", "-o", "json", "--kubeconfig", kubeconfig, "--selector=node-role.kubernetes.io/control-plane"}
+	stdOut, err := k.Execute(ctx, params...)
+	if err != nil {
+		return nil, fmt.Errorf("error getting control plane nodes: %v", err)
+	}
+	response := &corev1.NodeList{}
+	err = json.Unmarshal(stdOut.Bytes(), response)
+
+	return response.Items, err
+}
+
 func (k *Kubectl) ValidateNodes(ctx context.Context, kubeconfig string) error {
 	template := "{{range .items}}{{.metadata.name}}\n{{end}}"
 	params := []string{"get", "nodes", "-o", "go-template", "--template", template, "--kubeconfig", kubeconfig}
@@ -478,6 +502,31 @@ func (k *Kubectl) GetMachines(ctx context.Context, cluster *types.Cluster, clust
 	err = json.Unmarshal(stdOut.Bytes(), response)
 	if err != nil {
 		return nil, fmt.Errorf("error parsing get machines response: %v", err)
+	}
+
+	return response.Items, nil
+}
+
+type machineSetResponse struct {
+	Items []clusterv1.MachineSet `json:"items,omitempty"`
+}
+
+func (k *Kubectl) GetMachineSets(ctx context.Context, machineDeploymentName string, cluster *types.Cluster) ([]clusterv1.MachineSet, error) {
+	params := []string{
+		"get", "machinesets", "-o", "json", "--kubeconfig", cluster.KubeconfigFile,
+		"--selector=cluster.x-k8s.io/deployment-name=" + machineDeploymentName,
+		"--namespace", constants.EksaSystemNamespace,
+	}
+
+	stdOut, err := k.Execute(ctx, params...)
+	if err != nil {
+		return nil, fmt.Errorf("error getting machineset associated with deployment %s: %v", machineDeploymentName, err)
+	}
+
+	response := &machineSetResponse{}
+	err = json.Unmarshal(stdOut.Bytes(), response)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing get machinesets response: %v", err)
 	}
 
 	return response.Items, nil
