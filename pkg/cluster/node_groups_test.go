283b63b3b7af00516b9d90a093a2e9caddea6887
Guillermo Gaston <gaslor@amazon.com>
2022-02-24 13:51:58 -0600

    Fix nodes to delete calculation for empty name (#1297)
    The old cluster spec (coming from v0.6.1 or prior) doesn't have a
    default name for the first worker node group, since the default is
    only set at the cli level and not at the api server level. So when
    retrieving the cluster object from the kube api server, the name
    is empty.
    
    This is just a quick fix for this specific bug. We should find a more
    robust way of implementing these defaults (probably with a mutation
    webhook) so we don't need to implement the same logic again and again.

---
+++ pkg/cluster/node_groups_test.go
@@ -0,0 +1,132 @@
+package cluster_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/aws/eks-anywhere/internal/test"
+	anywherev1 "github.com/aws/eks-anywhere/pkg/api/v1alpha1"
+	"github.com/aws/eks-anywhere/pkg/cluster"
+)
+
+func TestNodeGroupsToDelete(t *testing.T) {
+	tests := []struct {
+		name         string
+		new, current *cluster.Spec
+		want         []anywherev1.WorkerNodeGroupConfiguration
+	}{
+		{
+			name: "one worker node group, missing name, no changes",
+			current: test.NewClusterSpec(func(s *cluster.Spec) {
+				s.Spec.WorkerNodeGroupConfigurations = []anywherev1.WorkerNodeGroupConfiguration{
+					{
+						MachineGroupRef: &anywherev1.Ref{
+							Kind: anywherev1.VSphereMachineConfigKind,
+							Name: "machine-config-1",
+						},
+					},
+				}
+			}),
+			new: test.NewClusterSpec(func(s *cluster.Spec) {
+				s.Spec.WorkerNodeGroupConfigurations = []anywherev1.WorkerNodeGroupConfiguration{
+					{
+						Name: "md-0",
+						MachineGroupRef: &anywherev1.Ref{
+							Kind: anywherev1.VSphereMachineConfigKind,
+							Name: "machine-config-1",
+						},
+					},
+				}
+			}),
+			want: []anywherev1.WorkerNodeGroupConfiguration{},
+		},
+		{
+			name: "one worker node group, missing name, new name is not default",
+			current: test.NewClusterSpec(func(s *cluster.Spec) {
+				s.Spec.WorkerNodeGroupConfigurations = []anywherev1.WorkerNodeGroupConfiguration{
+					{
+						MachineGroupRef: &anywherev1.Ref{
+							Kind: anywherev1.VSphereMachineConfigKind,
+							Name: "machine-config-1",
+						},
+					},
+				}
+			}),
+			new: test.NewClusterSpec(func(s *cluster.Spec) {
+				s.Spec.WorkerNodeGroupConfigurations = []anywherev1.WorkerNodeGroupConfiguration{
+					{
+						Name: "worker-node-group-0",
+						MachineGroupRef: &anywherev1.Ref{
+							Kind: anywherev1.VSphereMachineConfigKind,
+							Name: "machine-config-1",
+						},
+					},
+				}
+			}),
+			want: []anywherev1.WorkerNodeGroupConfiguration{
+				{
+					Name: "md-0",
+					MachineGroupRef: &anywherev1.Ref{
+						Kind: anywherev1.VSphereMachineConfigKind,
+						Name: "machine-config-1",
+					},
+				},
+			},
+		},
+		{
+			name: "new added, some removed, some stay",
+			current: test.NewClusterSpec(func(s *cluster.Spec) {
+				s.Spec.WorkerNodeGroupConfigurations = []anywherev1.WorkerNodeGroupConfiguration{
+					{
+						Name: "worker-node-group-0",
+						MachineGroupRef: &anywherev1.Ref{
+							Kind: anywherev1.VSphereMachineConfigKind,
+							Name: "machine-config-1",
+						},
+					},
+					{
+						Name: "worker-node-group-1",
+						MachineGroupRef: &anywherev1.Ref{
+							Kind: anywherev1.VSphereMachineConfigKind,
+							Name: "machine-config-1",
+						},
+					},
+				}
+			}),
+			new: test.NewClusterSpec(func(s *cluster.Spec) {
+				s.Spec.WorkerNodeGroupConfigurations = []anywherev1.WorkerNodeGroupConfiguration{
+					{
+						Name: "worker-node-group-0",
+						MachineGroupRef: &anywherev1.Ref{
+							Kind: anywherev1.VSphereMachineConfigKind,
+							Name: "machine-config-1",
+						},
+					},
+					{
+						Name: "worker-node-group-2",
+						MachineGroupRef: &anywherev1.Ref{
+							Kind: anywherev1.VSphereMachineConfigKind,
+							Name: "machine-config-1",
+						},
+					},
+				}
+			}),
+			want: []anywherev1.WorkerNodeGroupConfiguration{
+				{
+					Name: "worker-node-group-1",
+					MachineGroupRef: &anywherev1.Ref{
+						Kind: anywherev1.VSphereMachineConfigKind,
+						Name: "machine-config-1",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := cluster.NodeGroupsToDelete(tt.current, tt.new); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NodeGroupsToDelete() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
