e0f959b5f4382ba53d992689a44336cd29a48720
Abhay Krishna Arunachalam <arnchlm@amazon.com>
2022-03-01 13:21:10 -0800

    Refactor artifact table generation for release assets

--- release/pkg/test/files.go
+++ release/pkg/test/files.go
@@ -1,3 +1,17 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package test
 
 import (
@@ -5,7 +19,7 @@ import (
 	"os/exec"
 	"testing"
 
-	"github.com/aws/eks-anywhere/release/pkg/utils"
+	commandutils "github.com/aws/eks-anywhere/release/pkg/util/command"
 )
 
 func CheckFilesEquals(t *testing.T, actualPath, expectedPath string, update bool) {
@@ -29,7 +43,7 @@ func CheckFilesEquals(t *testing.T, actualPath, expectedPath string, update bool
 
 	if actualContent != expectedContent {
 		diffCmd := exec.Command("diff", expectedPath, actualPath)
-		diff, err := utils.ExecCommand(diffCmd)
+		diff, err := commandutils.ExecCommand(diffCmd)
 		if err != nil {
 			if exitError, ok := err.(*exec.ExitError); ok {
 				if exitError.ExitCode() == 1 {
