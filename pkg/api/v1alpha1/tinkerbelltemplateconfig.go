acf2e844a0ffd2adb5263fc939b202f0da97d8cc
Aravind Ramalingam <ramaliar@amazon.com>
2022-02-28 21:02:56 -0800

    Adding TinkerbellTemplateConfig CRD type

---
+++ pkg/api/v1alpha1/tinkerbelltemplateconfig.go
@@ -0,0 +1,84 @@
+package v1alpha1
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/yaml"
+
+	"github.com/aws/eks-anywhere/pkg/api/v1alpha1/thirdparty/tinkerbell"
+)
+
+const TinkerbellTemplateConfigKind = "TinkerbellTemplateConfig"
+
+func NewTinkerbellTemplateConfigGenerate(name string, opts ...ActionOpt) *TinkerbellTemplateConfigGenerate {
+	config := &TinkerbellTemplateConfigGenerate{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       TinkerbellTemplateConfigKind,
+			APIVersion: SchemeBuilder.GroupVersion.String(),
+		},
+		ObjectMeta: ObjectMeta{
+			Name: name,
+		},
+		Spec: TinkerbellTemplateConfigSpec{
+			Template: tinkerbell.Workflow{
+				Version:       "0.1",
+				Name:          name,
+				GlobalTimeout: 6000,
+				Tasks: []tinkerbell.Task{{
+					Name:       name,
+					WorkerAddr: "{{.device_1}}",
+					Volumes: []string{
+						"/dev:/dev",
+						"/dev/console:/dev/console",
+						"/lib/firmware:/lib/firmware:ro",
+					},
+				}},
+			},
+		},
+	}
+	for _, opt := range opts {
+		opt(&config.Spec.Template.Tasks[0].Actions)
+	}
+
+	return config
+}
+
+func (c *TinkerbellTemplateConfigGenerate) APIVersion() string {
+	return c.TypeMeta.APIVersion
+}
+
+func (c *TinkerbellTemplateConfigGenerate) Kind() string {
+	return c.TypeMeta.Kind
+}
+
+func (c *TinkerbellTemplateConfigGenerate) Name() string {
+	return c.ObjectMeta.Name
+}
+
+func GetTinkerbellTemplateConfig(fileName string) (map[string]*TinkerbellTemplateConfig, error) {
+	templates := make(map[string]*TinkerbellTemplateConfig)
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read file due to: %v", err)
+	}
+	for _, c := range strings.Split(string(content), YamlSeparator) {
+		var template TinkerbellTemplateConfig
+		if err := yaml.Unmarshal([]byte(c), &template); err != nil {
+			return nil, fmt.Errorf("unable to unmarshall content from file due to: %v", err)
+		}
+
+		if template.Kind() == template.ExpectedKind() {
+			if err = yaml.UnmarshalStrict([]byte(c), &template); err == nil {
+				templates[template.Name] = &template
+				continue
+			}
+		}
+	}
+	if len(templates) == 0 {
+		return nil, fmt.Errorf("unable to find kind %v in file", TinkerbellTemplateConfigKind)
+	}
+	return templates, nil
+}
