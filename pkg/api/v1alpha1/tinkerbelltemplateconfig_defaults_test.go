262a31b7b5d0742be7f009f5ed5af79d802b6bc3
Aravind Ramalingam <ramaliar@amazon.com>
2022-03-02 02:51:37 -0800

    Adding unit tests for tinkerbelltemplateconfig

--- pkg/api/v1alpha1/tinkerbelltemplateconfig_defaults_test.go
+++ pkg/api/v1alpha1/tinkerbelltemplateconfig_defaults_test.go
@@ -1,74 +1,28 @@
 package v1alpha1
 
 import (
+	"reflect"
+	"testing"
+
 	"github.com/aws/eks-anywhere/pkg/api/v1alpha1/thirdparty/tinkerbell"
 	"github.com/aws/eks-anywhere/release/api/v1alpha1"
 )
 
-const (
-	netplan = `network:
-  version: 2
-  renderer: networkd
-  ethernets:
-      eno1:
-          dhcp4: true
-      eno2:
-          dhcp4: true
-      eno3:
-          dhcp4: true
-      eno4:
-          dhcp4: true
-`
-	cloudInit = `datasource:
-  Ec2:
-    metadata_urls: ["http://TINKERBELL_IP:50061"]
-    strict_id: false
-system_info:
-  default_user:
-    name: tink
-    groups: [wheel, adm]
-    sudo: ["ALL=(ALL) NOPASSWD:ALL"]
-    shell: /bin/bash
-manage_etc_hosts: localhost
-warnings:
-  dsid_missing_source: off
-`
-)
-
-// +kubebuilder:object:generate=false
-type ActionOpt func(action *[]tinkerbell.Action)
-
-func WithDefaultActionsFromBundle(b v1alpha1.VersionsBundle) []ActionOpt {
-	return []ActionOpt{
-		withStreamImageAction(b),
-		withInstallOpensslAction(b),
-		withNetplanAction(b),
-		withTinkCloudInitAction(b),
-		withDsCloudInitAction(b),
-		withKexecAction(b),
-	}
-}
-
-func withStreamImageAction(b v1alpha1.VersionsBundle) ActionOpt {
-	return func(a *[]tinkerbell.Action) {
-		*a = append(*a, tinkerbell.Action{
+func TestWithDefaultActionsFromBundle(t *testing.T) {
+	vBundle := givenVersionBundle()
+	givenActions := []tinkerbell.Action{}
+	wantActions := []tinkerbell.Action{
+		{
 			Name:    "stream-image",
 			Image:   "image2disk:v1.0.0",
 			Timeout: 360,
 			Environment: map[string]string{
-				"IMG_URL":    b.EksD.Raw.Ubuntu.URI,
+				"IMG_URL":    "http://tinkerbell-example:8080/ubuntu-2004-kube-v1.21.5.gz",
 				"DEST_DISK":  "/dev/sda",
 				"COMPRESSED": "true",
 			},
-		})
-	}
-}
-
-// TODO: The below functions currently dont use the bundle. Update functions to pull action images from bundle.
-
-func withInstallOpensslAction(b v1alpha1.VersionsBundle) ActionOpt {
-	return func(a *[]tinkerbell.Action) {
-		*a = append(*a, tinkerbell.Action{
+		},
+		{
 			Name:    "install-openssl",
 			Image:   "cexec:v1.0.0",
 			Timeout: 90,
@@ -79,13 +33,8 @@ func withInstallOpensslAction(b v1alpha1.VersionsBundle) ActionOpt {
 				"DEFAULT_INTERPRETER": "/bin/sh -c",
 				"CMD_LINE":            "apt -y update && apt -y install openssl",
 			},
-		})
-	}
-}
-
-func withNetplanAction(b v1alpha1.VersionsBundle) ActionOpt {
-	return func(a *[]tinkerbell.Action) {
-		*a = append(*a, tinkerbell.Action{
+		},
+		{
 			Name:    "write-netplan",
 			Image:   "writefile:v1.0.0",
 			Timeout: 90,
@@ -99,13 +48,8 @@ func withNetplanAction(b v1alpha1.VersionsBundle) ActionOpt {
 				"MODE":      "0644",
 				"DIRMODE":   "0755",
 			},
-		})
-	}
-}
-
-func withTinkCloudInitAction(b v1alpha1.VersionsBundle) ActionOpt {
-	return func(a *[]tinkerbell.Action) {
-		*a = append(*a, tinkerbell.Action{
+		},
+		{
 			Name:    "add-tink-cloud-init-config",
 			Image:   "writefile:v1.0.0",
 			Timeout: 90,
@@ -119,13 +63,8 @@ func withTinkCloudInitAction(b v1alpha1.VersionsBundle) ActionOpt {
 				"MODE":      "0600",
 				"DIRMODE":   "0700",
 			},
-		})
-	}
-}
-
-func withDsCloudInitAction(b v1alpha1.VersionsBundle) ActionOpt {
-	return func(a *[]tinkerbell.Action) {
-		*a = append(*a, tinkerbell.Action{
+		},
+		{
 			Name:    "add-tink-cloud-init-ds-config",
 			Image:   "writefile:v1.0.0",
 			Timeout: 90,
@@ -139,13 +78,8 @@ func withDsCloudInitAction(b v1alpha1.VersionsBundle) ActionOpt {
 				"MODE":      "0600",
 				"DIRMODE":   "0700",
 			},
-		})
-	}
-}
-
-func withKexecAction(b v1alpha1.VersionsBundle) ActionOpt {
-	return func(a *[]tinkerbell.Action) {
-		*a = append(*a, tinkerbell.Action{
+		},
+		{
 			Name:    "kexec-image",
 			Image:   "kexec:v1.0.0",
 			Timeout: 90,
@@ -154,6 +88,27 @@ func withKexecAction(b v1alpha1.VersionsBundle) ActionOpt {
 				"BLOCK_DEVICE": "/dev/sda1",
 				"FS_TYPE":      "ext4",
 			},
-		})
+		},
 	}
+
+	opts := WithDefaultActionsFromBundle(vBundle)
+	for _, opt := range opts {
+		opt(&givenActions)
+	}
+
+	if !reflect.DeepEqual(givenActions, wantActions) {
+		t.Fatalf("Got default actions = %v, want %v", givenActions, wantActions)
+	}
+}
+
+func givenVersionBundle() v1alpha1.VersionsBundle {
+	return v1alpha1.VersionsBundle{EksD: v1alpha1.EksDRelease{
+		Raw: v1alpha1.OSImageBundle{
+			Ubuntu: v1alpha1.OSImage{
+				Archive: v1alpha1.Archive{
+					URI: "http://tinkerbell-example:8080/ubuntu-2004-kube-v1.21.5.gz",
+				},
+			},
+		},
+	}}
 }
