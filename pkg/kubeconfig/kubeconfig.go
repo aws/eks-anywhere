d699002d1463d08b94659747031e4970dcfea809
Chris Doherty <chris.doherty4@gmail.com>
2022-03-07 12:09:56 -0500

    Define kubeconfig env var in kubeconfig package (#1389)

--- pkg/kubeconfig/kubeconfig.go
+++ pkg/kubeconfig/kubeconfig.go
@@ -9,6 +9,10 @@ import (
 // FromClusterFormat defines the format of the kubeconfig of the
 const FromClusterFormat = "%s-eks-a-cluster.kubeconfig"
 
+// EnvName is the standard KubeConfig environment variable name.
+// https://kubernetes.io/docs/tasks/access-application-cluster/configure-access-multiple-clusters/#set-the-kubeconfig-environment-variable
+const EnvName = "KUBECONFIG"
+
 // FromClusterName formats an expected Kubeconfig path for EKS-A clusters. This includes a subdirecftory
 // named after the cluster name. For example, if the clusterName is 'sandbox' the generated path would be
 // sandbox/sandbox-eks-a-cluster.kubeconfig
@@ -16,8 +20,9 @@ func FromClusterName(clusterName string) string {
 	return filepath.Join(clusterName, fmt.Sprintf(FromClusterFormat, clusterName))
 }
 
-func FromEnvironment(envVariable string) string {
-	return os.Getenv(envVariable)
+// FromEnvironment reads the KubeConfig path from the standard KUBECONFIG environment variable.
+func FromEnvironment() string {
+	return os.Getenv(EnvName)
 }
 
 type missingFileError struct {
