1df7f79bec15943752fc643e695c6143d78a2650
Abdullahi Abdinur <acool@amazon.com>
2022-03-15 16:19:50 -0700

    Request kubeVersion from client

--- pkg/curatedpackages/bundle.go
+++ pkg/curatedpackages/bundle.go
@@ -10,14 +10,28 @@ import (
 	"github.com/aws/eks-anywhere/pkg/constants"
 	"github.com/aws/eks-anywhere/pkg/executables"
 	"github.com/go-logr/logr"
+	"strings"
 )
 
-const RegistryBaseRef = "public.ecr.aws/q0f6t3x4/eksa-package-bundles"
+const (
+	FromCluster     = "cluster"
+	FromRegistry    = "registry"
+	RegistryBaseRef = "public.ecr.aws/q0f6t3x4/eksa-package-bundles"
+)
 
-func GetLatestBundle(ctx context.Context, kubeConfig string) (*api.PackageBundle, error) {
-	packageBundle, err := GetLatestBundleFromCluster(ctx, kubeConfig)
-	if err != nil {
-		packageBundle, err = GetLatestBundleFromRegistry(ctx)
+func GetLatestBundle(ctx context.Context, kubeConfig string, location string, kubeVersion string) (*api.PackageBundle, error) {
+	var (
+		packageBundle *api.PackageBundle
+		err           error
+	)
+
+	switch strings.ToLower(location) {
+	case FromCluster:
+		packageBundle, err = getLatestBundleFromCluster(ctx, kubeConfig)
+	case FromRegistry:
+		packageBundle, err = getLatestBundleFromRegistry(ctx, kubeVersion)
+	default:
+		return nil, fmt.Errorf("unsupported location, Please use the options: %v, or %v", FromCluster, FromRegistry)
 	}
 
 	if err != nil {
@@ -26,21 +40,23 @@ func GetLatestBundle(ctx context.Context, kubeConfig string) (*api.PackageBundle
 	return packageBundle, nil
 }
 
-func createBundleManager() (manager bundle.Manager) {
+func createBundleManager(kubeVersion string) (manager bundle.Manager) {
+	versionSplit := strings.Split(kubeVersion, ".")
+	major, minor := versionSplit[0], versionSplit[1]
 	log := logr.Discard()
-	discovery := testutil.NewFakeDiscoveryWithDefaults() // TODO: Implement get kube version
+	discovery := testutil.NewFakeDiscovery(major, minor)
 	puller := artifacts.NewRegistryPuller()
 	bm := bundle.NewBundleManager(log, discovery, puller)
 	return bm
 }
 
-func GetLatestBundleFromRegistry(ctx context.Context) (*api.PackageBundle, error) {
-	bm := createBundleManager()
+func getLatestBundleFromRegistry(ctx context.Context, kubeVersion string) (*api.PackageBundle, error) {
+	bm := createBundleManager(kubeVersion)
 	bundle, err := bm.LatestBundle(ctx, RegistryBaseRef)
 	return bundle, err
 }
 
-func GetLatestBundleFromCluster(ctx context.Context, kubeConfig string) (*api.PackageBundle, error) {
+func getLatestBundleFromCluster(ctx context.Context, kubeConfig string) (*api.PackageBundle, error) {
 	params := []executables.KubectlOpt{executables.WithKubeconfig(kubeConfig), executables.WithNamespace(constants.EksaPackagesName)}
 	deps, err := createKubectl(ctx)
 	if err != nil {
