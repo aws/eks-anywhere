718cb3c76b1d353c7683a3c2bc2a70261436e290
Abdullahi Abdinur <acool@amazon.com>
2022-03-15 23:22:53 -0700

    Beautify Display

--- pkg/curatedpackages/package.go
+++ pkg/curatedpackages/package.go
@@ -2,17 +2,34 @@ package curatedpackages
 
 import (
 	"fmt"
+	"os"
 	"strings"
+
+	"text/tabwriter"
 
 	api "github.com/aws/eks-anywhere-packages/api/v1alpha1"
 )
 
+const (
+	MinWidth = 16
+	TabWidth = 8
+	Padding  = 0
+	PadChar  = '\t'
+	flags    = 0
+)
+
 func DisplayPackages(m map[string][]string) {
+	// initialize tabwriter
+	w := new(tabwriter.Writer)
+
+	w.Init(os.Stdout, MinWidth, TabWidth, Padding, PadChar, flags)
+	defer w.Flush()
+
+	fmt.Fprintf(w, "\n %s\t%s\t", "Package", "Version(s)")
+	fmt.Fprintf(w, "\n %s\t%s\t", "----", "----")
 	fmt.Println("Package", "Version(s)")
 	for key, values := range m {
-		fmt.Print(key)
-		fmt.Print(strings.Join(values, ","))
-		fmt.Println()
+		fmt.Fprintf(w, "\n %s\t%s\t", key, strings.Join(values, ","))
 	}
 }
 
