0c1e620167370b1acc14a88d37568607bff00820
Guillermo Gaston <gaslor@amazon.com>
2022-04-06 18:35:01 +0000

    Make artifacts download tests more robust
    Before this change, the code was depending on bundles.Charts, which calls
    Bundles.Charts, which return a map. This is made the order non
    predictable, hence making any tests depending on it, flaky.
    
    This change has the added benefit that it makes the tests independent
    from the number of charts stored in the bundles manifest. Hence we can
    keep adding more info in the VersionsBundle without having to update
    unrelated unit tests every time.

--- cmd/eksctl-anywhere/cmd/internal/commands/artifacts/download.go
+++ cmd/eksctl-anywhere/cmd/internal/commands/artifacts/download.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/aws/eks-anywhere/pkg/bundles"
 	"github.com/aws/eks-anywhere/pkg/logger"
 	"github.com/aws/eks-anywhere/pkg/version"
 	releasev1 "github.com/aws/eks-anywhere/release/api/v1alpha1"
@@ -14,6 +13,7 @@ import (
 type Reader interface {
 	ReadBundlesForVersion(eksaVersion string) (*releasev1.Bundles, error)
 	ReadImagesFromBundles(bundles *releasev1.Bundles) ([]releasev1.Image, error)
+	ReadChartsFromBundles(bundles *releasev1.Bundles) []releasev1.Image
 }
 
 type ImageMover interface {
@@ -62,7 +62,7 @@ func (d Download) Run(ctx context.Context) error {
 		return err
 	}
 
-	charts := bundles.Charts(b)
+	charts := d.Reader.ReadChartsFromBundles(b)
 
 	if err := d.ChartDownloader.Download(ctx, artifactNames(charts)...); err != nil {
 		return err
