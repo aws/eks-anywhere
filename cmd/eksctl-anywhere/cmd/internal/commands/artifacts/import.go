181515e41c65ea384d8e2c8b7e2f7ddbc01baee9
Guillermo Gaston <gaslor@amazon.com>
2022-04-06 20:19:05 +0000

    Import images command
    This command complements `download images`, taking the same tarbal it
    outputs as input.
    
    The eks-a tools image needs to be imported separetly since it's needed
    in order to initialize the helm executable, whic is needed to push the
    helm charts to the registry.

---
+++ cmd/eksctl-anywhere/cmd/internal/commands/artifacts/import.go
@@ -0,0 +1,44 @@
+package artifacts
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	releasev1 "github.com/aws/eks-anywhere/release/api/v1alpha1"
+)
+
+type Import struct {
+	Reader             Reader
+	Bundles            *releasev1.Bundles
+	ImageMover         ImageMover
+	ChartImporter      ChartImporter
+	TmpArtifactsFolder string
+}
+
+type ChartImporter interface {
+	Import(ctx context.Context, charts ...string) error
+}
+
+func (i Import) Run(ctx context.Context) error {
+	images, err := i.Reader.ReadImagesFromBundles(i.Bundles)
+	if err != nil {
+		return fmt.Errorf("downloading images: %v", err)
+	}
+
+	if err = i.ImageMover.Move(ctx, artifactNames(images)...); err != nil {
+		return err
+	}
+
+	charts := i.Reader.ReadChartsFromBundles(i.Bundles)
+
+	if err := i.ChartImporter.Import(ctx, artifactNames(charts)...); err != nil {
+		return err
+	}
+
+	if err := os.RemoveAll(i.TmpArtifactsFolder); err != nil {
+		return fmt.Errorf("deleting tmp artifact import folder: %v", err)
+	}
+
+	return nil
+}
