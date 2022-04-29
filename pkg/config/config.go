26c496f9a81d62590ec1f739c33a7c37da1317c0
danbudris <budris@amazon.com>
2022-04-29 14:36:09 -0400

    add ssh known hosts file env var

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,9 +3,11 @@ package config
 const (
 	EksaGitPasswordTokenEnv   = "EKSA_GIT_PASSWORD"
 	EksaGitPrivateKeyTokenEnv = "EKSA_GIT_PRIVATE_KEY"
+	EksaGitKnownHostsFileEnv  = "EKSA_GIT_KNOWN_HOSTS"
 )
 
 type CliConfig struct {
 	GitPassword       string
 	GitPrivateKeyFile string
+	GitKnownHostsFile string
 }
