c6a7192e92db09c28eda8d2932bcec8c67564d11
danbudris <budris@amazon.com>
2022-04-29 20:24:09 -0400

    upgrade: test fetcher for FluxConfig

--- pkg/cluster/fetch_test.go
+++ pkg/cluster/fetch_test.go
@@ -32,3 +32,85 @@ func TestGetBundlesForCluster(t *testing.T) {
 	g.Expect(err).To(BeNil())
 	g.Expect(gotBundles).To(Equal(wantBundles))
 }
+
+func TestGetFluxConfigForClusterIsNil(t *testing.T) {
+	g := NewWithT(t)
+	c := &v1alpha1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "eksa-cluster",
+			Namespace: "eksa",
+		},
+		Spec: v1alpha1.ClusterSpec{
+			GitOpsRef: nil,
+		},
+	}
+	var wantFlux *v1alpha1.FluxConfig
+	mockFetch := func(ctx context.Context, name, namespace string) (*v1alpha1.FluxConfig, error) {
+		g.Expect(name).To(Equal(c.Name))
+		g.Expect(namespace).To(Equal(c.Namespace))
+
+		return wantFlux, nil
+	}
+
+	gotFlux, err := cluster.GetFluxConfigForCluster(context.Background(), c, mockFetch)
+	g.Expect(err).To(BeNil())
+	g.Expect(gotFlux).To(Equal(wantFlux))
+}
+
+func TestGetFluxConfigForCluster(t *testing.T) {
+	g := NewWithT(t)
+	c := &v1alpha1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "eksa-cluster",
+			Namespace: "eksa",
+		},
+		Spec: v1alpha1.ClusterSpec{
+			GitOpsRef: &v1alpha1.Ref{
+				Kind: v1alpha1.FluxConfigKind,
+				Name: "eksa-cluster",
+			},
+		},
+	}
+	wantFlux := &v1alpha1.FluxConfig{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "",
+			APIVersion: "",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:                       "",
+			GenerateName:               "",
+			Namespace:                  "",
+			SelfLink:                   "",
+			UID:                        "",
+			ResourceVersion:            "",
+			Generation:                 0,
+			CreationTimestamp:          metav1.Time{},
+			DeletionTimestamp:          nil,
+			DeletionGracePeriodSeconds: nil,
+			Labels:                     nil,
+			Annotations:                nil,
+			OwnerReferences:            nil,
+			Finalizers:                 nil,
+			ClusterName:                "",
+			ManagedFields:              nil,
+		},
+		Spec: v1alpha1.FluxConfigSpec{
+			SystemNamespace:   "",
+			ClusterConfigPath: "",
+			Branch:            "",
+			Github:            nil,
+			Git:               nil,
+		},
+		Status: v1alpha1.FluxConfigStatus{},
+	}
+	mockFetch := func(ctx context.Context, name, namespace string) (*v1alpha1.FluxConfig, error) {
+		g.Expect(name).To(Equal(c.Name))
+		g.Expect(namespace).To(Equal(c.Namespace))
+
+		return wantFlux, nil
+	}
+
+	gotFlux, err := cluster.GetFluxConfigForCluster(context.Background(), c, mockFetch)
+	g.Expect(err).To(BeNil())
+	g.Expect(gotFlux).To(Equal(wantFlux))
+}
