b3a1965a771ce2adaba7d1300deb64ba0ecea0e7
Taylor Neyland <tneyla@amazon.com>
2022-05-04 08:33:20 -0500

    Set SSH_KNOWN_HOSTS to same value as EKSA_GIT_KNOWN_HOSTS

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,6 +4,7 @@ const (
 	EksaGitPasswordTokenEnv   = "EKSA_GIT_PASSWORD"
 	EksaGitPrivateKeyTokenEnv = "EKSA_GIT_PRIVATE_KEY"
 	EksaGitKnownHostsFileEnv  = "EKSA_GIT_KNOWN_HOSTS"
+	SshKnownHostsEnv          = "SSH_KNOWN_HOSTS"
 )
 
 type CliConfig struct {
