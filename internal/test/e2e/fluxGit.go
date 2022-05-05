bef76814299be3e00a10fe26d0a84ba80498325c
Guillermo Gaston <gaslor@amazon.com>
2022-05-05 16:37:32 +0000

    Download git flux files from s3 into test instance

--- internal/test/e2e/fluxGit.go
+++ internal/test/e2e/fluxGit.go
@@ -5,18 +5,16 @@ import (
 	"os"
 	"regexp"
 
+	"github.com/aws/eks-anywhere/internal/pkg/ssm"
 	"github.com/aws/eks-anywhere/pkg/config"
 	"github.com/aws/eks-anywhere/pkg/logger"
 	e2etests "github.com/aws/eks-anywhere/test/framework"
 )
 
-const knownHostsContent = `
-# github.com:22 SSH-2.0-babeld-e301d840
-github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
-# github.com:22 SSH-2.0-babeld-e301d840
-github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
-# github.com:22 SSH-2.0-babeld-e301d840
-github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl`
+type s3Files struct {
+	key, dstPath string
+	permission   int
+}
 
 func (e *E2ESession) setupFluxGitEnv(testRegex string) error {
 	re := regexp.MustCompile(`^.*GitFlux.*$`)
@@ -32,16 +30,38 @@ func (e *E2ESession) setupFluxGitEnv(testRegex string) error {
 		}
 	}
 
-	err := e.writeStringToFile(knownHostsContent, e.testEnvVars[config.EksaGitKnownHostsFileEnv])
-	if err != nil {
-		return fmt.Errorf("creating Flux Git known hosts file: %v", err)
+	for _, file := range buildFluxGitFiles(e.testEnvVars) {
+		if err := e.downloadFileInInstance(file); err != nil {
+			return fmt.Errorf("downloading flux-git file to instance: %v", err)
+		}
 	}
 
-	k := os.Getenv(e2etests.GitPrivateKeyContent)
-	err = e.writeStringToFile(k, e.testEnvVars[config.EksaGitPrivateKeyTokenEnv])
-	if err != nil {
-		return fmt.Errorf("creating Flux Git private key file: %v", err)
+	return nil
+}
+
+func buildFluxGitFiles(envVars map[string]string) []s3Files {
+	return []s3Files{
+		{
+			key:        "git-flux/known_hosts",
+			dstPath:    envVars[config.EksaGitKnownHostsFileEnv],
+			permission: 600,
+		},
+		{
+			key:        "git-flux/private-key",
+			dstPath:    envVars[config.EksaGitPrivateKeyTokenEnv],
+			permission: 600,
+		},
 	}
+}
+
+func (e *E2ESession) downloadFileInInstance(file s3Files) error {
+	logger.V(1).Info("Downloading from s3 in instance", "file", file.key)
+
+	command := fmt.Sprintf("aws s3 cp s3://%s/%s %s && chmod %d %[3]s", e.storageBucket, file.key, file.dstPath, file.permission)
+	if err := ssm.Run(e.session, e.instanceId, command); err != nil {
+		return fmt.Errorf("downloading file in instance: %v", err)
+	}
+	logger.V(1).Info("Successfully downloaded file", "file", file.key)
 
 	return nil
 }
