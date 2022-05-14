36415159f269698cde08c34720a00c1fd77dd725
Chris Doherty <chris.doherty4@gmail.com>
2022-05-13 21:12:51 -0400

    Add labels to Tinkerbell hardware CSV
    Labels allow customers to label the Hardware resource so they can target
    it for scheduling provisioning onto nodes.

--- pkg/providers/tinkerbell/hardware/validator_test.go
+++ pkg/providers/tinkerbell/hardware/validator_test.go
@@ -150,6 +150,101 @@ func TestUniquenessAssertionsWithDupes(t *testing.T) {
 	}
 }
 
+func TestStaticMachineAssertions_ValidMachine(t *testing.T) {
+	g := gomega.NewWithT(t)
+
+	machine := hardware.Machine{
+		ID:           "unique string",
+		IPAddress:    "10.10.10.10",
+		Gateway:      "10.10.10.1",
+		Nameservers:  []string{"nameserver1"},
+		Netmask:      "255.255.255.255",
+		MACAddress:   "00:00:00:00:00:00",
+		Hostname:     "localhost",
+		Labels:       make(hardware.Labels),
+		BMCIPAddress: "10.10.10.11",
+		BMCUsername:  "username",
+		BMCPassword:  "password",
+		BMCVendor:    "dell",
+	}
+
+	validate := hardware.StaticMachineAssertions()
+	g.Expect(validate(machine)).ToNot(gomega.HaveOccurred())
+}
+
+func TestStaticMachineAssertions_InvalidMachines(t *testing.T) {
+	g := gomega.NewWithT(t)
+
+	cases := map[string]func(*hardware.Machine){
+		"EmptyID": func(h *hardware.Machine) {
+			h.ID = ""
+		},
+		"EmptyIPAddress": func(h *hardware.Machine) {
+			h.IPAddress = ""
+		},
+		"InvalidIPAddress": func(h *hardware.Machine) {
+			h.IPAddress = "invalid"
+		},
+		"EmptyGateway": func(h *hardware.Machine) {
+			h.Gateway = ""
+		},
+		"InvalidGateway": func(h *hardware.Machine) {
+			h.Gateway = "invalid"
+		},
+		"NoNameservers": func(h *hardware.Machine) {
+			h.Nameservers = []string{}
+		},
+		"EmptyNameserver": func(h *hardware.Machine) {
+			h.Nameservers = []string{""}
+		},
+		"EmptyNetmask": func(h *hardware.Machine) {
+			h.Netmask = ""
+		},
+		"EmptyMACAddress": func(h *hardware.Machine) {
+			h.MACAddress = ""
+		},
+		"InvalidMACAddress": func(h *hardware.Machine) {
+			h.MACAddress = "invalid mac"
+		},
+		"EmptyHostname": func(h *hardware.Machine) {
+			h.Hostname = ""
+		},
+		"InvalidHostname": func(h *hardware.Machine) {
+			h.Hostname = "!@#$%"
+		},
+		"EmptyBMCIPAddress": func(h *hardware.Machine) {
+			h.BMCIPAddress = ""
+		},
+		"InvalidBMCIPAddress": func(h *hardware.Machine) {
+			h.BMCIPAddress = "invalid"
+		},
+		"EmptyBMCUsername": func(h *hardware.Machine) {
+			h.BMCUsername = ""
+		},
+		"EmptyBMCPassword": func(h *hardware.Machine) {
+			h.BMCPassword = ""
+		},
+		"EmptyBMCVendor": func(h *hardware.Machine) {
+			h.BMCVendor = ""
+		},
+		"InvalidLabelKey": func(h *hardware.Machine) {
+			h.Labels["?$?$?"] = "foo"
+		},
+		"InvalidLabelValue": func(h *hardware.Machine) {
+			h.Labels["foo"] = "\\/dsa"
+		},
+	}
+
+	validate := hardware.StaticMachineAssertions()
+	for name, mutate := range cases {
+		t.Run(name, func(t *testing.T) {
+			machine := NewValidMachine()
+			mutate(&machine)
+			g.Expect(validate(machine)).To(gomega.HaveOccurred())
+		})
+	}
+}
+
 func NewValidMachine() hardware.Machine {
 	return hardware.Machine{
 		ID:           uuid.NewString(),
@@ -159,6 +254,7 @@ func NewValidMachine() hardware.Machine {
 		MACAddress:   "00:00:00:00:00:00",
 		Netmask:      "255.255.255.255",
 		Hostname:     "localhost",
+		Labels:       make(hardware.Labels),
 		BMCIPAddress: "10.10.10.11",
 		BMCUsername:  "username",
 		BMCPassword:  "password",
