a8a8419e5c224fe3702185ff4421f68766a4e54a
danbudris <budris@amazon.com>
2022-06-01 14:34:16 -0400

    auto init repos

--- pkg/git/gogithub/gogithub.go
+++ pkg/git/gogithub/gogithub.go
@@ -91,7 +91,13 @@ func (ggc *githubClient) AddDeployKeyToRepo(ctx context.Context, owner, repo str
 // file must be added to it via the github api before it can be successfully cloned.
 func (g *GoGithub) CreateRepo(ctx context.Context, opts git.CreateRepoOpts) (repository *git.Repository, err error) {
 	logger.V(3).Info("Attempting to create new Github repo", "repo", opts.Name, "owner", opts.Owner)
-	r := &goGithub.Repository{Name: &opts.Name, Private: &opts.Privacy, Description: &opts.Description}
+	autoInit := true
+	r := &goGithub.Repository{
+		Name:        &opts.Name,
+		Private:     &opts.Privacy,
+		Description: &opts.Description,
+		AutoInit:    &autoInit,
+	}
 
 	org := ""
 	if !opts.Personal {
