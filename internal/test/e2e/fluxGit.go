a51c7edab33c51ad26ca36f0cecebd87ba54fb7c
danbudris <budris@amazon.com>
2022-06-06 11:19:02 -0400

    update comment

--- internal/test/e2e/fluxGit.go
+++ internal/test/e2e/fluxGit.go
@@ -141,7 +141,7 @@ func (e *E2ESession) setupGithubRepo(repo string, envVars map[string]string) (*g
 		return nil, fmt.Errorf("creating repository in Github for test: %v", err)
 	}
 
-	// Wait for eventual consistency in the GitHub API when creating a repository
+	// Wait for eventual consistency in the GitHub API when creating a repository before adding a deploy key
 	time.Sleep(time.Second * 5)
 
 	pk, pub, err := e.generateKeyPairForGitTest()
