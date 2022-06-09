8db3b813b57a31a6aaff7d07a39856d885f6c4b3
Abdullahi Abdinur <acool@amazon.com>
2022-06-09 12:08:57 -0700

    Include unit test for GetRegistry

--- pkg/curatedpackages/curatedpackages_test.go
+++ pkg/curatedpackages/curatedpackages_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	. "github.com/onsi/gomega"
 
 	"github.com/aws/eks-anywhere/pkg/api/v1alpha1"
 	"github.com/aws/eks-anywhere/pkg/curatedpackages"
@@ -109,4 +110,20 @@ func TestGetVersionBundleFailsWhenBundleNil(t *testing.T) {
 	if err == nil {
 		t.Errorf("GetVersionBundle should fail when version bundle for kubeversion doesn't exist")
 	}
+}
+
+func TestGetRegistrySuccess(t *testing.T) {
+	g := NewWithT(t)
+	uri := "public.ecr.aws/l0g8r8j6/eks-anywhere-packages"
+	registry := curatedpackages.GetRegistry(uri)
+	expected := "public.ecr.aws/l0g8r8j6"
+	g.Expect(registry).To(Equal(expected))
+}
+
+func TestGetRegistryFail(t *testing.T) {
+	g := NewWithT(t)
+	uri := "public.ecr.aws"
+	registry := curatedpackages.GetRegistry(uri)
+	expected := "public.ecr.aws"
+	g.Expect(registry).To(Equal(expected))
 }
