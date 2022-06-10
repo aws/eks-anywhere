ceda3bea1dd23577b7529f7bc26244f914f483c4
danbudris <budris@amazon.com>
2022-06-10 10:20:08 -0400

    add retry to deploy key add

--- internal/test/e2e/fluxGit.go
+++ internal/test/e2e/fluxGit.go
@@ -3,17 +3,17 @@ package e2e
 import (
 	"context"
 	"fmt"
-	"log"
-	"os"
-	"regexp"
-	"time"
-
 	"github.com/aws/eks-anywhere/internal/pkg/ssm"
 	"github.com/aws/eks-anywhere/pkg/api/v1alpha1"
 	"github.com/aws/eks-anywhere/pkg/config"
 	"github.com/aws/eks-anywhere/pkg/git"
 	"github.com/aws/eks-anywhere/pkg/logger"
+	"github.com/aws/eks-anywhere/pkg/retrier"
 	e2etests "github.com/aws/eks-anywhere/test/framework"
+	"log"
+	"os"
+	"regexp"
+	"time"
 )
 
 type s3Files struct {
@@ -141,9 +141,6 @@ func (e *E2ESession) setupGithubRepo(repo string, envVars map[string]string) (*g
 		return nil, fmt.Errorf("creating repository in Github for test: %v", err)
 	}
 
-	// Wait for eventual consistency in the GitHub API when creating a repository before adding a deploy key
-	time.Sleep(time.Second * 5)
-
 	pk, pub, err := e.generateKeyPairForGitTest()
 	if err != nil {
 		return nil, fmt.Errorf("generating key pair for git tests: %v", err)
@@ -158,9 +155,16 @@ func (e *E2ESession) setupGithubRepo(repo string, envVars map[string]string) (*g
 		ReadOnly:   false,
 	}
 
-	err = g.AddDeployKeyToRepo(ctx, ko)
+	//Newly generated repositories may take some time to show up in the GitHub API; retry a few times to get around this
+	err = retrier.Retry(6, time.Second*10, func() error {
+		err = g.AddDeployKeyToRepo(ctx, ko)
+		if err != nil {
+			return fmt.Errorf("couldn't add deploy key to repo: %v", err)
+		}
+		return nil
+	})
 	if err != nil {
-		return r, fmt.Errorf("couldn't add deploy key to repo: %v", err)
+		return r, err
 	}
 
 	// Generate a PEM file from the private key and write it instance at the user-provided path
