2c4b9f52c0bfef1f4ea057ad8afeb380987c119c
Max Dribinsky <maxdrib@users.noreply.github.com>
2022-06-30 15:46:02 -0400

    Adding e2e test support for TestCloudStackKubernetes121RedhatProxyConfig (#2598)
    * Refactoring proxy tests to support hostnames as well as provider-specific proxy configs
    
    * Minor cleanup
    
    * Moving api cluster.go changes to separate PR and applying suggested changes
    
    * linting
    
    * Adding Cloudstack proxy env vars to test environment variables
    
    * Renaming vsphere proxy config env vars to have vsphere suffixes

--- test/e2e/proxy_test.go
+++ test/e2e/proxy_test.go
@@ -27,7 +27,7 @@ func TestVSphereKubernetes123UbuntuProxyConfig(t *testing.T) {
 		framework.WithClusterFiller(api.WithControlPlaneCount(1)),
 		framework.WithClusterFiller(api.WithWorkerNodeCount(1)),
 		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube123)),
-		framework.WithProxy(),
+		framework.WithProxy(framework.VsphereProxyRequiredEnvVars),
 		framework.WithEnvVar(features.K8s123SupportEnvVar, "true"),
 	)
 	runProxyConfigFlow(test)
@@ -42,7 +42,7 @@ func TestVSphereKubernetes123BottlerocketProxyConfig(t *testing.T) {
 		framework.WithClusterFiller(api.WithControlPlaneCount(1)),
 		framework.WithClusterFiller(api.WithWorkerNodeCount(1)),
 		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube123)),
-		framework.WithProxy(),
+		framework.WithProxy(framework.VsphereProxyRequiredEnvVars),
 		framework.WithEnvVar(features.K8s123SupportEnvVar, "true"),
 	)
 	runProxyConfigFlow(test)
@@ -56,7 +56,7 @@ func TestCloudStackKubernetes121RedhatProxyConfig(t *testing.T) {
 		framework.WithClusterFiller(api.WithControlPlaneCount(1)),
 		framework.WithClusterFiller(api.WithWorkerNodeCount(1)),
 		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube121)),
-		framework.WithProxy(),
+		framework.WithProxy(framework.CloudstackProxyRequiredEnvVars),
 	)
 	runProxyConfigFlow(test)
 }
@@ -68,7 +68,8 @@ func TestSnowKubernetes121UbuntuProxyConfig(t *testing.T) {
 		framework.WithClusterFiller(api.WithKubernetesVersion(v1alpha1.Kube121)),
 		framework.WithClusterFiller(api.WithControlPlaneCount(1)),
 		framework.WithClusterFiller(api.WithWorkerNodeCount(1)),
-		framework.WithProxy(),
+		// TODO: provide separate Proxy Env Vars for Snow provider. Leaving VSphere for backwards compatibility
+		framework.WithProxy(framework.VsphereProxyRequiredEnvVars),
 		framework.WithEnvVar("SNOW_PROVIDER", "true"),
 	)
 	runProxyConfigFlow(test)
