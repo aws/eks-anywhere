6f2215ffdf658d9b7b817ce21a0d896f7a3b80b5
Guillermo Gaston <gaslor@amazon.com>
2022-07-06 17:39:04 -0500

    Re-organize cluster controller code

---
+++ pkg/controller/clientutil/kubernetes_test.go
@@ -0,0 +1,47 @@
+package clientutil_test
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	_ "github.com/aws/eks-anywhere/internal/test/envtest"
+	anywherev1 "github.com/aws/eks-anywhere/pkg/api/v1alpha1"
+	"github.com/aws/eks-anywhere/pkg/controller/clientutil"
+)
+
+func TestKubeClientGet(t *testing.T) {
+	g := NewWithT(t)
+	ctx := context.Background()
+	cluster := &anywherev1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-cluster",
+			Namespace: "default",
+		},
+		TypeMeta: metav1.TypeMeta{
+			Kind:       anywherev1.ClusterKind,
+			APIVersion: anywherev1.GroupVersion.String(),
+		},
+	}
+	cb := fake.NewClientBuilder()
+	cl := cb.WithRuntimeObjects(cluster).Build()
+
+	client := clientutil.NewKubeClient(cl)
+	receiveCluster := &anywherev1.Cluster{}
+	g.Expect(client.Get(ctx, "my-cluster", "default", receiveCluster)).To(Succeed())
+	g.Expect(receiveCluster).To(Equal(cluster))
+}
+
+func TestKubeClientGetNotFound(t *testing.T) {
+	g := NewWithT(t)
+	ctx := context.Background()
+	cb := fake.NewClientBuilder()
+	cl := cb.WithRuntimeObjects().Build()
+
+	client := clientutil.NewKubeClient(cl)
+	receiveCluster := &anywherev1.Cluster{}
+	g.Expect(client.Get(ctx, "my-cluster", "default", receiveCluster)).Error()
+}
