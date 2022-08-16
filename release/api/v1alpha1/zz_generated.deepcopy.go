3a57832999121c3cfe05130ca16b599d959b67a1
jonahjon <jonahjones094@gmail.com>
2022-08-15 19:32:54 -0500

    updating crds
    Signed-off-by: jonahjon <jonahjones094@gmail.com>

--- release/api/v1alpha1/zz_generated.deepcopy.go
+++ release/api/v1alpha1/zz_generated.deepcopy.go
@@ -603,7 +603,7 @@ func (in *OSImageBundle) DeepCopy() *OSImageBundle {
 func (in *PackageBundle) DeepCopyInto(out *PackageBundle) {
 	*out = *in
 	in.Controller.DeepCopyInto(&out.Controller)
-	in.Refreshser.DeepCopyInto(&out.Refreshser)
+	in.TokenRefresher.DeepCopyInto(&out.TokenRefresher)
 	in.HelmChart.DeepCopyInto(&out.HelmChart)
 }
 
