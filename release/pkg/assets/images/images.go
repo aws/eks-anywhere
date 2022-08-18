6b5b2d99741c88adfd1e4f8226ca4bc5dab320b7
Abhay Krishna Arunachalam <arnchlm@amazon.com>
2022-08-18 14:04:13 -0700

    Construct full URI before checking existence

--- release/pkg/assets/images/images.go
+++ release/pkg/assets/images/images.go
@@ -65,7 +65,7 @@ func GetImageAssets(rc *releasetypes.ReleaseConfig, ac *assettypes.AssetConfig, 
 		}
 	}
 
-	sourceImageUri, sourcedFromBranch, err := images.GetSourceImageURI(rc, assetName, sourceRepoName, imageTagOptionsMap, image.ImageTagConfiguration)
+	sourceImageUri, sourcedFromBranch, err := images.GetSourceImageURI(rc, assetName, sourceRepoName, imageTagOptionsMap, image.ImageTagConfiguration, image.TrimVersionSignifier)
 	if err != nil {
 		return nil, "", errors.Cause(err)
 	}
@@ -77,16 +77,9 @@ func GetImageAssets(rc *releasetypes.ReleaseConfig, ac *assettypes.AssetConfig, 
 		imageTagOptionsMap["gitTag"] = gitTag
 	}
 
-	if image.TrimVersionSignifier {
-		sourceImageUri = strings.ReplaceAll(sourceImageUri, ":v", ":")
-	}
-
-	releaseImageUri, err := images.GetReleaseImageURI(rc, assetName, releaseRepoName, imageTagOptionsMap, image.ImageTagConfiguration)
+	releaseImageUri, err := images.GetReleaseImageURI(rc, assetName, releaseRepoName, imageTagOptionsMap, image.ImageTagConfiguration, image.TrimVersionSignifier)
 	if err != nil {
 		return nil, "", errors.Cause(err)
-	}
-	if image.TrimVersionSignifier {
-		releaseImageUri = strings.ReplaceAll(releaseImageUri, ":v", ":")
 	}
 
 	imageArtifact := &releasetypes.ImageArtifact{
