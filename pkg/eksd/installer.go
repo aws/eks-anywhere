518a1343424c3c218a2329064336c56e61eb76b1
Max Dribinsky <dribinm@amazon.com>
2022-08-23 10:15:51 -0400

    Exposing method to update retrier instead of making the variable public

--- pkg/eksd/installer.go
+++ pkg/eksd/installer.go
@@ -27,22 +27,21 @@ type Reader interface {
 
 type Installer struct {
 	client  EksdInstallerClient
-	// Exposing for unit testing purposes
-	Retrier *retrier.Retrier
+	retrier *retrier.Retrier
 	reader  Reader
 }
 
 func NewEksdInstaller(client EksdInstallerClient, reader Reader) *Installer {
 	return &Installer{
 		client:  client,
-		Retrier: retrier.NewWithMaxRetries(maxRetries, backOffPeriod),
+		retrier: retrier.NewWithMaxRetries(maxRetries, backOffPeriod),
 		reader:  reader,
 	}
 }
 
 func (i *Installer) InstallEksdCRDs(ctx context.Context, clusterSpec *cluster.Spec, cluster *types.Cluster) error {
 	var eksdCRDs []byte
-	if err := i.Retrier.Retry(
+	if err := i.retrier.Retry(
 		func() error {
 			var readerErr error
 			eksdCRDs, readerErr = i.reader.ReadFile(clusterSpec.VersionsBundle.EksD.Components)
@@ -52,7 +51,7 @@ func (i *Installer) InstallEksdCRDs(ctx context.Context, clusterSpec *cluster.Sp
 		return fmt.Errorf("loading manifest for eksd components: %v", err)
 	}
 
-	if err := i.Retrier.Retry(
+	if err := i.retrier.Retry(
 		func() error {
 			return i.client.ApplyKubeSpecFromBytesWithNamespace(ctx, cluster, eksdCRDs, constants.EksaSystemNamespace)
 		},
@@ -63,9 +62,14 @@ func (i *Installer) InstallEksdCRDs(ctx context.Context, clusterSpec *cluster.Sp
 	return nil
 }
 
+// SetRetrier Exposing retrier for unit testing purposes
+func (i *Installer) SetRetrier(retrier *retrier.Retrier) {
+	i.retrier = retrier
+}
+
 func (i *Installer) InstallEksdManifest(ctx context.Context, clusterSpec *cluster.Spec, cluster *types.Cluster) error {
 	var eksdReleaseManifest []byte
-	if err := i.Retrier.Retry(
+	if err := i.retrier.Retry(
 		func() error {
 			var readerErr error
 			eksdReleaseManifest, readerErr = i.reader.ReadFile(clusterSpec.VersionsBundle.EksD.EksDReleaseUrl)
@@ -76,7 +80,7 @@ func (i *Installer) InstallEksdManifest(ctx context.Context, clusterSpec *cluste
 	}
 
 	logger.V(4).Info("Applying eksd manifest to cluster")
-	if err := i.Retrier.Retry(
+	if err := i.retrier.Retry(
 		func() error {
 			return i.client.ApplyKubeSpecFromBytesWithNamespace(ctx, cluster, eksdReleaseManifest, constants.EksaSystemNamespace)
 		},
