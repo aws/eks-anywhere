ae4433da5fe4bf2506b58519169a66505dab3dff
Raymond Zhang <rayzhng@amazon.com>
2022-08-31 14:22:28 -0700

    grouping flags

--- cmd/eksctl-anywhere/cmd/flags.go
+++ cmd/eksctl-anywhere/cmd/flags.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+
+	"github.com/aws/eks-anywhere/pkg/validations"
 )
 
 func bindFlagsToViper(cmd *cobra.Command, args []string) error {
@@ -15,4 +19,40 @@ func bindFlagsToViper(cmd *cobra.Command, args []string) error {
 		err = viper.BindPFlag(flag.Name, flag)
 	})
 	return err
+}
+
+func setupClusterOptionFlags(cmd *cobra.Command, clusterOpt *clusterOptions) {
+	cmd.Flags().StringVarP(&clusterOpt.fileName, "filename", "f", "", "Filename that contains EKS-A cluster configuration")
+	cmd.Flags().StringVar(&clusterOpt.bundlesOverride, "bundles-override", "", "Override default Bundles manifest (not recommended)")
+	cmd.Flags().StringVar(&clusterOpt.managementKubeconfig, "kubeconfig", "", "Management cluster kubeconfig file")
+}
+
+func addTinkerbellFlag(cmd *cobra.Command, path *string) {
+	cmd.Flags().StringVarP(
+		path,
+		TinkerbellHardwareCSVFlagName,
+		TinkerbellHardwareCSVFlagAlias,
+		"",
+		TinkerbellHardwareCSVFlagDescription,
+	)
+}
+
+func checkTinkerbellFlags(cmd *cobra.Command, hardwareCSVPath string) error {
+	flag := cmd.Flags().Lookup(TinkerbellHardwareCSVFlagName)
+
+	// If no flag was returned there is a developer error as the flag has been removed
+	// from the program rendering it invalid.
+	if flag == nil {
+		panic("'hardwarefile' flag not configured")
+	}
+
+	if !viper.IsSet(TinkerbellHardwareCSVFlagName) || viper.GetString(TinkerbellHardwareCSVFlagName) == "" {
+		return fmt.Errorf("required flag \"%v\" not set", TinkerbellHardwareCSVFlagName)
+	}
+
+	if !validations.FileExists(hardwareCSVPath) {
+		return fmt.Errorf("hardware config file %s does not exist", hardwareCSVPath)
+	}
+
+	return nil
 }
