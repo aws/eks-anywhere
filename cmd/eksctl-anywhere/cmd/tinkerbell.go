b1dd4d23475c0546c54f65eb80febf6e204d8a99
Raymond Zhang <67798267+raymond-zhang00@users.noreply.github.com>
2022-09-01 14:48:15 -0500

    group clusterOptions flags (#3227)
    * grouping flags
    
    * Update cmd/eksctl-anywhere/cmd/upgradecluster.go
    
    Co-authored-by: Chris Doherty <chris.doherty4@gmail.com>
    
    * Update cmd/eksctl-anywhere/cmd/flags.go
    
    Co-authored-by: Chris Doherty <chris.doherty4@gmail.com>
    
    * renaming functions and using flagSet
    
    * updating applyTimeoutFlags to use flagSet
    
    Co-authored-by: Chris Doherty <chris.doherty4@gmail.com>

--- cmd/eksctl-anywhere/cmd/tinkerbell.go
+++
@@ -1,7 +0,0 @@
-package cmd
-
-const (
-	TinkerbellHardwareCSVFlagName        = "hardware-csv"
-	TinkerbellHardwareCSVFlagAlias       = "z"
-	TinkerbellHardwareCSVFlagDescription = "Path to a CSV file containing hardware data."
-)
