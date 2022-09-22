c19496bfc28a583df2c2fc72e3b00eadfdbb89f3
Vivek Koppuru <koppv@amazon.com>
2022-09-22 12:31:00 -0700

    Add vcenter session validate permission analyzer (#3419) (#3425)

--- pkg/diagnostics/analyzers.go
+++ pkg/diagnostics/analyzers.go
@@ -326,36 +326,66 @@ func (a *analyzerFactory) crdAnalyzer(crdName string) *Analyze {
 
 // vsphereDiagnosticAnalyzers will return diagnostic analyzers to analyze the condition of vSphere cluster
 func (a *analyzerFactory) vsphereDiagnosticAnalyzers() []*Analyze {
-	return a.controlPlaneIPAnalyzer()
+	return []*Analyze{a.validControlPlaneIPAnalyzer(), a.vcenterSessionValidatePermissionAnalyzer()}
 }
 
-// controlPlaneIPAnalyzer analyze whether a valid control plane IP is used to connect
+// validControlPlaneIPAnalyzer analyzes whether a valid control plane IP is used to connect
 // to API server
-func (a *analyzerFactory) controlPlaneIPAnalyzer() []*Analyze {
+func (a *analyzerFactory) validControlPlaneIPAnalyzer() *Analyze {
 	runPingPod := "ping-host-ip"
 	runPingPodLog := fmt.Sprintf("%s.log", runPingPod)
 	fullRunPingPodLogPath := path.Join(runPingPod, runPingPodLog)
-	return []*Analyze{
-		{
-			TextAnalyze: &textAnalyze{
-				analyzeMeta: analyzeMeta{
-					CheckName: fmt.Sprintf("%s:  Destination Host Unreachable. Log: %s", logAnalysisAnalyzerPrefix, fullRunPingPodLogPath),
-				},
-				FileName:     fullRunPingPodLogPath,
-				RegexPattern: `exit code: 0`,
-				Outcomes: []*outcome{
-					{
-						Fail: &singleOutcome{
-							When:    "false",
-							Message: fmt.Sprintf("The control plane endpoint host is unavailable. See %s", fullRunPingPodLogPath),
-						},
+	return &Analyze{
+		TextAnalyze: &textAnalyze{
+			analyzeMeta: analyzeMeta{
+				CheckName: fmt.Sprintf("%s:  Destination Host Unreachable. Log: %s", logAnalysisAnalyzerPrefix, fullRunPingPodLogPath),
+			},
+			FileName:     fullRunPingPodLogPath,
+			RegexPattern: `exit code: 0`,
+			Outcomes: []*outcome{
+				{
+					Fail: &singleOutcome{
+						When:    "false",
+						Message: fmt.Sprintf("The control plane endpoint host is unavailable. See %s", fullRunPingPodLogPath),
 					},
+				},
 
-					{
-						Pass: &singleOutcome{
-							When:    "true",
-							Message: "Control plane IP verified.",
-						},
+				{
+					Pass: &singleOutcome{
+						When:    "true",
+						Message: "Control plane IP verified.",
+					},
+				},
+			},
+		},
+	}
+}
+
+// vcenterSessionValidateAnalyzer analyzes whether the vcenter user has Session validate permissions for CAPV
+// to be able to look up existing valid sessions to reuse them instead of having to create new ones
+func (a *analyzerFactory) vcenterSessionValidatePermissionAnalyzer() *Analyze {
+	capvManagerPod := "capv-controller-manager-*"
+	capvManagerContainerLogFile := capvManagerPod + ".log"
+	fullManagerPodLogPath := path.Join(logpath(constants.CapvSystemNamespace), capvManagerContainerLogFile)
+	return &Analyze{
+		TextAnalyze: &textAnalyze{
+			analyzeMeta: analyzeMeta{
+				CheckName: fmt.Sprintf("%s:  Session Validate permission missing. Log: %s", logAnalysisAnalyzerPrefix, fullManagerPodLogPath),
+			},
+			FileName:     fullManagerPodLogPath,
+			RegexPattern: `session "msg"="error checking if session is active" "error"="ServerFaultCode: Permission to perform this operation was denied."`,
+			Outcomes: []*outcome{
+				{
+					Fail: &singleOutcome{
+						When:    "true",
+						Message: fmt.Sprintf("VCenter user doesn't have Sessions.ValidateSession permission. See %s", fullManagerPodLogPath),
+					},
+				},
+
+				{
+					Pass: &singleOutcome{
+						When:    "false",
+						Message: "VCenter user has Sessions.ValidateSession permission.",
 					},
 				},
 			},
