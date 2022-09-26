adaab4db70ba0ab2f157555b3cfdc1985a9360ab
Joseph Weitekamp <92933487+jweite-amazon@users.noreply.github.com>
2022-09-26 16:43:34 -0400

    clusterctl move retry (#3324)
    * clusterctl move retry
    
    * Fix lint complaints
    
    * Broadened set of clusterctl network error messages that will be retried.  Added comments requested by reviewers.
    
    * Unexported ClusterctlMoveRetryPolicy as requested by reviewers.
    
    * gofumpt'd pkg/executables/export_test.go

--- pkg/executables/export_test.go
+++ pkg/executables/export_test.go
@@ -5,7 +5,10 @@ import (
 	"time"
 )
 
-var KubectlWaitRetryPolicy = kubectlWaitRetryPolicy
+var (
+	KubectlWaitRetryPolicy    = kubectlWaitRetryPolicy
+	ClusterctlMoveRetryPolicy = clusterctlMoveRetryPolicy
+)
 
 func CallKubectlPrivateWait(k *Kubectl, ctx context.Context, kubeconfig string, timeoutTime time.Time, forCondition string, property string, namespace string) error {
 	return k.wait(ctx, kubeconfig, timeoutTime, forCondition, property, namespace)
