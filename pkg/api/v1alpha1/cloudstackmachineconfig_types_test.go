5d39854421570cee88cfffda9df75be07c7348f8
Max Dribinsky <dribinm@amazon.com>
2022-09-28 14:42:49 -0400

    Adding second unit test

--- pkg/api/v1alpha1/cloudstackmachineconfig_types_test.go
+++ pkg/api/v1alpha1/cloudstackmachineconfig_types_test.go
@@ -32,7 +32,7 @@ func TestCloudStackMachineConfigDiskOfferingEqual(t *testing.T) {
 	g.Expect(diskOffering1.Equal(diskOffering2)).To(BeTrue())
 }
 
-func TestCloudStackMachineConfigEmptyDiskOfferingEqual(t *testing.T) {
+func TestCloudStackMachineConfigNilDiskOfferingEqual(t *testing.T) {
 	var nilDiskOffering *v1alpha1.CloudStackResourceDiskOffering
 	emptyDiskOffering := &v1alpha1.CloudStackResourceDiskOffering{
 		MountPath:  "",
@@ -42,6 +42,18 @@ func TestCloudStackMachineConfigEmptyDiskOfferingEqual(t *testing.T) {
 	}
 	g := NewWithT(t)
 	g.Expect(nilDiskOffering.Equal(emptyDiskOffering)).To(BeTrue())
+}
+
+func TestCloudStackMachineConfigEmptyDiskOfferingEqual(t *testing.T) {
+	emptyDiskOffering1 := v1alpha1.CloudStackResourceDiskOffering{}
+	emptyDiskOffering2 := &v1alpha1.CloudStackResourceDiskOffering{
+		MountPath:  "",
+		Device:     "",
+		Filesystem: "",
+		Label:      "",
+	}
+	g := NewWithT(t)
+	g.Expect(emptyDiskOffering1.Equal(emptyDiskOffering2)).To(BeTrue())
 }
 
 func TestCloudStackMachineConfigDiskOfferingEqualSelf(t *testing.T) {
