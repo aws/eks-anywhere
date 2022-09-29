2170354df72634c05010c40ceffe5d49b3b1cfb8
Jonah Jones <jonahjones094@gmail.com>
2022-09-29 15:52:43 -0500

    Revert "Fixing nil pointer error (#3487)"
    This reverts commit a88f3f65abd4951eebe95a43b1bc6b33f4f1ee21.

--- release/pkg/operations/upload.go
+++ release/pkg/operations/upload.go
@@ -80,11 +80,12 @@ func UploadArtifacts(r *releasetypes.ReleaseConfig, eksArtifacts map[string][]re
 				}
 			}
 
+			// If the artifact is a helm chart, skip the skopeo copy as it's handled separately.
+			if strings.HasSuffix(artifact.Image.AssetName, "helm") {
+				continue
+			}
+
 			if artifact.Image != nil {
-				// If the artifact is a helm chart, skip the skopeo copy as it's handled separately.
-				if strings.HasSuffix(artifact.Image.AssetName, "helm") {
-					continue
-				}
 				sourceImageUri := artifact.Image.SourceImageURI
 				releaseImageUri := artifact.Image.ReleaseImageURI
 				fmt.Printf("Source Image - %s\n", sourceImageUri)
