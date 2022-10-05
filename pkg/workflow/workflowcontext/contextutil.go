bdbc9b9b4203c65641a9089ee0751e0962a05511
Chris Doherty <chris.doherty4@gmail.com>
2022-10-05 12:35:40 -0400

    Rename contextutil to workflowcontext (#3555)
    contextutil was too ambiguous for how closely related to workflow
    execution it is. While workflowcontext creates an undesirable directory
    hierarchy, it provides clearity for consumers when leveraging the
    behavior it provides.

--- pkg/workflow/workflowcontext/contextutil.go
+++ pkg/workflow/workflowcontext/contextutil.go
@@ -1,5 +1,5 @@
 /*
-Package contextutil contains utility functions for populating workflow context specific data
+Package workflowcontext contains utility functions for populating workflow context specific data
 in a context.Context.
 
 Data appropriate for the context includes anything that cannot be determined at time of
@@ -7,4 +7,4 @@ object construction. For example, a bootstrap cluster does not exist when execut
 workflows, therefore a Kubeconfig isn't available to communicate with the cluster so must be passed
 as contextual data.
 */
-package contextutil
+package workflowcontext
