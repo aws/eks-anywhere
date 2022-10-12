e705c5666f3dbe0ac9227e0910a624ecf78e1141
Jonathan Meier <jwmeier@amazon.com>
2022-10-12 07:55:14 -0400

    Use more descriptive variable names in cmd

--- cmd/eksctl-anywhere/cmd/vspheresetupuser.go
+++ cmd/eksctl-anywhere/cmd/vspheresetupuser.go
@@ -16,7 +16,7 @@ type vSphereSetupUserOptions struct {
 	password string
 }
 
-var vsuo = &vSphereSetupUserOptions{}
+var setupUserOptions = &vSphereSetupUserOptions{}
 
 var setupUserCmd = &cobra.Command{
 	Use:          "user -f <config-file> [flags]",
@@ -24,35 +24,34 @@ var setupUserCmd = &cobra.Command{
 	Long:         "Use eksctl anywhere vsphere setup user to configure EKS Anywhere vSphere user",
 	PreRunE:      bindFlagsToViper,
 	SilenceUsage: false,
-	RunE:         vsuo.setupUser,
+	RunE:         setupUserOptions.setupUser,
 }
 
 func init() {
 	vsphereSetupCmd.AddCommand(setupUserCmd)
 
-	setupUserCmd.Flags().StringVarP(&vsuo.fileName, "filename", "f", "", "Filename containing vsphere setup configuration")
-	setupUserCmd.Flags().StringVarP(&vsuo.password, "password", "p", "", "Password for creating new user")
-	setupUserCmd.Flags().BoolVarP(&vsuo.force, "force", "", false, "Force flag. When set, setup user will proceed even if group and role objects already exists. default: false")
+	setupUserCmd.Flags().StringVarP(&setupUserOptions.fileName, "filename", "f", "", "Filename containing vsphere setup configuration")
+	setupUserCmd.Flags().StringVarP(&setupUserOptions.password, "password", "p", "", "Password for creating new user")
+	setupUserCmd.Flags().BoolVarP(&setupUserOptions.force, "force", "", false, "Force flag. When set, setup user will proceed even if group and role objects already exists. default: false")
 
 	if err := setupUserCmd.MarkFlagRequired("filename"); err != nil {
 		log.Fatalf("error marking flag as required: %v", err)
 	}
 }
 
-func (vsuo *vSphereSetupUserOptions) setupUser(cmd *cobra.Command, _ []string) error {
+func (setupUserOptions *vSphereSetupUserOptions) setupUser(cmd *cobra.Command, _ []string) error {
 	ctx := cmd.Context()
 
-	if vsuo.force && vsuo.password != "" {
+	if setupUserOptions.force && setupUserOptions.password != "" {
 		return fmt.Errorf("--password and --force are mutually exclusive. --force may only be run on an existing user")
 	}
 
-	c, err := setupuser.GenerateConfig(ctx, vsuo.fileName)
+	cfg, err := setupuser.GenerateConfig(ctx, setupUserOptions.fileName)
 	if err != nil {
 		return err
 	}
 
-	// hacky
-	err = setupuser.SetupGOVCEnv(ctx, c)
+	err = setupuser.SetupGOVCEnv(ctx, cfg)
 	if err != nil {
 		return err
 	}
@@ -63,21 +62,21 @@ func (vsuo *vSphereSetupUserOptions) setupUser(cmd *cobra.Command, _ []string) e
 	defer close(ctx, deps)
 
 	// if force flag not used, we should create a user
-	if !vsuo.force {
-		err = deps.Govc.CreateUser(ctx, c.Spec.Username, vsuo.password)
+	if !setupUserOptions.force {
+		err = deps.Govc.CreateUser(ctx, cfg.Spec.Username, setupUserOptions.password)
 	}
 	if err != nil {
 		return err
 	}
 
-	if !vsuo.force {
-		err = setupuser.ValidateVSphereObjects(ctx, c, deps.Govc)
+	if !setupUserOptions.force {
+		err = setupuser.ValidateVSphereObjects(ctx, cfg, deps.Govc)
 		if err != nil {
 			return err
 		}
 	}
 
-	err = setupuser.Run(ctx, c, deps.Govc)
+	err = setupuser.Run(ctx, cfg, deps.Govc)
 	if err != nil {
 		return err
 	}
