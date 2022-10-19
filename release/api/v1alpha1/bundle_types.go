66183da83f0b477e01d56a71a86abe5ed515e957
Deepak Muley <deepak.muley@nutanix.com>
2022-10-19 09:31:53 -0700

    fixed kube-vip yaml for nutanix provider (#3662)
    * fixed kube-vip
    
    * now using eks-a built kube-vip container image
    
    * update output of release generated yamls
    
    make release-manifests
    make -C ./release update-bundle-golden-files
    
    * added kubevip for nutanix
    
    * added missing entry to update kubeVip
    
    * ran make commands to get latest bundle changes
    
    make release-manifests
    make manifests -C release
    
    * made the code working with kube-vip v0.5.5
    
    currently its not wokring with v0.5.5 version of kube-vip built by aws
    we will revisit the same once we have a fix for the same
    
    * now updating /etc/hosts per requirement
    
    required due to https://github.com/aws/eks-anywhere-build-tooling/blob/main/projects/kube-vip/kube[…]0001-use-hostname-instead-of-kubernetes-to-contact-apiser.patch

--- release/api/v1alpha1/bundle_types.go
+++ release/api/v1alpha1/bundle_types.go
@@ -333,6 +333,7 @@ type SnowBundle struct {
 type NutanixBundle struct {
 	ClusterAPIController Image    `json:"clusterAPIController"`
 	Version              string   `json:"version"`
+	KubeVip              Image    `json:"kubeVip"`
 	Components           Manifest `json:"components"`
 	Metadata             Manifest `json:"metadata"`
 	ClusterTemplate      Manifest `json:"clusterTemplate"`
