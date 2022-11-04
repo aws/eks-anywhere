318fabf48edf764ea604c6306557b5fa7d54ea59
Taylor Neyland <57606775+taneyland@users.noreply.github.com>
2022-11-04 12:03:12 -0500

    Update CP template names if there are changes (#3920)

--- pkg/providers/vsphere/workers.go
+++ pkg/providers/vsphere/workers.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
-	"k8s.io/apimachinery/pkg/api/equality"
 	vspherev1 "sigs.k8s.io/cluster-api-provider-vsphere/api/v1beta1"
 
 	"github.com/aws/eks-anywhere/pkg/clients/kubernetes"
@@ -70,17 +69,4 @@ func machineTemplateMapping() yamlutil.Mapping[*vspherev1.VSphereMachineTemplate
 			return &vspherev1.VSphereMachineTemplate{}
 		},
 	)
-}
-
-func getMachineTemplate(ctx context.Context, client kubernetes.Client, name, namespace string) (*vspherev1.VSphereMachineTemplate, error) {
-	m := &vspherev1.VSphereMachineTemplate{}
-	if err := client.Get(ctx, name, namespace, m); err != nil {
-		return nil, errors.Wrap(err, "reading vSphereMachineTemplate")
-	}
-
-	return m, nil
-}
-
-func machineTemplateEqual(new, old *vspherev1.VSphereMachineTemplate) bool {
-	return equality.Semantic.DeepDerivative(new, old)
 }
