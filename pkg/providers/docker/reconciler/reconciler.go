3d44eae5322d78d4e09fe959b6ac453ea9ed12fe
Jonathan Meier <jonathanmeier5@users.noreply.github.com>
2022-11-09 18:54:43 -0500

    Build docker reconciler (#3984)

--- pkg/providers/docker/reconciler/reconciler.go
+++ pkg/providers/docker/reconciler/reconciler.go
@@ -4,15 +4,40 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	anywherev1 "github.com/aws/eks-anywhere/pkg/api/v1alpha1"
+	"github.com/aws/eks-anywhere/pkg/cluster"
 	"github.com/aws/eks-anywhere/pkg/controller"
+	"github.com/aws/eks-anywhere/pkg/controller/serverside"
 )
 
-type Reconciler struct{}
+// Reconciler contains dependencies for a docker reconciler.
+type Reconciler struct {
+	client               client.Client
+	cniReconciler        CNIReconciler
+	remoteClientRegistry RemoteClientRegistry
+	*serverside.ObjectApplier
+}
 
-func New() *Reconciler {
-	return &Reconciler{}
+// CNIReconciler is an interface for reconciling CNI in the Docker cluster reconciler.
+type CNIReconciler interface {
+	Reconcile(ctx context.Context, logger logr.Logger, client client.Client, spec *cluster.Spec) (controller.Result, error)
+}
+
+// RemoteClientRegistry is an interface that defines methods for remote clients.
+type RemoteClientRegistry interface {
+	GetClient(ctx context.Context, cluster client.ObjectKey) (client.Client, error)
+}
+
+// New creates a new Docker provider reconciler.
+func New(client client.Client, cniReconciler CNIReconciler, remoteClientRegistry RemoteClientRegistry) *Reconciler {
+	return &Reconciler{
+		client:               client,
+		cniReconciler:        cniReconciler,
+		remoteClientRegistry: remoteClientRegistry,
+		ObjectApplier:        serverside.NewObjectApplier(client),
+	}
 }
 
 func (v *Reconciler) Reconcile(ctx context.Context, log logr.Logger, cluster *anywherev1.Cluster) (controller.Result, error) {
