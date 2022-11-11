b634f3f4ed904a406cd103c6e5e00e2f317037d8
Tanvir Tatla <tatlat@amazon.com>
2022-11-11 14:36:58 -0800

    added comment to exported function, new unit test

--- pkg/providers/docker/validator.go
+++ pkg/providers/docker/validator.go
@@ -6,6 +6,7 @@ import (
 	"github.com/aws/eks-anywhere/pkg/cluster"
 )
 
+// ValidateControlPlaneEnpoint - checks to see if endpoint host configuration is specified for docker cluster and returns an error if true.
 func ValidateControlPlaneEndpoint(clusterSpec *cluster.Spec) error {
 	if clusterSpec.Cluster.Spec.ControlPlaneConfiguration.Endpoint != nil {
 		return fmt.Errorf("specifying endpoint host configuration in Cluster is not supported")
