7b383e511a532303dc4a2e8f989bed4a40e9926e
Abdullahi Abdinur <acool@amazon.com>
2022-11-14 09:15:31 -0800

    Remove tokenRefresher from the extracted artifacts

--- release/api/v1alpha1/artifacts.go
+++ release/api/v1alpha1/artifacts.go
@@ -222,7 +222,6 @@ func (vb *VersionsBundle) Charts() map[string]*Image {
 	return map[string]*Image{
 		"cilium":                &vb.Cilium.HelmChart,
 		"eks-anywhere-packages": &vb.PackageController.HelmChart,
-		"ecr-token-refresher":   &vb.PackageController.TokenRefresher,
 		"tinkerbell-chart":      &vb.Tinkerbell.TinkerbellStack.TinkebellChart,
 	}
 }
