9b9914164b5ca826658014e326a3e0f4fa68e8b8
Jonah Jones <jonahjones094@gmail.com>
2022-11-16 16:23:51 -0600

    updating packages version for e2e (#4121)
    Signed-off-by: jonahjon <jonahjones094@gmail.com>
    
    Signed-off-by: jonahjon <jonahjones094@gmail.com>

--- test/e2e/curated_packages_test.go
+++ test/e2e/curated_packages_test.go
@@ -25,7 +25,7 @@ const (
 	EksaPackageControllerHelmChartName = "eks-anywhere-packages"
 	EksaPackagesSourceRegistry         = "public.ecr.aws/l0g8r8j6"
 	EksaPackageControllerHelmURI       = "oci://" + EksaPackagesSourceRegistry + "/eks-anywhere-packages"
-	EksaPackageControllerHelmVersion   = "0.2.13-eks-a-v0.0.0-dev-build.4532"
+	EksaPackageControllerHelmVersion   = "0.2.20-eks-a-v0.0.0-dev-build.4894"
 	EksaPackageBundleURI               = "oci://" + EksaPackagesSourceRegistry + "/eks-anywhere-packages-bundles"
 )
 
