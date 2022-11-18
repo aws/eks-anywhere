1a0990f98249f7b84e35923670274698dccbda18
Cavaughn Browne <113555337+cxbrowne1207@users.noreply.github.com>
2022-11-18 13:36:58 -0600

    added some minor fixes to the reconcile tests for docker and exported CAPIClusterOpt (#4158)
    Co-authored-by: Cavaughn Browne <cavaughn@amazon.com>

--- pkg/providers/docker/reconciler/reconciler_test.go
+++ pkg/providers/docker/reconciler/reconciler_test.go
@@ -136,7 +136,7 @@ func TestReconcileControlPlaneStackedEtcdSuccess(t *testing.T) {
 	tt.ShouldEventuallyNotExist(tt.ctx,
 		&dockerv1.DockerMachineTemplate{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "workload-cluster-etcd-1",
+				Name:      tt.cluster.Name + "-etcd-1",
 				Namespace: constants.EksaSystemNamespace,
 			},
 		},
@@ -144,7 +144,7 @@ func TestReconcileControlPlaneStackedEtcdSuccess(t *testing.T) {
 	tt.ShouldEventuallyNotExist(tt.ctx,
 		&etcdv1.EtcdadmCluster{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "workload-cluster-etcd",
+				Name:      tt.cluster.Name + "-etcd",
 				Namespace: constants.EksaSystemNamespace,
 			},
 		},
