55fdd1d4934519fe491a2fda5ced6ae17484f7b5
Tanvir Tatla <tatlat@amazon.com>
2022-11-17 17:46:51 -0800

    added tests for docker reconcile workers

--- pkg/providers/docker/reconciler/reconciler_test.go
+++ pkg/providers/docker/reconciler/reconciler_test.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	bootstrapv1 "sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
@@ -15,6 +19,7 @@ import (
 	"github.com/aws/eks-anywhere/internal/test/envtest"
 	anywherev1 "github.com/aws/eks-anywhere/pkg/api/v1alpha1"
 	clusterspec "github.com/aws/eks-anywhere/pkg/cluster"
+	"github.com/aws/eks-anywhere/pkg/clusterapi"
 	"github.com/aws/eks-anywhere/pkg/constants"
 	"github.com/aws/eks-anywhere/pkg/controller"
 	"github.com/aws/eks-anywhere/pkg/controller/clientutil"
@@ -41,6 +46,14 @@ func TestReconcilerReconcileSuccess(t *testing.T) {
 
 func TestReconcilerReconcileWorkerNodesSuccess(t *testing.T) {
 	tt := newReconcilerTest(t)
+	tt.cluster.Name = "my-management-cluster"
+	tt.cluster.SetSelfManaged()
+	capiCluster := capiCluster(func(c *clusterv1.Cluster) {
+		c.Name = tt.cluster.Name
+	})
+
+	tt.eksaSupportObjs = append(tt.eksaSupportObjs, capiCluster)
+	tt.createAllObjs()
 
 	logger := test.NewNullLogger()
 
@@ -49,6 +62,24 @@ func TestReconcilerReconcileWorkerNodesSuccess(t *testing.T) {
 	tt.Expect(err).NotTo(HaveOccurred())
 	tt.Expect(tt.cluster.Status.FailureMessage).To(BeZero())
 	tt.Expect(result).To(Equal(controller.Result{}))
+
+	tt.ShouldEventuallyExist(tt.ctx,
+		&clusterv1.MachineDeployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "my-management-cluster-md-0",
+				Namespace: constants.EksaSystemNamespace,
+			},
+		},
+	)
+
+	tt.ShouldEventuallyExist(tt.ctx,
+		&bootstrapv1.KubeadmConfigTemplate{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "my-management-cluster-md-0-1",
+				Namespace: constants.EksaSystemNamespace,
+			},
+		},
+	)
 }
 
 func TestReconcileCNISuccess(t *testing.T) {
@@ -85,6 +116,17 @@ func TestReconcileCNIErrorClientRegistry(t *testing.T) {
 	result, err := tt.reconciler().ReconcileCNI(tt.ctx, logger, spec)
 
 	tt.Expect(err).To(MatchError(ContainSubstring("building client")))
+	tt.Expect(tt.cluster.Status.FailureMessage).To(BeZero())
+	tt.Expect(result).To(Equal(controller.Result{}))
+}
+
+func TestReconcilerReconcileWorkersSuccess(t *testing.T) {
+	tt := newReconcilerTest(t)
+	tt.createAllObjs()
+
+	result, err := tt.reconciler().ReconcileWorkers(tt.ctx, test.NewNullLogger(), tt.buildSpec())
+
+	tt.Expect(err).NotTo(HaveOccurred())
 	tt.Expect(tt.cluster.Status.FailureMessage).To(BeZero())
 	tt.Expect(result).To(Equal(controller.Result{}))
 }
@@ -146,6 +188,7 @@ func newReconcilerTest(t testing.TB) *reconcilerTest {
 		c.Spec.WorkerNodeGroupConfigurations = append(c.Spec.WorkerNodeGroupConfigurations,
 			anywherev1.WorkerNodeGroupConfiguration{
 				Count: ptr.Int(1),
+				Name:  "md-0",
 			},
 		)
 	})
@@ -193,6 +236,11 @@ func (tt *reconcilerTest) buildSpec() *clusterspec.Spec {
 
 func (tt *reconcilerTest) withFakeClient() {
 	tt.client = fake.NewClientBuilder().WithObjects(clientutil.ObjectsToClientObjects(tt.allObjs())...).Build()
+}
+
+func (tt *reconcilerTest) createAllObjs() {
+	tt.t.Helper()
+	envtest.CreateObjs(tt.ctx, tt.t, tt.client, tt.allObjs()...)
 }
 
 func (tt *reconcilerTest) allObjs() []client.Object {
@@ -245,4 +293,34 @@ func dataCenter() *anywherev1.DockerDatacenterConfig {
 			Namespace: clusterNamespace,
 		},
 	}
+}
+
+type capiClusterOpt func(*clusterv1.Cluster)
+
+func capiCluster(opts ...capiClusterOpt) *clusterv1.Cluster {
+	c := &clusterv1.Cluster{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Cluster",
+			APIVersion: clusterv1.GroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-cluster",
+			Namespace: "eksa-system",
+		},
+		Status: clusterv1.ClusterStatus{
+			Conditions: clusterv1.Conditions{
+				{
+					Type:               clusterapi.ControlPlaneReadyCondition,
+					Status:             corev1.ConditionTrue,
+					LastTransitionTime: metav1.NewTime(time.Now()),
+				},
+			},
+		},
+	}
+
+	for _, opt := range opts {
+		opt(c)
+	}
+
+	return c
 }
