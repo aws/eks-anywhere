6a436c5271a75fecbeeecf1db10bc7ea6ee17173
Tanvir Tatla <tatlat@amazon.com>
2022-11-17 17:20:05 -0800

    implemented docker reconciliation of workers

--- pkg/providers/docker/reconciler/reconciler.go
+++ pkg/providers/docker/reconciler/reconciler.go
@@ -2,14 +2,19 @@ package reconciler
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	anywherev1 "github.com/aws/eks-anywhere/pkg/api/v1alpha1"
+	"github.com/aws/eks-anywhere/pkg/clients/kubernetes"
 	"github.com/aws/eks-anywhere/pkg/cluster"
+	c "github.com/aws/eks-anywhere/pkg/cluster"
 	"github.com/aws/eks-anywhere/pkg/controller"
+	"github.com/aws/eks-anywhere/pkg/controller/clientutil"
 	"github.com/aws/eks-anywhere/pkg/controller/serverside"
+	"github.com/aws/eks-anywhere/pkg/providers/docker"
 )
 
 // Reconciler contains dependencies for a docker reconciler.
@@ -59,5 +64,29 @@ func (r *Reconciler) ReconcileCNI(ctx context.Context, log logr.Logger, clusterS
 // ReconcileWorkerNodes validates the cluster definition and reconciles the worker nodes
 // to the desired state.
 func (r *Reconciler) ReconcileWorkerNodes(ctx context.Context, log logr.Logger, cluster *anywherev1.Cluster) (controller.Result, error) {
-	return controller.Result{}, nil
+	log = log.WithValues("provider", "docker", "reconcile type", "workers")
+	clusterSpec, err := c.BuildSpec(ctx, clientutil.NewKubeClient(r.client), cluster)
+	if err != nil {
+		return controller.Result{}, err
+	}
+
+	return controller.NewPhaseRunner().Register(
+		r.ReconcileWorkers,
+	).Run(ctx, log, clusterSpec)
+}
+
+// ReconcileWorkers applies the worker CAPI objects to the cluster.
+func (r *Reconciler) ReconcileWorkers(ctx context.Context, log logr.Logger, clusterSpec *c.Spec) (controller.Result, error) {
+	log = log.WithValues("phase", "reconcileWorkers")
+	log.Info("Applying worker CAPI objects")
+	return r.Apply(ctx, func() ([]kubernetes.Object, error) {
+		w, err := docker.WorkersSpec(ctx, log, clientutil.NewKubeClient(r.client), clusterSpec)
+		if err != nil {
+			return nil, err
+		}
+		for _, w := range w.WorkerObjects() {
+			fmt.Printf("%v \n", w.GetName())
+		}
+		return w.WorkerObjects(), nil
+	})
 }
