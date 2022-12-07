a1c21f75e95b4e71dd57a5af4176aabc974834a8
Jun Shun Zhang <42823312+junshun@users.noreply.github.com>
2022-12-07 15:07:04 -0500

    Adding autoscaling annotations to nutanix (#4314)
    * Adding autoscaling annotations to nutanix
    
    * Adding test for autoscaling template nutanix

--- pkg/providers/nutanix/template_test.go
+++ pkg/providers/nutanix/template_test.go
@@ -26,6 +26,9 @@ var nutanixDatacenterConfigSpec string
 //go:embed testdata/machineConfig.yaml
 var nutanixMachineConfigSpec string
 
+//go:embed testdata/eksa-cluster-autoscaler.yaml
+var nutanixClusterConfigSpecWithAutoscaler string
+
 func fakemarshal(v interface{}) ([]byte, error) {
 	return []byte{}, errors.New("marshalling failed")
 }
@@ -101,4 +104,45 @@ func TestNewNutanixTemplateBuilderGenerateCAPISpecSecret(t *testing.T) {
 	secretSpec, err := builder.GenerateCAPISpecSecret(buildSpec)
 	assert.Nil(t, secretSpec)
 	assert.Error(t, err)
+}
+
+func TestNutanixTemplateBuilderGenerateCAPISpecForCreateWithAutoscalingConfiguration(t *testing.T) {
+	clusterConf := &anywherev1.Cluster{}
+	err := yaml.Unmarshal([]byte(nutanixClusterConfigSpecWithAutoscaler), clusterConf)
+	require.NoError(t, err)
+
+	dcConf := &anywherev1.NutanixDatacenterConfig{}
+	err = yaml.Unmarshal([]byte(nutanixDatacenterConfigSpec), dcConf)
+	require.NoError(t, err)
+
+	machineConf := &anywherev1.NutanixMachineConfig{}
+	err = yaml.Unmarshal([]byte(nutanixMachineConfigSpec), machineConf)
+	require.NoError(t, err)
+
+	workerConfs := map[string]anywherev1.NutanixMachineConfigSpec{
+		"eksa-unit-test": machineConf.Spec,
+	}
+
+	t.Setenv(constants.NutanixUsernameKey, "admin")
+	t.Setenv(constants.NutanixPasswordKey, "password")
+	creds := GetCredsFromEnv()
+
+	builder := NewNutanixTemplateBuilder(&dcConf.Spec, &machineConf.Spec, &machineConf.Spec, workerConfs, creds, time.Now)
+	assert.NotNil(t, builder)
+
+	v := version.Info{GitVersion: "v0.0.1"}
+	buildSpec, err := cluster.NewSpecFromClusterConfig("testdata/eksa-cluster-autoscaler.yaml", v, cluster.WithReleasesManifest("testdata/simple_release.yaml"))
+	assert.NoError(t, err)
+
+	workloadTemplateNames := map[string]string{
+		"eksa-unit-test": "eksa-unit-test",
+	}
+	kubeadmconfigTemplateNames := map[string]string{
+		"eksa-unit-test": "eksa-unit-test",
+	}
+	workerSpec, err := builder.GenerateCAPISpecWorkers(buildSpec, workloadTemplateNames, kubeadmconfigTemplateNames)
+	assert.NoError(t, err)
+	expectedWorkerSpec, err := os.ReadFile("testdata/expected_results_autoscaling_md.yaml")
+	require.NoError(t, err)
+	assert.Equal(t, workerSpec, expectedWorkerSpec)
 }
