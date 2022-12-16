e079d61d5d5b97bf34834756c8a84e7fe77b19aa
Terry Howe <tlhowe@amazon.com>
2022-12-16 12:09:08 -0700

    More credential tests

--- pkg/registry/credentials_test.go
+++ pkg/registry/credentials_test.go
@@ -1,6 +1,8 @@
 package registry
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -32,4 +34,29 @@ func TestCredentialStore_Init(t *testing.T) {
 	assert.Equal(t, "", result.Password)
 	assert.Equal(t, "", result.AccessToken)
 	assert.Equal(t, "", result.RefreshToken)
+
+	result, err = credentialStore.Credential("5551212.dkr.ecr.us-west-2.amazonaws.com")
+	assert.EqualError(t, err, "error getting credentials - err: exec: \"docker-credential-bogus\": executable file not found in $PATH, out: ``")
+	assert.Equal(t, "", result.Username)
+	assert.Equal(t, "", result.Password)
+	assert.Equal(t, "", result.AccessToken)
+	assert.Equal(t, "", result.RefreshToken)
+}
+
+func TestCredentialStore_InitEmpty(t *testing.T) {
+	credentialStore := NewCredentialStore("testdata/empty")
+	err := credentialStore.Init()
+	assert.NoError(t, err)
+}
+
+func TestCredentialStore_InitNoPermissions(t *testing.T) {
+	dir, err := ioutil.TempDir("testdata", "noperms")
+	defer os.Remove(dir)
+	assert.NoError(t, err)
+	err = os.WriteFile(dir+"/config.json", []byte("{}"), 0000)
+	assert.NoError(t, err)
+
+	credentialStore := NewCredentialStore(dir)
+	err = credentialStore.Init()
+	assert.EqualError(t, err, dir+"/config.json: open "+dir+"/config.json: permission denied")
 }
