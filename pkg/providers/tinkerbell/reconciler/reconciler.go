eeb8bd09906c6c24b0a0290ecae4cfab9c291210
Mitali Paygude <mitalipaygude@gmail.com>
2023-01-17 17:18:18 -0800

    Add IP validator for tinkerbell reconciler

--- pkg/providers/tinkerbell/reconciler/reconciler.go
+++ pkg/providers/tinkerbell/reconciler/reconciler.go
@@ -4,18 +4,31 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	anywherev1 "github.com/aws/eks-anywhere/pkg/api/v1alpha1"
 	c "github.com/aws/eks-anywhere/pkg/cluster"
 	"github.com/aws/eks-anywhere/pkg/controller"
+	"github.com/aws/eks-anywhere/pkg/controller/clientutil"
 )
 
+// IPValidator is an interface that defines methods to validate the control plane IP.
+type IPValidator interface {
+	ValidateControlPlaneIP(ctx context.Context, log logr.Logger, spec *c.Spec) (controller.Result, error)
+}
+
 // Reconciler for Tinkerbell.
-type Reconciler struct{}
+type Reconciler struct {
+	client      client.Client
+	ipValidator IPValidator
+}
 
 // New defines a new Tinkerbell reconciler.
-func New() *Reconciler {
-	return &Reconciler{}
+func New(client client.Client, ipValidator IPValidator) *Reconciler {
+	return &Reconciler{
+		client:      client,
+		ipValidator: ipValidator,
+	}
 }
 
 // Reconcile reconciles cluster to desired state.
@@ -24,7 +37,15 @@ func (r *Reconciler) Reconcile(ctx context.Context, log logr.Logger, cluster *an
 	// This would include IP validator, validating machine and datacenter configs
 	// and reconciling cp and worker nodes.
 
-	return controller.Result{}, nil
+	log = log.WithValues("provider", "vsphere")
+	clusterSpec, err := c.BuildSpec(ctx, clientutil.NewKubeClient(r.client), cluster)
+	if err != nil {
+		return controller.Result{}, err
+	}
+
+	return controller.NewPhaseRunner().Register(
+		r.ipValidator.ValidateControlPlaneIP,
+	).Run(ctx, log, clusterSpec)
 }
 
 // ReconcileControlPlane applies the control plane CAPI objects to the cluster.
