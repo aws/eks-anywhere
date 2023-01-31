86b79f01d91ad98e551c9363327ab28685d25a8f
Jiayi Wang <jiayiwang7@yahoo.com>
2023-01-31 16:50:21 -0500

    Add snow ip pool validation

--- pkg/api/v1alpha1/snowippool_webhook_test.go
+++ pkg/api/v1alpha1/snowippool_webhook_test.go
@@ -15,11 +15,26 @@ func TestSnowIPPoolValidateCreate(t *testing.T) {
 	g.Expect(new.ValidateCreate()).To(Succeed())
 }
 
+func TestSnowIPPoolValidateCreateInvalidIPPool(t *testing.T) {
+	g := NewWithT(t)
+	new := snowIPPool()
+	new.Spec.Pools[0].IPStart = "invalid"
+	g.Expect(new.ValidateCreate()).To(MatchError(ContainSubstring("SnowIPPool Pools[0].IPStart is invalid")))
+}
+
 func TestSnowIPPoolValidateUpdate(t *testing.T) {
 	g := NewWithT(t)
 	new := snowIPPool()
 	old := new.DeepCopy()
 	g.Expect(new.ValidateUpdate(old)).To(Succeed())
+}
+
+func TestSnowIPPoolValidateUpdateInvalidIPPool(t *testing.T) {
+	g := NewWithT(t)
+	new := snowIPPool()
+	new.Spec.Pools[0].IPStart = "invalid"
+	old := new.DeepCopy()
+	g.Expect(new.ValidateUpdate(old)).To(MatchError(ContainSubstring("SnowIPPool Pools[0].IPStart is invalid")))
 }
 
 func TestSnowIPPoolValidateUpdateInvalidObjectType(t *testing.T) {
@@ -35,30 +50,16 @@ func TestSnowIPPoolValidateUpdateIPPoolsSame(t *testing.T) {
 	old := new.DeepCopy()
 	new.Spec.Pools = []v1alpha1.IPPool{
 		{
-			IPStart: "start-2",
-			IPEnd:   "end-2",
-			Subnet:  "subnet-2",
-			Gateway: "gateway-2",
+			IPStart: "192.168.1.20",
+			IPEnd:   "192.168.1.30",
+			Gateway: "192.168.1.1",
+			Subnet:  "192.168.1.0/24",
 		},
 		{
-			IPStart: "start-1",
-			IPEnd:   "end-1",
-			Subnet:  "subnet-1",
-			Gateway: "gateway-1",
-		},
-	}
-	old.Spec.Pools = []v1alpha1.IPPool{
-		{
-			IPStart: "start-1",
-			IPEnd:   "end-1",
-			Subnet:  "subnet-1",
-			Gateway: "gateway-1",
-		},
-		{
-			IPStart: "start-2",
-			IPEnd:   "end-2",
-			Subnet:  "subnet-2",
-			Gateway: "gateway-2",
+			IPStart: "192.168.1.2",
+			IPEnd:   "192.168.1.14",
+			Gateway: "192.168.1.1",
+			Subnet:  "192.168.1.0/24",
 		},
 	}
 	g.Expect(new.ValidateUpdate(old)).To(Succeed())
@@ -82,18 +83,16 @@ func TestSnowIPPoolValidateUpdateIPPoolsDiff(t *testing.T) {
 	old := new.DeepCopy()
 	new.Spec.Pools = []v1alpha1.IPPool{
 		{
-			IPStart: "start-1",
-			IPEnd:   "end-1",
-			Subnet:  "subnet-1",
-			Gateway: "gateway-1",
+			IPStart: "192.168.1.21",
+			IPEnd:   "192.168.1.30",
+			Gateway: "192.168.1.1",
+			Subnet:  "192.168.1.0/24",
 		},
-	}
-	old.Spec.Pools = []v1alpha1.IPPool{
 		{
-			IPStart: "start-2",
-			IPEnd:   "end-2",
-			Subnet:  "subnet-2",
-			Gateway: "gateway-2",
+			IPStart: "192.168.1.2",
+			IPEnd:   "192.168.1.14",
+			Gateway: "192.168.1.1",
+			Subnet:  "192.168.1.0/24",
 		},
 	}
 	g.Expect(new.ValidateUpdate(old)).To(MatchError(ContainSubstring("spec.pools: Forbidden: field is immutable")))
@@ -109,7 +108,22 @@ func snowIPPool() v1alpha1.SnowIPPool {
 	return v1alpha1.SnowIPPool{
 		TypeMeta:   metav1.TypeMeta{},
 		ObjectMeta: metav1.ObjectMeta{},
-		Spec:       v1alpha1.SnowIPPoolSpec{},
-		Status:     v1alpha1.SnowIPPoolStatus{},
+		Spec: v1alpha1.SnowIPPoolSpec{
+			Pools: []v1alpha1.IPPool{
+				{
+					IPStart: "192.168.1.2",
+					IPEnd:   "192.168.1.14",
+					Gateway: "192.168.1.1",
+					Subnet:  "192.168.1.0/24",
+				},
+				{
+					IPStart: "192.168.1.20",
+					IPEnd:   "192.168.1.30",
+					Gateway: "192.168.1.1",
+					Subnet:  "192.168.1.0/24",
+				},
+			},
+		},
+		Status: v1alpha1.SnowIPPoolStatus{},
 	}
 }
