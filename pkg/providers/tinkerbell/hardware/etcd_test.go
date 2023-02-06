9c8dfcdb86a4f44d345b58b07180629268a881b9
Aravind Ramalingam <ramaliar@amazon.com>
2023-02-06 02:59:52 -0800

    Adding reader for reading hw from etcd

---
+++ pkg/providers/tinkerbell/hardware/etcd_test.go
@@ -0,0 +1,78 @@
+package hardware_test
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	tinkv1alpha1 "github.com/tinkerbell/tink/pkg/apis/core/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/aws/eks-anywhere/pkg/providers/tinkerbell/hardware"
+)
+
+func TestNewCatalogueFromETCDSuccess(t *testing.T) {
+	g := NewWithT(t)
+	ctx := context.Background()
+
+	hw := tinkv1alpha1.Hardware{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "hw1",
+			Labels: map[string]string{
+				"type": "cp",
+			},
+		},
+		Spec: tinkv1alpha1.HardwareSpec{
+			Metadata: &tinkv1alpha1.HardwareMetadata{
+				Instance: &tinkv1alpha1.MetadataInstance{
+					ID: "foo",
+				},
+			},
+		},
+	}
+
+	scheme := runtime.NewScheme()
+	_ = tinkv1alpha1.AddToScheme(scheme)
+	objs := []runtime.Object{&hw}
+	cb := fake.NewClientBuilder()
+	cl := cb.WithScheme(scheme).WithRuntimeObjects(objs...).Build()
+
+	etcdReader := hardware.NewETCDReader(cl)
+	err := etcdReader.NewCatalogueFromETCD(ctx)
+	g.Expect(err).ToNot(HaveOccurred())
+	g.Expect(len(etcdReader.GetCatalogue().AllHardware())).To(Equal(1))
+}
+
+func TestNewCatalogueFromETCDNoHardware(t *testing.T) {
+	g := NewWithT(t)
+	ctx := context.Background()
+
+	hw := tinkv1alpha1.Hardware{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "hw1",
+			Labels: map[string]string{
+				"v1alpha1.tinkerbell.org/ownerName": "cluster",
+				"type":                              "cp",
+			},
+		},
+		Spec: tinkv1alpha1.HardwareSpec{
+			Metadata: &tinkv1alpha1.HardwareMetadata{
+				Instance: &tinkv1alpha1.MetadataInstance{
+					ID: "foo",
+				},
+			},
+		},
+	}
+	scheme := runtime.NewScheme()
+	_ = tinkv1alpha1.AddToScheme(scheme)
+	objs := []runtime.Object{&hw}
+	cb := fake.NewClientBuilder()
+	cl := cb.WithScheme(scheme).WithRuntimeObjects(objs...).Build()
+
+	etcdReader := hardware.NewETCDReader(cl)
+	err := etcdReader.NewCatalogueFromETCD(ctx)
+	g.Expect(err).To(HaveOccurred())
+	g.Expect(err.Error()).To(ContainSubstring("no available hardware"))
+}
