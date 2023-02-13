96697e9f472c5adb1eacdda31923a42f1f1b318c
Taylor Neyland <57606775+taneyland@users.noreply.github.com>
2023-02-13 15:41:06 -0600

    Update default kube version to 1.25 (#4922)

--- pkg/api/v1alpha1/cluster_test.go
+++ pkg/api/v1alpha1/cluster_test.go
@@ -3010,7 +3010,7 @@ func TestValidateMDUpgradeRolloutStrategy(t *testing.T) {
 
 func TestGetClusterDefaultKubernetesVersion(t *testing.T) {
 	g := NewWithT(t)
-	g.Expect(GetClusterDefaultKubernetesVersion()).To(Equal(Kube124))
+	g.Expect(GetClusterDefaultKubernetesVersion()).To(Equal(Kube125))
 }
 
 func TestClusterWorkerNodeConfigCount(t *testing.T) {
