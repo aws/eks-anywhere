bfebacd5cbd9881609c011f79b4c413e11bd7209
Lewis Diamond <github.com@lewisdiamond.com>
2023-02-16 11:26:23 -0500

    Update pkg/curatedpackages/packagecontrollerclient.go
    Co-authored-by: Eric Wollesen <169516+ewollesen@users.noreply.github.com>

--- pkg/curatedpackages/packagecontrollerclient.go
+++ pkg/curatedpackages/packagecontrollerclient.go
@@ -205,7 +205,7 @@ const packageBundleControllerResource string = "packageBundleController"
 // It returns nil on success. Success is defined as receiving a valid package
 // bundle controller from the API with a non-empty active bundle.
 //
-// If no timeout is specified, a default of 1 minute is used.
+// If no timeout is specified, a default of 3 minutes is used.
 func (pc *PackageControllerClient) waitForActiveBundle(ctx context.Context) error {
 	timeout := 3 * time.Minute
 	if pc.activeBundleTimeout > 0 {
