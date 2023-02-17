02dcd09f9f61b441b5dd31658ba7d0c6e3e7fbb6
Abhinav Pandey <abhinavmpandey08@gmail.com>
2023-02-16 16:48:15 -0800

    Add e2e test for Bottlerocket NTP

---
+++ internal/test/e2e/ntp.go
@@ -0,0 +1,22 @@
+package e2e
+
+import (
+	"os"
+	"regexp"
+
+	e2etests "github.com/aws/eks-anywhere/test/framework"
+)
+
+func (e *E2ESession) setupNTPEnv(testRegex string) error {
+	re := regexp.MustCompile(`^.*NTP.*$`)
+	if !re.MatchString(testRegex) {
+		return nil
+	}
+
+	for _, eVar := range e2etests.RequiredNTPServersEnvVars() {
+		if val, ok := os.LookupEnv(eVar); ok {
+			e.testEnvVars[eVar] = val
+		}
+	}
+	return nil
+}
