f76caf4b3ac1734f8f160831a9ba4107b6338a42
Tanvir Tatla <tatlat@amazon.com>
2023-02-28 13:12:39 -0800

    added test for reconcile

--- controllers/cloudstack_datacenter_controller_test.go
+++ controllers/cloudstack_datacenter_controller_test.go
@@ -1,9 +1,12 @@
 package controllers_test
 
 import (
+	"context"
 	"testing"
 
 	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/aws/eks-anywhere/controllers"
 )
@@ -14,4 +17,22 @@ func TestCloudStackDatacenterReconcilerSetupWithManager(t *testing.T) {
 
 	g := NewWithT(t)
 	g.Expect(r.SetupWithManager(env.Manager())).To(Succeed())
+}
+
+func TestCloudStackDatacenterReconcilerSuccess(t *testing.T) {
+	g := NewWithT(t)
+	ctx := context.Background()
+	client := env.Client()
+
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+
+	r := controllers.NewCloudStackDatacenterReconciler(client)
+
+	_, err := r.Reconcile(ctx, req)
+	g.Expect(err).NotTo(HaveOccurred())
 }
