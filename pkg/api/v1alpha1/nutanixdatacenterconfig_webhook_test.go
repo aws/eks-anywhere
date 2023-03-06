751bcd016876e12d1a7969e37ed02479d9e42d7c
Sid Shukla <sid.shukla@nutanix.com>
2023-03-06 20:05:53 +0100

    Add some more unit tests

--- pkg/api/v1alpha1/nutanixdatacenterconfig_webhook_test.go
+++ pkg/api/v1alpha1/nutanixdatacenterconfig_webhook_test.go
@@ -3,15 +3,23 @@ package v1alpha1
 import (
 	"testing"
 
+	"github.com/aws/eks-anywhere/pkg/constants"
+
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestNutanixDatacenterConfigWebhookSetsDefaults(t *testing.T) {
+func TestNutanixDatacenterConfigWebhooks(t *testing.T) {
 	g := NewWithT(t)
 	dcConf := nutanixDatacenterConfig()
-	dcConf.SetDefaults()
+	dcConf.Default()
+	g.Expect(dcConf.Spec.CredentialRef.Kind).To(Equal(constants.SecretKind))
+	g.Expect(dcConf.Spec.CredentialRef.Name).To(Equal(constants.NutanixCredentialsName))
 	g.Expect(dcConf.ValidateCreate()).To(Succeed())
+	newConf := dcConf.DeepCopy()
+	newConf.Spec.CredentialRef.Name = "new-creds"
+	g.Expect(dcConf.ValidateUpdate(dcConf)).To(Succeed())
+	g.Expect(newConf.ValidateDelete()).To(Succeed())
 }
 
 func nutanixDatacenterConfig() *NutanixDatacenterConfig {
