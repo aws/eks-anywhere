6194a5624f5d6686e528cd4ac21235d5816e0b91
Guillermo Gaston <gaslor@amazon.com>
2023-03-29 19:52:04 +0000

    Fix clustermanager no-timeouts option
    When the using --no-timeouts and a number of replicas bigger then 1,
    there was an int overflow that made the calculated timeout negative,
    which hit the branch where we select machinesMinWait. This is by default
    30 minutes, so it defeats the purpose of --no-timeouts.

---
+++ pkg/clustermanager/cluster_manager_wb_test.go
@@ -0,0 +1,65 @@
+package clustermanager
+
+import (
+	"math"
+	"testing"
+	"time"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestClusterManager_totalTimeoutForMachinesReadyWait(t *testing.T) {
+	tests := []struct {
+		name     string
+		replicas int
+		opts     []ClusterManagerOpt
+		want     time.Duration
+	}{
+		{
+			name:     "default timeouts with 1 replica",
+			replicas: 1,
+			want:     30 * time.Minute,
+		},
+		{
+			name:     "default timeouts with 2 replicas",
+			replicas: 2,
+			want:     30 * time.Minute,
+		},
+		{
+			name:     "default timeouts with 4 replicas",
+			replicas: 4,
+			want:     40 * time.Minute,
+		},
+		{
+			name:     "no timeouts with 1 replica",
+			replicas: 1,
+			opts:     []ClusterManagerOpt{WithNoTimeouts()},
+			want:     math.MaxInt64,
+		},
+		{
+			name:     "no timeouts with 2 replicas",
+			replicas: 2,
+			opts:     []ClusterManagerOpt{WithNoTimeouts()},
+			want:     math.MaxInt64,
+		},
+		{
+			name:     "no timeouts with 1 replica",
+			replicas: 1,
+			opts:     []ClusterManagerOpt{WithNoTimeouts()},
+			want:     math.MaxInt64,
+		},
+		{
+			name:     "no timeouts with 0 replicas",
+			replicas: 1,
+			opts:     []ClusterManagerOpt{WithNoTimeouts()},
+			want:     math.MaxInt64,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := New(nil, nil, nil, nil, nil, nil, tt.opts...)
+			g := NewWithT(t)
+			g.Expect(c.totalTimeoutForMachinesReadyWait(tt.replicas)).To(Equal(tt.want))
+		})
+	}
+}
