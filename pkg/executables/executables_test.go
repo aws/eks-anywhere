d61a7c8799e73e2438f7a202a6952dead86a7b3d
panktishah26 <shah.pankti2609@gmail.com>
2023-04-05 18:46:01 +0000

    Redact authenticated registry mirror username and password from CLI logs and support bundle

--- pkg/executables/executables_test.go
+++ pkg/executables/executables_test.go
@@ -9,7 +9,9 @@ import (
 
 func TestRedactCreds(t *testing.T) {
 	str := "My username is username123. My password is password456"
-	envMap := map[string]string{"var": "value", constants.VSphereUsernameKey: "username123", constants.VSpherePasswordKey: "password456"}
+	t.Setenv(constants.VSphereUsernameKey, "username123")
+	envMap := map[string]string{"var": "value", constants.VSpherePasswordKey: "password456"}
+
 	expected := "My username is *****. My password is *****"
 
 	redactedStr := executables.RedactCreds(str, envMap)
