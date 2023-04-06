47997ff9a1cf2ffbc2cd94a863ea1d499e31b5c6
Jia Deng <ddjia@amazon.com>
2023-04-06 11:16:52 -0700

    lint

--- pkg/providers/cloudstack/reconciler/reconciler_test.go
+++ pkg/providers/cloudstack/reconciler/reconciler_test.go
@@ -2,11 +2,11 @@ package reconciler_test
 
 import (
 	"context"
-	"github.com/pkg/errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/gomega"
+	"github.com/pkg/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
