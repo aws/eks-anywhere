a769b6f82f4c92bf70225dad7fe66c0e7808a60b
Aravind Ramalingam <ramaliar@amazon.com>
2023-04-10 10:04:07 -0700

    Reading from registry auth secret for FLC

--- pkg/config/registry_test.go
+++ pkg/config/registry_test.go
@@ -1,10 +1,15 @@
 package config
 
 import (
+	"context"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	"github.com/aws/eks-anywhere/pkg/constants"
 )
@@ -24,4 +29,35 @@ func TestReadConfig(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, expectedUser, username)
 	assert.Equal(t, expectedPassword, password)
+}
+
+func TestSetCredentialsEnv(t *testing.T) {
+	uName := ""
+	uPass := ""
+	err := SetCredentialsEnv(uName, uPass)
+	assert.NoError(t, err)
+}
+
+func TestReadCredentialsFromSecret(t *testing.T) {
+	ctx := context.Background()
+	expectedUser := "testuser"
+	expectedPassword := "testpass"
+	sec := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      registryAuthSecretName,
+			Namespace: constants.EksaSystemNamespace,
+		},
+		Data: map[string][]byte{
+			"username": []byte(expectedUser),
+			"password": []byte(expectedPassword),
+		},
+	}
+
+	objs := []runtime.Object{sec}
+	cb := fake.NewClientBuilder()
+	cl := cb.WithRuntimeObjects(objs...).Build()
+	u, p, err := ReadCredentialsFromSecret(ctx, cl)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedUser, u)
+	assert.Equal(t, expectedPassword, p)
 }
