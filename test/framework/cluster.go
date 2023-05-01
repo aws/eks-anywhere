56c91f8760a259b792c6019dbdd98489f279d551
Taylor Neyland <tneyla@amazon.com>
2023-05-01 10:22:03 -0500

    Set 1.27 feature flag in e2e tests

--- test/framework/cluster.go
+++ test/framework/cluster.go
@@ -31,6 +31,7 @@ import (
 	"github.com/aws/eks-anywhere/pkg/cluster"
 	"github.com/aws/eks-anywhere/pkg/constants"
 	"github.com/aws/eks-anywhere/pkg/executables"
+	"github.com/aws/eks-anywhere/pkg/features"
 	"github.com/aws/eks-anywhere/pkg/filewriter"
 	"github.com/aws/eks-anywhere/pkg/git"
 	"github.com/aws/eks-anywhere/pkg/retrier"
@@ -57,7 +58,6 @@ const (
 	hardwareYamlPath                       = "hardware.yaml"
 	hardwareCsvPath                        = "hardware.csv"
 	EksaPackagesInstallation               = "eks-anywhere-packages"
-	unreleasedK8sVersion                   = v1alpha1.Kube127
 )
 
 //go:embed testdata/oidc-roles.yaml
@@ -2130,10 +2130,11 @@ func dumpFile(description string, path string, t T) {
 func (e *ClusterE2ETest) setFeatureFlagForUnreleasedKubernetesVersion(version v1alpha1.KubernetesVersion) {
 	// Update this variable to equal the feature flagged k8s version when applicable.
 	// For example, if k8s 1.26 is under a feature flag, we would set this to v1alpha1.Kube126
-	var unreleasedK8sVersion v1alpha1.KubernetesVersion
+	unreleasedK8sVersion := v1alpha1.Kube127
 
 	if version == unreleasedK8sVersion {
 		// Set feature flag for the unreleased k8s version when applicable
 		e.T.Logf("Setting k8s version support feature flag...")
+		os.Setenv(features.K8s127SupportEnvVar, "true")
 	}
 }
