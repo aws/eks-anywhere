117a34aa324c3b642933fa9c4ad77f7542e010c4
Tanvir Tatla <tatlat@amazon.com>
2023-05-11 10:01:38 -0700

    fix linting

--- pkg/validations/version.go
+++ pkg/validations/version.go
@@ -2,6 +2,7 @@ package validations
 
 import "github.com/aws/eks-anywhere/pkg/version"
 
+// VersionClient is used to mock version package.
 type VersionClient interface {
 	Get() version.Info
 }
--- pkg/version/versionwrapper.go
+++ pkg/version/versionwrapper.go
@@ -1,11 +1,9 @@
 package version
 
+// VersionClient exists to make mocking the version package easier.
 type VersionClient struct{}
 
+// Get returns the version info of eksa.
 func (vc *VersionClient) Get() Info {
 	return Get()
-}
-
-func newVersionClient() *VersionClient {
-	return &VersionClient{}
 }
