1272142b7741d7a0342564bc50abe41f23577c6f
hiteshrepo <pattanayak.hitesh03@gmail.com>
2023-07-13 21:23:08 +0530

    check existence of machine config before further operations

--- pkg/providers/tinkerbell/tinkerbell.go
+++ pkg/providers/tinkerbell/tinkerbell.go
@@ -263,19 +263,23 @@ func (p *Provider) DatacenterConfig(_ *cluster.Spec) providers.DatacenterConfig 
 func (p *Provider) MachineConfigs(_ *cluster.Spec) []providers.MachineConfig {
 	configs := make(map[string]providers.MachineConfig, len(p.machineConfigs))
 	controlPlaneMachineName := p.clusterConfig.Spec.ControlPlaneConfiguration.MachineGroupRef.Name
-	p.machineConfigs[controlPlaneMachineName].Annotations = map[string]string{p.clusterConfig.ControlPlaneAnnotation(): "true"}
-	if p.clusterConfig.IsManaged() {
-		p.machineConfigs[controlPlaneMachineName].SetManagedBy(p.clusterConfig.ManagedBy())
+	if _, ok := p.machineConfigs[controlPlaneMachineName]; ok {
+		p.machineConfigs[controlPlaneMachineName].Annotations = map[string]string{p.clusterConfig.ControlPlaneAnnotation(): "true"}
+		if p.clusterConfig.IsManaged() {
+			p.machineConfigs[controlPlaneMachineName].SetManagedBy(p.clusterConfig.ManagedBy())
+		}
+		configs[controlPlaneMachineName] = p.machineConfigs[controlPlaneMachineName]
 	}
-	configs[controlPlaneMachineName] = p.machineConfigs[controlPlaneMachineName]
 
 	if p.clusterConfig.Spec.ExternalEtcdConfiguration != nil {
 		etcdMachineName := p.clusterConfig.Spec.ExternalEtcdConfiguration.MachineGroupRef.Name
-		p.machineConfigs[etcdMachineName].Annotations = map[string]string{p.clusterConfig.EtcdAnnotation(): "true"}
-		if etcdMachineName != controlPlaneMachineName {
-			configs[etcdMachineName] = p.machineConfigs[etcdMachineName]
-			if p.clusterConfig.IsManaged() {
-				p.machineConfigs[etcdMachineName].SetManagedBy(p.clusterConfig.ManagedBy())
+		if _, ok := p.machineConfigs[etcdMachineName]; ok {
+			p.machineConfigs[etcdMachineName].Annotations = map[string]string{p.clusterConfig.EtcdAnnotation(): "true"}
+			if etcdMachineName != controlPlaneMachineName {
+				configs[etcdMachineName] = p.machineConfigs[etcdMachineName]
+				if p.clusterConfig.IsManaged() {
+					p.machineConfigs[etcdMachineName].SetManagedBy(p.clusterConfig.ManagedBy())
+				}
 			}
 		}
 	}
