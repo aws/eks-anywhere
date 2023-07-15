6fedb80d3ed17b7f009e0b9d7ce86687a9a7093f
Aravind Ramalingam <ramaliar@amazon.com>
2023-07-14 18:14:29 -0700

    Refactoring to fail on vsphere perms validations

--- pkg/validations/createvalidations/createvalidations.go
+++ pkg/validations/createvalidations/createvalidations.go
@@ -4,6 +4,16 @@ import (
 	"github.com/aws/eks-anywhere/pkg/validations"
 )
 
+// string values of supported validation names that can be skipped.
+const (
+	VSphereUserPriv = "vsphere-user-privilege"
+)
+
+// SkippableValidations represents all the validations we offer for users to skip.
+var SkippableValidations = []string{
+	VSphereUserPriv,
+}
+
 func New(opts *validations.Opts) *CreateValidations {
 	opts.SetDefaults()
 	return &CreateValidations{Opts: opts}
--- pkg/validations/upgradevalidations/upgradevalidations.go
+++ pkg/validations/upgradevalidations/upgradevalidations.go
@@ -4,6 +4,16 @@ import (
 	"github.com/aws/eks-anywhere/pkg/validations"
 )
 
+// string values of supported validation names that can be skipped.
+const (
+	PDB = "pod-disruption"
+)
+
+// SkippableValidations represents all the validations we offer for users to skip.
+var SkippableValidations = []string{
+	PDB,
+}
+
 func New(opts *validations.Opts) *UpgradeValidations {
 	opts.SetDefaults()
 	return &UpgradeValidations{Opts: opts}
