78c645ba74e2718002844c847cd853accc9950b3
Cavaughn Browne <cxbrowne@amazon.com>
2023-07-26 23:39:28 +0000

    fix nil pointer when using cni unsupported by the controller

--- pkg/controller/clusters/status.go
+++ pkg/controller/clusters/status.go
@@ -58,7 +58,7 @@ func UpdateClusterStatusForCNI(ctx context.Context, cluster *anywherev1.Cluster)
 		ciliumCfg := cluster.Spec.ClusterNetwork.CNIConfig.Cilium
 		// Though it may be installed initially to successfully create the cluster,
 		// if the CNI is configured to skip upgrades, we mark the condition as "False"
-		if !ciliumCfg.IsManaged() {
+		if ciliumCfg != nil && !ciliumCfg.IsManaged() {
 			conditions.MarkFalse(cluster, anywherev1.DefaultCNIConfiguredCondition, anywherev1.SkipUpgradesForDefaultCNIConfiguredReason, clusterv1.ConditionSeverityWarning, "Configured to skip default Cilium CNI upgrades")
 			return
 		}
