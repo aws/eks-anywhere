b0b2ee3a6bc4b3a4db67e2b39867959bcd3aeaa4
Rahul Ganesh <rahulgab@amazon.com>
2023-07-26 14:45:13 -0700

    tinkerbell specific flow for harbor packages tests
    Signed-off-by: Rahul Ganesh <rahulgab@amazon.com>

--- test/e2e/harbor.go
+++ test/e2e/harbor.go
@@ -29,13 +29,31 @@ func runCuratedPackageHarborInstallSimpleFlowLocalStorageProvisioner(test *frame
 	})
 }
 
+func runCuratedPackageHarborInstall(test *framework.ClusterE2ETest) {
+	test.InstallLocalStorageProvisioner()
+	packagePrefix := "test"
+	installNs := "harbor"
+	test.CreateNamespace(installNs)
+	test.InstallCuratedPackage("harbor", packagePrefix, kubeconfig.FromClusterName(test.ClusterName),
+		"--set secretKey=use-a-secret-key",
+		"--set expose.tls.certSource=auto",
+		"--set expose.tls.auto.commonName=localhost",
+		"--set persistence.persistentVolumeClaim.registry.storageClass=local-path",
+		"--set persistence.persistentVolumeClaim.jobservice.jobLog.storageClass=local-path",
+		"--set persistence.persistentVolumeClaim.database.storageClass=local-path",
+		"--set persistence.persistentVolumeClaim.redis.storageClass=local-path",
+		"--set persistence.persistentVolumeClaim.trivy.storageClass=local-path",
+	)
+	test.VerifyHarborPackageInstalled(packagePrefix, installNs)
+}
+
 func runCuratedPackageHarborInstallTinkerbellSimpleFlow(test *framework.ClusterE2ETest) {
 	test.GenerateClusterConfig()
 	test.GenerateHardwareConfig()
 	test.PowerOnHardware()
 	test.CreateCluster(framework.WithControlPlaneWaitTimeout("20m"))
 	test.ValidateControlPlaneNodes(framework.ValidateControlPlaneNoTaints, framework.ValidateControlPlaneLabels)
-	runCuratedPackageHarborInstallSimpleFlowLocalStorageProvisioner(test)
+	runCuratedPackageHarborInstall(test)
 	test.DeleteCluster()
 	test.PowerOffHardware()
 	test.ValidateHardwareDecommissioned()
