19d94e2684f4c573d29c3d400053779c1ee50740
Xu Deng <xudeng@amazon.com>
2023-08-03 10:54:29 -0400

    Fix copy packages command for airgapped environment (#6359)

--- pkg/registry/copy.go
+++ pkg/registry/copy.go
@@ -3,6 +3,8 @@ package registry
 import (
 	"context"
 	"fmt"
+
+	"github.com/aws/eks-anywhere/pkg/logger"
 )
 
 // Copy an image from a source to a destination.
@@ -23,6 +25,7 @@ func Copy(ctx context.Context, srcClient StorageClient, dstClient StorageClient,
 	}
 
 	if len(image.Tag) > 0 {
+		logger.V(6).Info("Tagging image", "repository", image.Repository, "tag", image.Tag)
 		err = dstClient.Tag(ctx, dstStorage, desc, image.Tag)
 		if err != nil {
 			return fmt.Errorf("image tag: %v", err)
