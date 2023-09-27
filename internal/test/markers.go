afed219b301243e7c3ca4be42704a481762633fc
Jacob Weinstock <jakobweinstock@gmail.com>
2023-09-27 16:05:50 -0600

    Fix linting issue
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- internal/test/markers.go
+++ internal/test/markers.go
@@ -5,8 +5,11 @@ import (
 	"testing"
 )
 
+// IntegrationTestEnvVar is the name of the environment variable that gates when integration tests are run.
 const IntegrationTestEnvVar = "INTEGRATION_TESTS_ENABLED"
 
+// MarkIntegration marks a test as an integration test.
+// Integration tests are only run when the environment variable INTEGRATION_TESTS_ENABLED is set to true.
 func MarkIntegration(t *testing.T) {
 	t.Helper()
 	if os.Getenv(IntegrationTestEnvVar) != "true" {
