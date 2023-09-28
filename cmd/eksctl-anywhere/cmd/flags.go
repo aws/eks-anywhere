0651f64da50259b344eb2d52b905d3c0d4db471d
Jacob Weinstock <jakobweinstock@gmail.com>
2023-09-28 13:56:13 -0600

    Enable CLI flags to be mapped to env vars:
    This enables passing env vars instead of cli flags.
    Will be used for BMC secrets.

--- cmd/eksctl-anywhere/cmd/flags.go
+++ cmd/eksctl-anywhere/cmd/flags.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
@@ -25,14 +26,24 @@ https://anywhere.eks.amazonaws.com/docs/troubleshooting/troubleshooting/#bootstr
 )
 
 func bindFlagsToViper(cmd *cobra.Command, args []string) error {
-	var err error
 	cmd.Flags().VisitAll(func(flag *pflag.Flag) {
-		if err != nil {
+		if err := viper.BindPFlag(flag.Name, flag); err != nil {
 			return
 		}
-		err = viper.BindPFlag(flag.Name, flag)
+		viper.AutomaticEnv()
+		// Environment variables can't have dashes in them, so bind them to their equivalent
+		// keys with underscores, e.g. --hardware-csv to HARDWARE_CSV
+		viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+		// viper.AutomaticEnv() needs help with dashes in flag names.
+		if !flag.Changed && viper.IsSet(flag.Name) {
+			val := viper.Get(flag.Name)
+			if err := cmd.Flags().Set(flag.Name, fmt.Sprintf("%v", val)); err != nil {
+				return
+			}
+		}
 	})
-	return err
+
+	return nil
 }
 
 func applyClusterOptionFlags(flagSet *pflag.FlagSet, clusterOpt *clusterOptions) {
