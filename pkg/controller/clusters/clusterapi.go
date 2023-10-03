22ba7bf3d0cdf6f2a85d669471d436f34d830b11
Pankti Shah <shah.pankti2609@gmail.com>
2023-10-03 14:52:14 -0700

    Waiting for control plane to be fully upgraded before upgrading other components

--- pkg/controller/clusters/clusterapi.go
+++ pkg/controller/clusters/clusterapi.go
@@ -11,13 +11,40 @@ import (
 	anywherev1 "github.com/aws/eks-anywhere/pkg/api/v1alpha1"
 	"github.com/aws/eks-anywhere/pkg/clusterapi"
 	"github.com/aws/eks-anywhere/pkg/controller"
+	"github.com/aws/eks-anywhere/pkg/features"
 )
 
-// CheckControlPlaneReady is a controller helper to check whether a CAPI cluster CP for
-// an eks-a cluster is ready or not. This is intended to be used from cluster reconcilers
+// CheckControlPlaneReady is a controller helper to check whether KCP object for
+// the cluster is ready or not. This is intended to be used from cluster reconcilers
 // due its signature and that it returns controller results with appropriate wait times whenever
 // the cluster is not ready.
 func CheckControlPlaneReady(ctx context.Context, client client.Client, log logr.Logger, cluster *anywherev1.Cluster) (controller.Result, error) {
+	if features.IsActive(features.ExperimentalSelfManagedClusterUpgrade()) {
+		kcp, err := controller.GetKubeadmControlPlane(ctx, client, cluster)
+		if err != nil {
+			return controller.Result{}, err
+		}
+
+		if kcp == nil {
+			log.Info("KCP does not exist yet, requeuing")
+			return controller.ResultWithRequeue(5 * time.Second), nil
+		}
+
+		// We make sure to check that the status is up to date before using it
+		if kcp.Status.ObservedGeneration != kcp.ObjectMeta.Generation {
+			log.Info("KCP information is outdated, requeing")
+			return controller.ResultWithRequeue(5 * time.Second), nil
+		}
+
+		if !conditions.IsTrue(kcp, clusterapi.ReadyCondition) {
+			log.Info("KCP is not ready yet, requeing")
+			return controller.ResultWithRequeue(30 * time.Second), nil
+		}
+
+		log.Info("KCP is ready")
+		return controller.Result{}, nil
+	}
+
 	capiCluster, err := controller.GetCAPICluster(ctx, client, cluster)
 	if err != nil {
 		return controller.Result{}, err
