8053f95cc2f497570dc00d6c3718b6a732ecf943
Pankti Shah <shah.pankti2609@gmail.com>
2023-10-18 16:04:08 -0700

    Enable kindless management cluster upgrade for snow provider

--- test/framework/cluster.go
+++ test/framework/cluster.go
@@ -121,7 +121,7 @@ func NewClusterE2ETest(t T, provider Provider, opts ...ClusterE2ETestOpt) *Clust
 
 	// For kindless management upgrade task
 	// Remove this once we remove feature flag for ExpSelfManagedAPIUpgrade.
-	if provider.Name() == constants.VSphereProviderName || provider.Name() == constants.CloudStackProviderName || provider.Name() == constants.TinkerbellProviderName {
+	if provider.Name() == constants.VSphereProviderName || provider.Name() == constants.CloudStackProviderName || provider.Name() == constants.TinkerbellProviderName || provider.Name() == constants.SnowProviderName {
 		opts = append(opts, WithEnvVar(features.ExperimentalSelfManagedClusterUpgradeGate, "true"))
 		opts = append(opts, WithEnvVar(features.ExperimentalSelfManagedClusterUpgradeEnvVar, "true"))
 	}
