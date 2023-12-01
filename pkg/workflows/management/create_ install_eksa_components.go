d81640f5abb2c6b2ddc30e55e4f8b22f0c90afc6
Mitali Paygude <mitalipaygude@gmail.com>
2023-12-01 01:49:58 -0800

    combine eksa components

--- pkg/workflows/management/create_ install_eksa_components.go
+++ pkg/workflows/management/create_ install_eksa_components.go
@@ -5,32 +5,15 @@ import (
 
 	"github.com/aws/eks-anywhere/pkg/logger"
 	"github.com/aws/eks-anywhere/pkg/task"
+	"github.com/aws/eks-anywhere/pkg/types"
 	"github.com/aws/eks-anywhere/pkg/workflows"
 )
 
 type installEksaComponentsTask struct{}
 
 func (s *installEksaComponentsTask) Run(ctx context.Context, commandContext *task.CommandContext) task.Task {
-	logger.Info("Installing EKS-A custom components (CRD and controller) on bootstrap cluster")
-	err := commandContext.ClusterManager.InstallCustomComponents(ctx, commandContext.ClusterSpec, commandContext.BootstrapCluster, commandContext.Provider)
-	if err != nil {
-		commandContext.SetError(err)
-		return &workflows.CollectDiagnosticsTask{}
-	}
-	logger.Info("Installing EKS-D components on bootstrap cluster")
-	err = commandContext.EksdInstaller.InstallEksdCRDs(ctx, commandContext.ClusterSpec, commandContext.BootstrapCluster)
-	if err != nil {
-		commandContext.SetError(err)
-		return &workflows.CollectDiagnosticsTask{}
-	}
-
-	logger.Info("Creating EKS-A CRDs instances on bootstrap cluster")
-	err = commandContext.ClusterManager.CreateEKSAReleaseBundle(ctx, commandContext.BootstrapCluster, commandContext.ClusterSpec)
-	if err != nil {
-		commandContext.SetError(err)
-		return &workflows.CollectDiagnosticsTask{}
-	}
-	err = commandContext.EksdInstaller.InstallEksdManifest(ctx, commandContext.ClusterSpec, commandContext.BootstrapCluster)
+	logger.Info("Installing EKS-A custom components on bootstrap cluster")
+	err := installEKSAComponents(ctx, commandContext, commandContext.BootstrapCluster)
 	if err != nil {
 		commandContext.SetError(err)
 		return &workflows.CollectDiagnosticsTask{}
@@ -54,35 +37,16 @@ func (s *installEksaComponentsTask) Checkpoint() *task.CompletedTask {
 type installEksaComponentsOnWorkloadTask struct{}
 
 func (s *installEksaComponentsOnWorkloadTask) Run(ctx context.Context, commandContext *task.CommandContext) task.Task {
-	logger.Info("Installing EKS-A custom components (CRD and controller) on workload cluster")
-	err := commandContext.ClusterManager.InstallCustomComponents(ctx, commandContext.ClusterSpec, commandContext.WorkloadCluster, commandContext.Provider)
-	if err != nil {
-		commandContext.SetError(err)
-		return &workflows.CollectDiagnosticsTask{}
-	}
-	logger.Info("Installing EKS-D components on workload cluster")
-	err = commandContext.EksdInstaller.InstallEksdCRDs(ctx, commandContext.ClusterSpec, commandContext.WorkloadCluster)
-	if err != nil {
-		commandContext.SetError(err)
-		return &workflows.CollectDiagnosticsTask{}
-	}
+	logger.Info("Installing EKS-A custom components on workload cluster")
 
-	logger.Info("Creating EKS-A CRDs instances on workload cluster")
-
-	err = commandContext.ClusterManager.CreateEKSAReleaseBundle(ctx, commandContext.WorkloadCluster, commandContext.ClusterSpec)
-	if err != nil {
-		commandContext.SetError(err)
-		return &workflows.CollectDiagnosticsTask{}
-	}
-
-	err = commandContext.EksdInstaller.InstallEksdManifest(ctx, commandContext.ClusterSpec, commandContext.WorkloadCluster)
+	err := installEKSAComponents(ctx, commandContext, commandContext.WorkloadCluster)
 	if err != nil {
 		commandContext.SetError(err)
 		return &workflows.CollectDiagnosticsTask{}
 	}
 
 	logger.Info("Applying cluster spec to workload cluster")
-	if err := commandContext.ClusterUpgrader.Run(ctx, commandContext.ClusterSpec, *commandContext.WorkloadCluster); err != nil {
+	if err = commandContext.ClusterUpgrader.Run(ctx, commandContext.ClusterSpec, *commandContext.WorkloadCluster); err != nil {
 		commandContext.SetError(err)
 		return &workflows.CollectMgmtClusterDiagnosticsTask{}
 	}
@@ -99,5 +63,36 @@ func (s *installEksaComponentsOnWorkloadTask) Restore(ctx context.Context, comma
 }
 
 func (s *installEksaComponentsOnWorkloadTask) Checkpoint() *task.CompletedTask {
+	return nil
+}
+
+
+func installEKSAComponents(ctx context.Context, commandContext *task.CommandContext, targetCluster *types.Cluster) error {
+
+	logger.Info("Installing EKS-A custom components (CRD and controller)")
+	err := commandContext.ClusterManager.InstallCustomComponents(ctx, commandContext.ClusterSpec, targetCluster, commandContext.Provider)
+	if err != nil {
+		return err
+	}
+	logger.Info("Installing EKS-D components")
+	err = commandContext.EksdInstaller.InstallEksdCRDs(ctx, commandContext.ClusterSpec, targetCluster)
+	if err != nil {
+		commandContext.SetError(err)
+		return err
+	}
+
+	logger.Info("Creating EKS-A CRDs instances")
+
+	err = commandContext.ClusterManager.CreateEKSAReleaseBundle(ctx, targetCluster, commandContext.ClusterSpec)
+	if err != nil {
+		commandContext.SetError(err)
+		return err
+	}
+
+	err = commandContext.EksdInstaller.InstallEksdManifest(ctx, commandContext.ClusterSpec, targetCluster)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
