d380699d69af0fb4e9e09724ca38f6a7cc05b648
Vignesh Goutham Ganesh <vgg@amazon.com>
2023-12-12 13:09:59 -0600

    metadata changes

--- tools/support-bundle-ui.go
+++ tools/support-bundle-ui.go
@@ -5,24 +5,123 @@ import (
 	"compress/gzip"
 	"encoding/json"
 	"fmt"
+	"fyne.io/fyne/v2"
 	"image/color"
 	"io"
 	"os"
 	"path/filepath"
+	"sigs.k8s.io/yaml"
+	"strconv"
 	"strings"
 
-	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 
 	"github.com/replicatedhq/troubleshoot/pkg/convert"
 )
 
-func extractErrorsFromSupportBundle(w fyne.Window, errorContainer *fyne.Container) {
+// Display the following metadata about the cluster
+// - eks-a version
+// - k8s version
+// - cp and worker node group replicas
+// - os family
+// - provider
+// - etcd stacked / unstacked with replicas
+// - registry mirror true/false
+func addClusterMetadata(w fyne.Window, container *fyne.Container, supportBundleFolderPath string, errorResults []*convert.Result) {
+	// Set label for cluster meta data
+	clusterMetaDataLabel := &widget.Label{
+		BaseWidget: widget.BaseWidget{},
+		Text:       "Cluster Metadata",
+		Alignment:  0,
+		Wrapping:   0,
+		TextStyle: fyne.TextStyle{
+			Bold: true,
+		},
+		Truncation: 0,
+		Importance: 0,
+	}
+	container.Add(clusterMetaDataLabel)
+
+	// Parse cluster metadata from support bundle files
+	crdPath := filepath.Join(supportBundleFolderPath, "cluster-resources", "custom-resource-definitions")
+	clusterSpecFilePath := filepath.Join(crdPath, "clusters.anywhere.eks.amazonaws.com", "default.yaml")
+	var clusterSpec []map[string]interface{}
+	contents, err := os.ReadFile(clusterSpecFilePath)
+	if err != nil {
+		dialog.ShowError(err, w)
+		return
+	}
+	err = yaml.Unmarshal(contents, &clusterSpec)
+	if err != nil {
+		dialog.ShowError(err, w)
+		return
+	}
+
+	// Cards for metadata
+	spec := clusterSpec[0]["spec"].(map[string]interface{})
+	version := spec["eksaVersion"]
+	eksaVersion := ""
+	if version != nil {
+		eksaVersion = version.(string)
+	}
+	container.Add(getCard("EKS-A Version", eksaVersion, 18, color.Black))
+
+	// k8s version
+	k8sVersion := spec["kubernetesVersion"].(string)
+	container.Add(getCard("K8s Version", k8sVersion, 18, color.Black))
+
+	// Provider
+	dcRef := spec["datacenterRef"].(map[string]interface{})
+	dcKind := dcRef["kind"].(string)
+	var provider string
+	switch dcKind {
+	case "VSphereDatacenterConfig":
+		provider = "vSphere"
+	case "TinkerbellDatacenterConfig":
+		provider = "Tinkerbell"
+	case "CloudStackDatacenterConfig":
+		provider = "CloudStack"
+	default:
+		provider = "Snow/Nutanix/Docker"
+	}
+	container.Add(getCard("Provider", provider, 18, color.Black))
+
+	extEtcd := spec["externalEtcdConfiguration"]
+	isExtEtcd := false
+	if extEtcd != nil {
+		isExtEtcd = true
+	}
+	container.Add(getCard("External Etcd", fmt.Sprintf("%t", isExtEtcd), 18, color.Black))
+
+	// CP count
+	cpConf := spec["controlPlaneConfiguration"].(map[string]interface{})
+	cpCount := cpConf["count"].(float64)
+	container.Add(getCard("CP Replica", strconv.FormatFloat(cpCount, 'f', -1, 64), 18, color.Black))
+
+	// OS Family
+}
+
+func getCard(title, content string, textsize float32, color color.Color) *widget.Card {
+	contentText := canvas.NewText(content, color)
+	contentText.Alignment = fyne.TextAlignCenter
+	contentText.TextSize = textsize
+	card := &widget.Card{
+		BaseWidget: widget.BaseWidget{},
+		Title:      title,
+		Subtitle:   "",
+		Image:      nil,
+		Content:    contentText,
+	}
+	return card
+}
+
+func extractErrorsFromSupportBundle(w fyne.Window, metadataContainer, errorContainer *fyne.Container) {
 	dialog.ShowFileOpen(func(dir fyne.URIReadCloser, err error) {
 		fileSelected := "No path selected"
 		if err != nil {
@@ -52,11 +151,11 @@ func extractErrorsFromSupportBundle(w fyne.Window, errorContainer *fyne.Containe
 
 		var results []*convert.Result
 		analysisFile := filepath.Join(supportBundleFolderPath, "analysis.json")
-		contents, err := os.ReadFile(analysisFile)
-		if err != nil {
-			dialog.ShowError(err, w)
-			return
-		}
+		contents, _ := os.ReadFile(analysisFile)
+		//if err != nil {
+		//	dialog.ShowError(err, w)
+		//	return
+		//}
 
 		fmt.Printf("Unmarshaling analysis file: %s\n", analysisFile)
 		err = json.Unmarshal(contents, &results)
@@ -65,7 +164,16 @@ func extractErrorsFromSupportBundle(w fyne.Window, errorContainer *fyne.Containe
 			return
 		}
 
+		addClusterMetadata(w, metadataContainer, supportBundleFolderPath, results)
 		for _, result := range results {
+			reportCard := &widget.Card{}
+			if result.Severity == convert.SeverityError {
+				reportCard.Title = result.Name
+				decoratedError := fmt.Sprintf("❌ %s", result.Error)
+				errorText := canvas.NewText(decoratedError, color.NRGBA{R: 255, A: 255})
+				reportCard.Content = errorText
+				errorContainer.Add(reportCard)
+			}
 			reportAccordion := &widget.Accordion{}
 			if result.Severity == convert.SeverityError {
 				decoratedError := fmt.Sprintf("❌ %s", result.Error)
@@ -159,9 +267,10 @@ func main() {
 	logo.SetMinSize(windowSize)
 
 	hello := widget.NewLabelWithStyle("Support Bundle Analyzer", fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
+	metadataContainer := container.New(layout.NewAdaptiveGridLayout(4))
 	errorContainer := container.NewVBox()
 	selectButton := widget.NewButtonWithIcon("Select Support Bundle file", theme.FolderOpenIcon(), func() {
-		extractErrorsFromSupportBundle(w, errorContainer)
+		extractErrorsFromSupportBundle(w, metadataContainer, errorContainer)
 	})
 	exitButton := widget.NewButtonWithIcon("Exit", theme.LogoutIcon(), func() {
 		fmt.Println("Exiting")
@@ -176,6 +285,7 @@ func main() {
 		selectButton,
 		startOverButton,
 		exitButton,
+		metadataContainer,
 		errorContainer,
 	)
 
