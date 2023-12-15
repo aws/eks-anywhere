a6086df2683c90c01fa47d87360bd2e28916ef58
cxbrowne1207 <cxbrowne@amazon.com>
2023-12-15 18:15:19 +0000

    extract code from poluted pkg/helm/factory.go file into client.go and config.go

--- pkg/helm/factory.go
+++ pkg/helm/factory.go
@@ -11,57 +11,6 @@ import (
 	"github.com/aws/eks-anywhere/pkg/registrymirror"
 )
 
-// Config contains configuration options for Helm.
-type Config struct {
-	RegistryMirror *registrymirror.RegistryMirror
-	ProxyConfig    map[string]string
-	Insecure       bool
-}
-
-// Opt is a functional option for configuring Helm behavior.
-type Opt func(*Config)
-
-// Client represents a Helm client.
-type Client interface {
-	PushChart(ctx context.Context, chart, registry string) error
-	PullChart(ctx context.Context, ociURI, version string) error
-	ListCharts(ctx context.Context, kubeconfigFilePath string) ([]string, error)
-	SaveChart(ctx context.Context, ociURI, version, folder string) error
-	Delete(ctx context.Context, kubeconfigFilePath, installName, namespace string) error
-	UpgradeChartWithValuesFile(ctx context.Context, chart, ociURI, version, kubeconfigFilePath, valuesFilePath string, opts ...Opt) error
-	InstallChartWithValuesFile(ctx context.Context, chart, ociURI, version, kubeconfigFilePath, valuesFilePath string) error
-	InstallChart(ctx context.Context, chart, ociURI, version, kubeconfigFilePath, namespace, valueFilePath string, skipCRDs bool, values []string) error
-	Template(ctx context.Context, ociURI, version, namespace string, values interface{}, kubeVersion string) ([]byte, error)
-	RegistryLogin(ctx context.Context, registry, username, password string) error
-}
-
-// WithRegistryMirror sets up registry mirror for helm.
-func WithRegistryMirror(mirror *registrymirror.RegistryMirror) Opt {
-	return func(c *Config) {
-		c.RegistryMirror = mirror
-	}
-}
-
-// WithInsecure configures helm to skip validating TLS certificates when
-// communicating with the Kubernetes API.
-func WithInsecure() Opt {
-	return func(c *Config) {
-		c.Insecure = true
-	}
-}
-
-// WithProxyConfig sets the proxy configurations on the Config.
-// proxyConfig contains configurations for the proxy settings used by the Helm client.
-// The valid keys are as follows:
-// "HTTPS_PROXY": Specifies the HTTPS proxy URL.
-// "HTTP_PROXY": Specifies the HTTP proxy URL.
-// "NO_PROXY": Specifies a comma-separated a list of destination domain names, domains, IP addresses, or other network CIDRs to exclude proxying.
-func WithProxyConfig(proxyConfig map[string]string) Opt {
-	return func(c *Config) {
-		c.ProxyConfig = proxyConfig
-	}
-}
-
 // ClientBuilder builds a Helm Client.
 type ClientBuilder interface {
 	BuildHelm(...Opt) Client
