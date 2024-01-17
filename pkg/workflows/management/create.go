e890122e13375d735b0199335ea94fb132bf7d61
Mitali Paygude <mitalipaygude@gmail.com>
2024-01-17 08:38:36 -0800

    Create management cluster using controller validation task (#7307)

---
+++ pkg/workflows/management/create.go
@@ -0,0 +1,60 @@
+package management
+
+import (
+	"context"
+
+	"github.com/aws/eks-anywhere/pkg/cluster"
+	"github.com/aws/eks-anywhere/pkg/filewriter"
+	"github.com/aws/eks-anywhere/pkg/providers"
+	"github.com/aws/eks-anywhere/pkg/task"
+	"github.com/aws/eks-anywhere/pkg/workflows/interfaces"
+)
+
+// Create is a schema for create cluster.
+type Create struct {
+	bootstrapper     interfaces.Bootstrapper
+	provider         providers.Provider
+	clusterManager   interfaces.ClusterManager
+	gitOpsManager    interfaces.GitOpsManager
+	writer           filewriter.FileWriter
+	eksdInstaller    interfaces.EksdInstaller
+	packageInstaller interfaces.PackageInstaller
+	clusterCreator   interfaces.ClusterCreator
+}
+
+// NewCreate builds a new create construct.
+func NewCreate(bootstrapper interfaces.Bootstrapper, provider providers.Provider,
+	clusterManager interfaces.ClusterManager, gitOpsManager interfaces.GitOpsManager,
+	writer filewriter.FileWriter, eksdInstaller interfaces.EksdInstaller,
+	packageInstaller interfaces.PackageInstaller,
+	clusterCreator interfaces.ClusterCreator,
+) *Create {
+	return &Create{
+		bootstrapper:     bootstrapper,
+		provider:         provider,
+		clusterManager:   clusterManager,
+		gitOpsManager:    gitOpsManager,
+		writer:           writer,
+		eksdInstaller:    eksdInstaller,
+		packageInstaller: packageInstaller,
+		clusterCreator:   clusterCreator,
+	}
+}
+
+// Run runs all the create management cluster tasks.
+func (c *Create) Run(ctx context.Context, clusterSpec *cluster.Spec, validator interfaces.Validator) error {
+	commandContext := &task.CommandContext{
+		Bootstrapper:     c.bootstrapper,
+		Provider:         c.provider,
+		ClusterManager:   c.clusterManager,
+		GitOpsManager:    c.gitOpsManager,
+		ClusterSpec:      clusterSpec,
+		Writer:           c.writer,
+		Validations:      validator,
+		EksdInstaller:    c.eksdInstaller,
+		PackageInstaller: c.packageInstaller,
+		ClusterCreator:   c.clusterCreator,
+	}
+
+	return task.NewTaskRunner(&setupAndValidateCreate{}, c.writer).RunTask(ctx, commandContext)
+}
