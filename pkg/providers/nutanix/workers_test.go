003a554bd3572717016b9794a53703e40f19cf0e
Raymond Zhang <67798267+raymond-zhang00@users.noreply.github.com>
2024-01-19 17:22:48 -0600

    adding nutanix upgrade rollout strategy (#7316)
    * adding nutanix upgrade rollout strategy
    
    * refactor nutanix upgrade rollout strategy tests

--- pkg/providers/nutanix/workers_test.go
+++ pkg/providers/nutanix/workers_test.go
@@ -8,7 +8,9 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/aws/eks-anywhere/internal/test"
+	"github.com/aws/eks-anywhere/pkg/api/v1alpha1"
 	"github.com/aws/eks-anywhere/pkg/constants"
+	"github.com/aws/eks-anywhere/pkg/utils/ptr"
 )
 
 func TestWorkersSpec(t *testing.T) {
@@ -21,4 +23,33 @@ func TestWorkersSpec(t *testing.T) {
 	workers, err := WorkersSpec(context.TODO(), logger, client, spec)
 	require.NoError(t, err)
 	assert.Len(t, workers.Groups, 2)
+}
+
+func TestWorkersSpecWithUpgradeRolloutStrategy(t *testing.T) {
+	t.Setenv(constants.EksaNutanixUsernameKey, "admin")
+	t.Setenv(constants.EksaNutanixPasswordKey, "password")
+
+	logger := test.NewNullLogger()
+	client := test.NewFakeKubeClient()
+	spec := test.NewFullClusterSpec(t, "testdata/eksa-cluster.yaml")
+	spec.Cluster.Spec.WorkerNodeGroupConfigurations = []v1alpha1.WorkerNodeGroupConfiguration{
+		{
+			Count: ptr.Int(4),
+			MachineGroupRef: &v1alpha1.Ref{
+				Name: "eksa-unit-test",
+			},
+			Name: "eksa-unit-test",
+			UpgradeRolloutStrategy: &v1alpha1.WorkerNodesUpgradeRolloutStrategy{
+				RollingUpdate: v1alpha1.WorkerNodesRollingUpdateParams{
+					MaxSurge:       1,
+					MaxUnavailable: 0,
+				},
+			},
+		},
+	}
+	workers, err := WorkersSpec(context.TODO(), logger, client, spec)
+	require.NoError(t, err)
+	assert.Len(t, workers.Groups, 1)
+	assert.Equal(t, int32(1), workers.Groups[0].MachineDeployment.Spec.Strategy.RollingUpdate.MaxSurge.IntVal)
+	assert.Equal(t, int32(0), workers.Groups[0].MachineDeployment.Spec.Strategy.RollingUpdate.MaxUnavailable.IntVal)
 }
