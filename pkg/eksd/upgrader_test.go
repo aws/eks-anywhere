658ed5924a09578e8deba569fb0cdceaff5fa5ac
Cavaughn Browne <cxbrowne@amazon.com>
2024-01-31 23:35:01 +0000

    add eksd kubernetes to upgrade plan

--- pkg/eksd/upgrader_test.go
+++ pkg/eksd/upgrader_test.go
@@ -34,7 +34,9 @@ func newUpgraderTest(t *testing.T) *upgraderTest {
 	client := mocks.NewMockEksdInstallerClient(ctrl)
 	reader := m.NewMockReader(ctrl)
 	currentSpec := test.NewClusterSpec(func(s *cluster.Spec) {
-		s.VersionsBundles["1.19"].EksD.Name = "eks-d-1"
+		s.VersionsBundles["1.19"].EksD.Name = "kubernetes-1-19-eks-1"
+		s.VersionsBundles["1.19"].EksD.ReleaseChannel = "1-19"
+		s.VersionsBundles["1.19"].EksD.KubeVersion = "v1.19.1"
 	})
 
 	return &upgraderTest{
@@ -68,34 +70,43 @@ func TestEksdUpgradeNoChanges(t *testing.T) {
 func TestEksdUpgradeSuccess(t *testing.T) {
 	tt := newUpgraderTest(t)
 
-	tt.newSpec.VersionsBundles["1.19"].EksD.Name = "eks-d-2"
+	tt.newSpec.VersionsBundles["1.19"].EksD.Name = "kubernetes-1-19-eks-2"
 	tt.newSpec.Bundles = bundle()
-
-	wantDiff := &types.ChangeDiff{
-		ComponentReports: []types.ComponentChangeDiff{
-			{
-				ComponentName: "EKS-D",
-				NewVersion:    "eks-d-2",
-				OldVersion:    "eks-d-1",
-			},
-		},
-	}
 
 	tt.reader.EXPECT().ReadFile(testdataFile).Return([]byte("test data"), nil)
 	tt.client.EXPECT().ApplyKubeSpecFromBytesWithNamespace(tt.ctx, tt.cluster, []byte("test data"), constants.EksaSystemNamespace).Return(nil)
-	tt.Expect(tt.eksdUpgrader.Upgrade(tt.ctx, tt.cluster, tt.currentSpec, tt.newSpec)).To(Equal(wantDiff))
+	tt.Expect(tt.eksdUpgrader.Upgrade(tt.ctx, tt.cluster, tt.currentSpec, tt.newSpec)).To(BeNil())
 }
 
 func TestUpgraderEksdUpgradeInstallError(t *testing.T) {
 	tt := newUpgraderTest(t)
 	tt.eksdUpgrader.SetRetrier(retrier.NewWithMaxRetries(1, 0))
-	tt.newSpec.VersionsBundles["1.19"].EksD.Name = "eks-d-2"
+	tt.newSpec.VersionsBundles["1.19"].EksD.Name = "kubernetes-1-19-eks-2"
 	tt.newSpec.Bundles = bundle()
 	tt.newSpec.Bundles.Spec.VersionsBundles[0].EksD.Components = ""
 	tt.newSpec.Bundles.Spec.VersionsBundles[1].EksD.Components = ""
 
 	tt.reader.EXPECT().ReadFile(tt.newSpec.Bundles.Spec.VersionsBundles[0].EksD.Components).Return([]byte(""), fmt.Errorf("error"))
 	// components file not set so this should return an error in failing to load manifest
-	_, err := tt.eksdUpgrader.Upgrade(tt.ctx, tt.cluster, tt.currentSpec, tt.newSpec)
+	err := tt.eksdUpgrader.Upgrade(tt.ctx, tt.cluster, tt.currentSpec, tt.newSpec)
 	tt.Expect(err).NotTo(BeNil())
+}
+
+func TestChangeDiff(t *testing.T) {
+	tt := newUpgraderTest(t)
+
+	tt.newSpec.VersionsBundles["1.19"].EksD.Name = "kubernetes-1-19-eks-2"
+	tt.newSpec.Bundles = bundle()
+
+	wantDiff := &types.ChangeDiff{
+		ComponentReports: []types.ComponentChangeDiff{
+			{
+				ComponentName: "kubernetes",
+				OldVersion:    "v1.19.1-eks-1-19-1",
+				NewVersion:    "v1.19.1-eks-1-19-2",
+			},
+		},
+	}
+
+	tt.Expect(eksd.ChangeDiff(tt.currentSpec, tt.newSpec)).To(Equal(wantDiff))
 }
