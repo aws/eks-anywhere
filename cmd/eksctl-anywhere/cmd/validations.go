6015b69011f13b3f751b352a484f5097fa91cefc
Cavaughn Browne <113555337+cxbrowne1207@users.noreply.github.com>
2024-02-09 14:47:04 -0600

    remove cluster config validation from common validations (#7543)

--- cmd/eksctl-anywhere/cmd/validations.go
+++ cmd/eksctl-anywhere/cmd/validations.go
@@ -20,7 +20,8 @@ func commonValidation(ctx context.Context, clusterConfigFile string) (*v1alpha1.
 	if !clusterConfigFileExist {
 		return nil, fmt.Errorf("the cluster config file %s does not exist", clusterConfigFile)
 	}
-	clusterConfig, err := v1alpha1.GetAndValidateClusterConfig(clusterConfigFile)
+
+	clusterConfig, err := v1alpha1.GetClusterConfig(clusterConfigFile)
 	if err != nil {
 		return nil, fmt.Errorf("the cluster config file provided is invalid: %v", err)
 	}
