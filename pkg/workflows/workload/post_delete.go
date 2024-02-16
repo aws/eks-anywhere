a590acd3a8e23db87b18eccd55f491a7c0898130
Tanvir Tatla <tatlat@amazon.com>
2024-02-16 07:11:15 +0000

    Add delete mgmt workflow (#7502)

--- pkg/workflows/workload/post_delete.go
+++ pkg/workflows/workload/post_delete.go
@@ -14,7 +14,12 @@ type (
 )
 
 func (s *postDeleteWorkload) Run(ctx context.Context, commandContext *task.CommandContext) task.Task {
-	commandContext.Writer.CleanUp()
+	logger.Info("Clean up Git Repo")
+	err := commandContext.GitOpsManager.CleanupGitRepo(ctx, commandContext.ClusterSpec)
+	if err != nil {
+		commandContext.SetError(err)
+		return &workflows.CollectDiagnosticsTask{}
+	}
 
 	if commandContext.OriginalError != nil {
 		collector := &workflows.CollectMgmtClusterDiagnosticsTask{}
@@ -35,28 +40,5 @@ func (s *postDeleteWorkload) Restore(ctx context.Context, commandContext *task.C
 }
 
 func (s *postDeleteWorkload) Checkpoint() *task.CompletedTask {
-	return nil
-}
-
-func (s *cleanupGitRepo) Run(ctx context.Context, commandContext *task.CommandContext) task.Task {
-	logger.Info("Clean up Git Repo")
-	err := commandContext.GitOpsManager.CleanupGitRepo(ctx, commandContext.ClusterSpec)
-	if err != nil {
-		commandContext.SetError(err)
-		return &workflows.CollectDiagnosticsTask{}
-	}
-
-	return &postDeleteWorkload{}
-}
-
-func (s *cleanupGitRepo) Name() string {
-	return "clean-up-git-repo"
-}
-
-func (s *cleanupGitRepo) Restore(ctx context.Context, commandContext *task.CommandContext, completedTask *task.CompletedTask) (task.Task, error) {
-	return nil, nil
-}
-
-func (s *cleanupGitRepo) Checkpoint() *task.CompletedTask {
 	return nil
 }
