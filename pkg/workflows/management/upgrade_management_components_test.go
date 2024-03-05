63318d8d5820d33503d46b01e0232654811a1b50
Cavaughn Browne <cxbrowne@amazon.com>
2024-03-05 23:39:27 +0000

    clear resource version to avoid optimistic locking

--- pkg/workflows/management/upgrade_management_components_test.go
+++ pkg/workflows/management/upgrade_management_components_test.go
@@ -70,7 +70,16 @@ func NewTestMocks(t *testing.T) *TestMocks {
 	}
 }
 
-func TestRunnerHappyPath(t *testing.T) {
+type upgradeManagementComponentsTest struct {
+	ctx               context.Context
+	runner            *UpgradeManagementComponentsWorkflow
+	mocks             *TestMocks
+	managementCluster *types.Cluster
+	currentSpec       *cluster.Spec
+	newSpec           *cluster.Spec
+}
+
+func newUpgradeManagementComponentsTest(t *testing.T) *upgradeManagementComponentsTest {
 	mocks := NewTestMocks(t)
 	runner := NewUpgradeManagementComponentsRunner(
 		mocks.clientFactory,
@@ -83,86 +92,70 @@ func TestRunnerHappyPath(t *testing.T) {
 		mocks.eksdInstaller,
 	)
 
-	clusterSpec := test.NewClusterSpec()
+	currentSpec := test.NewClusterSpec(func(s *cluster.Spec) {
+		s.Bundles = test.Bundle()
+		s.EKSARelease = test.EKSARelease()
+	})
+	newSpec := currentSpec.DeepCopy()
+
 	managementCluster := &types.Cluster{
-		Name:           clusterSpec.Cluster.Name,
-		KubeconfigFile: kubeconfig.FromClusterName(clusterSpec.Cluster.Name),
+		Name:           currentSpec.Cluster.Name,
+		KubeconfigFile: kubeconfig.FromClusterName(currentSpec.Cluster.Name),
 	}
 
-	ctx := context.Background()
-	bundles := test.Bundle()
-	eksaRelease := test.EKSARelease()
+	return &upgradeManagementComponentsTest{
+		mocks:             mocks,
+		runner:            runner,
+		managementCluster: managementCluster,
+		currentSpec:       currentSpec,
+		newSpec:           newSpec,
+	}
+}
 
-	curSpec := test.NewClusterSpec()
-	currentManagementComponents := cluster.ManagementComponentsFromBundles(bundles)
+func TestRunnerHappyPath(t *testing.T) {
+	tt := newUpgradeManagementComponentsTest(t)
+	currentManagementComponents := cluster.ManagementComponentsFromBundles(tt.currentSpec.Bundles)
+	newManagementComponents := cluster.ManagementComponentsFromBundles(tt.newSpec.Bundles)
 
-	newSpec := test.NewClusterSpec(func(s *cluster.Spec) {
-		s.EKSARelease = eksaRelease
-	})
-
-	newManagementComponents := cluster.ManagementComponentsFromBundles(newSpec.Bundles)
-
-	client := test.NewFakeKubeClient(curSpec.Cluster, eksaRelease, bundles)
-
-	mocks.clusterManager.EXPECT().GetCurrentClusterSpec(ctx, gomock.Any(), managementCluster.Name).Return(curSpec, nil)
+	client := test.NewFakeKubeClient(tt.currentSpec.Cluster, tt.currentSpec.EKSARelease, tt.currentSpec.Bundles)
+	tt.mocks.clusterManager.EXPECT().GetCurrentClusterSpec(tt.ctx, gomock.Any(), tt.managementCluster.Name).Return(tt.currentSpec, nil)
 	gomock.InOrder(
-		mocks.validator.EXPECT().PreflightValidations(ctx).Return(nil),
-		mocks.provider.EXPECT().Name(),
-		mocks.provider.EXPECT().SetupAndValidateUpgradeManagementComponents(ctx, newSpec),
-		mocks.provider.EXPECT().PreCoreComponentsUpgrade(gomock.Any(), gomock.Any(), newManagementComponents, gomock.Any()),
-		mocks.clientFactory.EXPECT().BuildClientFromKubeconfig(managementCluster.KubeconfigFile).Return(client, nil),
-		mocks.capiManager.EXPECT().Upgrade(ctx, managementCluster, mocks.provider, currentManagementComponents, newManagementComponents, newSpec).Return(capiChangeDiff, nil),
-		mocks.gitOpsManager.EXPECT().Install(ctx, managementCluster, newManagementComponents, curSpec, newSpec).Return(nil),
-		mocks.gitOpsManager.EXPECT().Upgrade(ctx, managementCluster, currentManagementComponents, newManagementComponents, curSpec, newSpec).Return(fluxChangeDiff, nil),
-		mocks.clusterManager.EXPECT().Upgrade(ctx, managementCluster, currentManagementComponents, newManagementComponents, newSpec).Return(eksaChangeDiff, nil),
-		mocks.eksdUpgrader.EXPECT().Upgrade(ctx, managementCluster, curSpec, newSpec).Return(nil),
-		mocks.clusterManager.EXPECT().ApplyBundles(
-			ctx, newSpec, managementCluster,
+		tt.mocks.validator.EXPECT().PreflightValidations(tt.ctx).Return(nil),
+		tt.mocks.provider.EXPECT().Name(),
+		tt.mocks.provider.EXPECT().SetupAndValidateUpgradeManagementComponents(tt.ctx, tt.newSpec),
+		tt.mocks.provider.EXPECT().PreCoreComponentsUpgrade(gomock.Any(), gomock.Any(), newManagementComponents, gomock.Any()),
+		tt.mocks.clientFactory.EXPECT().BuildClientFromKubeconfig(tt.managementCluster.KubeconfigFile).Return(client, nil),
+		tt.mocks.capiManager.EXPECT().Upgrade(tt.ctx, tt.managementCluster, tt.mocks.provider, currentManagementComponents, newManagementComponents, tt.newSpec).Return(capiChangeDiff, nil),
+		tt.mocks.gitOpsManager.EXPECT().Install(tt.ctx, tt.managementCluster, newManagementComponents, tt.currentSpec, tt.newSpec).Return(nil),
+		tt.mocks.gitOpsManager.EXPECT().Upgrade(tt.ctx, tt.managementCluster, currentManagementComponents, newManagementComponents, tt.currentSpec, tt.newSpec).Return(fluxChangeDiff, nil),
+		tt.mocks.clusterManager.EXPECT().Upgrade(tt.ctx, tt.managementCluster, currentManagementComponents, newManagementComponents, tt.newSpec).Return(eksaChangeDiff, nil),
+		tt.mocks.eksdUpgrader.EXPECT().Upgrade(tt.ctx, tt.managementCluster, tt.currentSpec, tt.newSpec).Return(nil),
+		tt.mocks.clusterManager.EXPECT().ApplyBundles(
+			tt.ctx, tt.newSpec, tt.managementCluster,
 		).Return(nil),
-		mocks.clusterManager.EXPECT().ApplyReleases(
-			ctx, newSpec, managementCluster,
+		tt.mocks.clusterManager.EXPECT().ApplyReleases(
+			tt.ctx, tt.newSpec, tt.managementCluster,
 		).Return(nil),
-		mocks.eksdInstaller.EXPECT().InstallEksdManifest(
-			ctx, newSpec, managementCluster,
+		tt.mocks.eksdInstaller.EXPECT().InstallEksdManifest(
+			tt.ctx, tt.newSpec, tt.managementCluster,
 		).Return(nil),
 	)
 
-	err := runner.Run(ctx, newSpec, managementCluster, mocks.validator)
+	err := tt.runner.Run(tt.ctx, tt.newSpec, tt.managementCluster, tt.mocks.validator)
 	if err != nil {
 		t.Fatalf("UpgradeManagementComponents.Run() err = %v, want err = nil", err)
 	}
 
 	g := NewWithT(t)
-	g.Expect(newSpec.Cluster.Annotations).To(Equal(managementComponentsVersionAnnotation))
+	g.Expect(tt.newSpec.Cluster.Annotations).To(Equal(managementComponentsVersionAnnotation))
 }
 
 func TestRunnerStopsWhenValidationFailed(t *testing.T) {
-	mocks := NewTestMocks(t)
-	runner := NewUpgradeManagementComponentsRunner(
-		mocks.clientFactory,
-		mocks.provider,
-		mocks.capiManager,
-		mocks.clusterManager,
-		mocks.gitOpsManager,
-		mocks.writer,
-		mocks.eksdUpgrader,
-		mocks.eksdInstaller,
-	)
-
-	clusterSpec := test.NewClusterSpec()
-	managementCluster := &types.Cluster{
-		Name:           clusterSpec.Cluster.Name,
-		KubeconfigFile: kubeconfig.FromClusterName(clusterSpec.Cluster.Name),
-	}
-
-	ctx := context.Background()
-	curSpec := test.NewClusterSpec()
-	newSpec := test.NewClusterSpec()
-
-	mocks.provider.EXPECT().Name()
-	mocks.provider.EXPECT().SetupAndValidateUpgradeManagementComponents(ctx, newSpec)
-	mocks.clusterManager.EXPECT().GetCurrentClusterSpec(ctx, gomock.Any(), managementCluster.Name).Return(curSpec, nil)
-	mocks.validator.EXPECT().PreflightValidations(ctx).Return(
+	tt := newUpgradeManagementComponentsTest(t)
+	tt.mocks.provider.EXPECT().Name()
+	tt.mocks.provider.EXPECT().SetupAndValidateUpgradeManagementComponents(tt.ctx, tt.newSpec)
+	tt.mocks.clusterManager.EXPECT().GetCurrentClusterSpec(tt.ctx, gomock.Any(), tt.managementCluster.Name).Return(tt.currentSpec, nil)
+	tt.mocks.validator.EXPECT().PreflightValidations(tt.ctx).Return(
 		[]validations.Validation{
 			func() *validations.ValidationResult {
 				return &validations.ValidationResult{
@@ -171,8 +164,8 @@ func TestRunnerStopsWhenValidationFailed(t *testing.T) {
 			},
 		})
 
-	mocks.writer.EXPECT().Write(fmt.Sprintf("%s-checkpoint.yaml", newSpec.Cluster.Name), gomock.Any())
-	err := runner.Run(ctx, newSpec, managementCluster, mocks.validator)
+	tt.mocks.writer.EXPECT().Write(fmt.Sprintf("%s-checkpoint.yaml", tt.newSpec.Cluster.Name), gomock.Any())
+	err := tt.runner.Run(tt.ctx, tt.newSpec, tt.managementCluster, tt.mocks.validator)
 	if err == nil {
 		t.Fatalf("UpgradeManagementComponents.Run() err == nil, want err != nil")
 	}
