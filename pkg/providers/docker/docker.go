12d22b16d6026af28be5aa02c32b2fdf6fa04f92
Vivek Koppuru <koppv@amazon.com>
2024-04-19 16:03:07 -0700

    Add check for nil eksaversion when setting etcd url

--- pkg/providers/docker/docker.go
+++ pkg/providers/docker/docker.go
@@ -328,7 +328,7 @@ func buildTemplateMapCP(clusterSpec *cluster.Spec) (map[string]interface{}, erro
 	if clusterSpec.Cluster.Spec.ExternalEtcdConfiguration != nil {
 		values["externalEtcd"] = true
 		values["externalEtcdReplicas"] = clusterSpec.Cluster.Spec.ExternalEtcdConfiguration.Count
-		etcdURL, _ := common.GetExternalEtcdReleaseURL(string(*clusterSpec.Cluster.Spec.EksaVersion), versionsBundle)
+		etcdURL, _ := common.GetExternalEtcdReleaseURL(clusterSpec.Cluster.Spec.EksaVersion, versionsBundle)
 		if etcdURL != "" {
 			values["externalEtcdReleaseUrl"] = etcdURL
 		}
