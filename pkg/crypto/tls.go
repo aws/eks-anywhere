9f9a74a2847b77a4ba098f5946b185ebecf9e9ff
Mitali Paygude <mitalipaygude@gmail.com>
2024-06-10 17:55:41 -0700

    Kubelet Configuration for cp and wn Tinkerbell (#8217)

--- pkg/crypto/tls.go
+++ pkg/crypto/tls.go
@@ -7,10 +7,10 @@ import (
 // This is what we currently support as the default. In the future,
 // we can make this customizable and return a wider range of
 // supported names.
-func secureCipherSuiteNames() []string {
+func SecureCipherSuiteNames() []string {
 	return []string{"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"}
 }
 
 func SecureCipherSuitesString() string {
-	return strings.Join(secureCipherSuiteNames(), ",")
+	return strings.Join(SecureCipherSuiteNames(), ",")
 }
