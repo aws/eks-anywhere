c3097b02a377046948912b94ce90d211d5ecf0b3
Abhay Krishna <arnchlm@amazon.com>
2024-09-12 09:17:33 -0700

    Add Tinkerbell E2E test for Ubuntu 22.04 generic kernel image (#8750)

--- test/framework/docker.go
+++ test/framework/docker.go
@@ -85,7 +85,7 @@ func (d *Docker) ClusterStateValidations() []clusterf.StateValidation {
 }
 
 // WithKubeVersionAndOS returns a cluster config filler that sets the cluster kube version.
-func (d *Docker) WithKubeVersionAndOS(kubeVersion anywherev1.KubernetesVersion, _ OS, _ *releasev1.EksARelease, _ ...bool) api.ClusterConfigFiller {
+func (d *Docker) WithKubeVersionAndOS(kubeVersion anywherev1.KubernetesVersion, _ OS, _ *releasev1.EksARelease, _ ...string) api.ClusterConfigFiller {
 	return api.JoinClusterConfigFillers(
 		api.ClusterToConfigFiller(api.WithKubernetesVersion(kubeVersion)),
 	)
