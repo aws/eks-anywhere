f613dafa3f1c4b2f59fdcd29db1420580227a86a
Saurabh Parekh <sjparekh@amazon.com>
2024-09-30 15:59:51 -0700

    Customize tinkerbell load balancer interface (#8805)
    * Customize tinkerbell load balancer interface
    
    * Add mini design doc for configuring tinkerbell stack LB interface
    
    * Add loadBalancerInterface as an optional configuration to the baremetal configuration docs

--- pkg/providers/tinkerbell/tinkerbell_test.go
+++ pkg/providers/tinkerbell/tinkerbell_test.go
@@ -605,6 +605,7 @@ func TestPreCAPIInstallOnBootstrapSuccess(t *testing.T) {
 		gomock.Any(),
 		gomock.Any(),
 		gomock.Any(),
+		gomock.Any(),
 	)
 
 	err := provider.PreCAPIInstallOnBootstrap(ctx, cluster, clusterSpec)
@@ -646,6 +647,7 @@ func TestPostWorkloadInitSuccess(t *testing.T) {
 		testIP,
 		"test.kubeconfig",
 		"",
+		gomock.Any(),
 		gomock.Any(),
 		gomock.Any(),
 		gomock.Any(),
