a0a9027d3eac9973963d6936cf3ebaf5f31a6e06
jhaanvi5 <148375165+jhaanvi5@users.noreply.github.com>
2024-10-17 10:21:14 -0700

    Skip generating AWS IAM Kubeconfig on cluster upgrade (#8851)
    * Skip generating AWS IAM kubeconfig on cluster upgrade
    
    * Added unit test to validate GenerateAWSIAMKubeconfig is not called
    
    * fix: update method name
    
    Co-authored-by: Vivek Koppuru <vivek.koppuru@gmail.com>
    
    * fix: update method name
    
    ---------
    
    Co-authored-by: Vivek Koppuru <vivek.koppuru@gmail.com>

--- pkg/workflows/workload/writeclusterconfig.go
+++ pkg/workflows/workload/writeclusterconfig.go
@@ -20,7 +20,8 @@ func (s *writeClusterConfig) Run(ctx context.Context, commandContext *task.Comma
 
 	}
 
-	if commandContext.ClusterSpec.AWSIamConfig != nil {
+	// Generate AWS IAM kubeconfig only for cluster creation step
+	if commandContext.CurrentClusterSpec == nil && commandContext.ClusterSpec.AWSIamConfig != nil {
 		logger.Info("Generating the aws iam kubeconfig file")
 		err = commandContext.ClusterManager.GenerateAWSIAMKubeconfig(ctx, commandContext.ManagementCluster)
 		if err != nil {
