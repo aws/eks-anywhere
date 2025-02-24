9d6ac977f3acb5d6c584e5128b3a3a9e7aaa239a
Saurabh Parekh <sjparekh@amazon.com>
2025-02-23 22:34:20 -0800

    Fix docker upgrade from latest tests (#9300)

--- test/framework/cluster.go
+++ test/framework/cluster.go
@@ -494,11 +494,11 @@ func (e *ClusterE2ETest) GenerateClusterConfigForVersion(eksaVersion string, opt
 		// We will need the conditional check as long as the latest minor is 'v0.21.*'.
 		currentSemver, err := semver.New(eksaVersion)
 		if err != nil {
-			e.T.Fatal("parsing esk-a version", err)
+			e.T.Fatalf("parsing eks-a version:", err)
 		}
 		semverV022, err := semver.New(releaseV022)
 		if err != nil {
-			e.T.Fatal("parsing esk-a version", err)
+			e.T.Fatalf("parsing eks-a version:", err)
 		}
 
 		if currentSemver.Compare(semverV022) != -1 {
