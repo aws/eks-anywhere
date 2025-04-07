d13e38ebfc6489de3581864b3aeaddae673621ac
Saurabh Parekh <sjparekh@amazon.com>
2025-04-06 23:21:07 -0700

    Revert "Use source image URI for replacing packages and ecr token refresher image tags (#9511)"
    This reverts commit b88c56fdc5a5b7093b77a5de82ff18fa55e30280.

--- release/cli/pkg/bundles/package-controller.go
+++ release/cli/pkg/bundles/package-controller.go
@@ -117,12 +117,12 @@ func GetPackagesBundle(r *releasetypes.ReleaseConfig, imageDigests releasetypes.
 					if err != nil {
 						return anywherev1alpha1.PackageBundle{}, fmt.Errorf("loading digest from image digests table: %v", err)
 					}
-					if strings.HasSuffix(imageArtifact.AssetName, "eks-anywhere-packages") && r.DevRelease && Imagesha != "" && Imagetag != "" {
+					if strings.HasSuffix(imageArtifact.AssetName, "eks-anywhere-packages") && r.DevRelease && TokenSha != "" && Tokentag != "" {
 						imageDigest = Imagesha
-						imageArtifact.SourceImageURI = replaceTag(imageArtifact.SourceImageURI, Imagetag)
-					} else if strings.HasSuffix(imageArtifact.AssetName, "ecr-token-refresher") && r.DevRelease && TokenSha != "" && Tokentag != "" {
+						imageArtifact.ReleaseImageURI = replaceTag(imageArtifact.ReleaseImageURI, Imagetag)
+					} else if strings.HasSuffix(imageArtifact.AssetName, "ecr-token-refresher") && r.DevRelease && Imagesha != "" && Imagetag != "" {
 						imageDigest = TokenSha
-						imageArtifact.SourceImageURI = replaceTag(imageArtifact.SourceImageURI, Tokentag)
+						imageArtifact.ReleaseImageURI = replaceTag(imageArtifact.ReleaseImageURI, Tokentag)
 					}
 					bundleImageArtifact = anywherev1alpha1.Image{
 						Name:        imageArtifact.AssetName,
