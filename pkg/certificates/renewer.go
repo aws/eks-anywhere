302174b046cbb715fc8e3bfb1c91dd8cb4d97c31
Howard Cao <junecao@amazon.com>
2025-06-05 09:47:03 -0700

    remove comment

--- pkg/certificates/renewer.go
+++ pkg/certificates/renewer.go
@@ -33,13 +33,12 @@ const (
 	componentControlPlane = "control-plane"
 )
 
-// sshDialer is a function type for creating SSH clients
 type sshDialer func(network, addr string, config *ssh.ClientConfig) (sshClient, error)
 
 type Renewer struct {
 	backupDir  string
 	sshConfig  *ssh.ClientConfig
-	sshKeyPath string // Store SSH key path from config
+	sshKeyPath string // store SSH key path from config
 	kubeClient kubernetes.Interface
 	sshDialer  sshDialer
 }
@@ -151,19 +150,6 @@ func (r *Renewer) checkAPIServerReachability(ctx context.Context) error {
 	return fmt.Errorf("kubernetes API server is not reachable")
 }
 
-// func (r *Renewer) backupKubeadmConfig(ctx context.Context) error {
-// 	cm, err := r.kubeClient.CoreV1().ConfigMaps("kube-system").Get(ctx, "kubeadm-config", metav1.GetOptions{})
-// 	if err != nil {
-// 		return fmt.Errorf("failed to get kubeadm-config: %v", err)
-// 	}
-
-// 	backupPath := filepath.Join(r.backupDir, "kubeadm-config.yaml")
-// 	if err := os.WriteFile(backupPath, []byte(cm.Data["ClusterConfiguration"]), 0600); err != nil {
-// 		return fmt.Errorf("failed to write kubeadm config backup: %v", err)
-// 	}
-
-//		return nil
-//	}
 func (r *Renewer) backupKubeadmConfig(ctx context.Context) error {
 	cm, err := r.kubeClient.CoreV1().ConfigMaps("kube-system").Get(ctx, "kubeadm-config", metav1.GetOptions{})
 	if err != nil {
