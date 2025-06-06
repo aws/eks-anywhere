0fd1e05e6d3d4c532c9238dfb95c3481db23c8d2
howard-junec <junecao@amazon.com>
2025-06-06 10:37:53 -0700

    Configuration Structure For Certificate Renewal (Part 1 of the certificate renewal command feature) (#9780)
    * core configuration structures and validation for the certificate renewal command
    
    * update cluster_config to support ubuntu renew by cluster name
    
    * add support for rhel config
    
    * Simplify certificate renewal to use config file approach
    
    * return nil for the scope of this PR 1, renew logics are in different PR
    
    * changes to certificates.ParseConfg
    
    * use OSFamily variabl,remove config file empty check

---
+++ cmd/eksctl-anywhere/cmd/renewcertificates.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/aws/eks-anywhere/pkg/certificates"
+	"github.com/spf13/cobra"
+)
+
+type renewCertificatesOptions struct {
+	configFile string
+	component  string
+}
+
+var rc = &renewCertificatesOptions{}
+
+var renewCertificatesCmd = &cobra.Command{
+	Use:          "certificates",
+	Short:        "Renew certificates",
+	Long:         "Renew external ETCD and control plane certificates",
+	PreRunE:      bindFlagsToViper,
+	SilenceUsage: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return rc.renewCertificates(cmd)
+	},
+}
+
+func init() {
+	renewCmd.AddCommand(renewCertificatesCmd)
+	renewCertificatesCmd.Flags().StringVarP(&rc.configFile, "config", "f", "", "Config file containing node and SSH information")
+	renewCertificatesCmd.Flags().StringVarP(&rc.component, "component", "c", "", "Component to renew certificates for (etcd or control-plane). If not specified, renews both.")
+	if err := renewCertificatesCmd.MarkFlagRequired("config"); err != nil {
+		log.Fatalf("marking config as required: %s", err)
+	}
+
+}
+
+func validateComponent(component string) error {
+	if component != "" && component != "etcd" && component != "control-plane" {
+		return fmt.Errorf("invalid component %q, must be either 'etcd' or 'control-plane'", component)
+	}
+	return nil
+}
+
+func (rc *renewCertificatesOptions) renewCertificates(cmd *cobra.Command) error {
+	if err := validateComponent(rc.component); err != nil {
+		return err
+	}
+
+	_, err := certificates.ParseConfig(rc.configFile)
+	if err != nil {
+		return fmt.Errorf("parsing config file: %v", err)
+	}
+	return nil
+}
---
+++ pkg/certificates/config.go
@@ -0,0 +1,89 @@
+package certificates
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/eks-anywhere/pkg/api/v1alpha1"
+	"gopkg.in/yaml.v2"
+)
+
+// NodeConfig holds SSH configuration for a node group
+type NodeConfig struct {
+	Nodes     []string `yaml:"nodes"`
+	OS        string   `yaml:"os"`
+	SSHKey    string   `yaml:"sshKey"`
+	SSHUser   string   `yaml:"sshUser"`
+	SSHPasswd string   `yaml:"sshPasswd,omitempty"` // Optional SSH key passphrase
+}
+
+type RenewalConfig struct {
+	ClusterName  string     `yaml:"clusterName"`
+	ControlPlane NodeConfig `yaml:"controlPlane"`
+	Etcd         NodeConfig `yaml:"etcd"`
+}
+
+func ParseConfig(path string) (*RenewalConfig, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("reading config file: %v", err)
+	}
+
+	config := &RenewalConfig{}
+	if err := yaml.Unmarshal(data, config); err != nil {
+		return nil, fmt.Errorf("parsing config file: %v", err)
+	}
+
+	if err := validateConfig(config); err != nil {
+		return nil, fmt.Errorf("validating config: %v", err)
+	}
+
+	return config, nil
+}
+
+func validateConfig(config *RenewalConfig) error {
+	if config.ClusterName == "" {
+		return fmt.Errorf("cluster name is required")
+	}
+
+	if len(config.ControlPlane.Nodes) == 0 {
+		return fmt.Errorf("at least one node is required in ControlPlane configuration")
+	}
+
+	if err := validateNodeConfig(&config.ControlPlane); err != nil {
+		return fmt.Errorf("validating control plane: %v", err)
+	}
+
+	// Etcd nodes are optional (could be embedded in control plane)
+	if len(config.Etcd.Nodes) > 0 {
+		if err := validateNodeConfig(&config.Etcd); err != nil {
+			return fmt.Errorf("validating etcd: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func validateNodeConfig(config *NodeConfig) error {
+	if len(config.Nodes) == 0 {
+		return fmt.Errorf("nodes are required")
+	}
+	if config.OS == "" {
+		return fmt.Errorf("OS is required")
+	}
+	if config.OS != string(v1alpha1.Ubuntu) && config.OS != string(v1alpha1.RedHat) && config.OS != string(v1alpha1.Bottlerocket) {
+		return fmt.Errorf("unsupported OS %q", config.OS)
+	}
+	if config.SSHKey == "" {
+		return fmt.Errorf("SSH key is required")
+	}
+	if config.SSHUser == "" {
+		return fmt.Errorf("SSH user is required")
+	}
+
+	if _, err := os.Stat(config.SSHKey); err != nil {
+		return fmt.Errorf("retrieving SSH key file information: %v", err)
+	}
+
+	return nil
+}
