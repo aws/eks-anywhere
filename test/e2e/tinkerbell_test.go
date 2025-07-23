68a24990ea071571e0445f06e253122bdb0efc41
Subbu Vakati <subbusrv@amazon.com>
2025-07-23 09:54:35 -0700

    fix hardcoded osimage in the new e2e test

--- test/e2e/tinkerbell_test.go
+++ test/e2e/tinkerbell_test.go
@@ -2127,7 +2127,9 @@ func TestTinkerbellCustomTemplateRefSimpleFlow(t *testing.T) {
 	)
 
 	// Get the custom template config
-	customTemplateConfig, err := framework.GetCustomTinkerbellConfig(provider.GetTinkerbellLBIP())
+	customTemplateConfig, err := framework.GetCustomTinkerbellConfig(
+		test.ClusterConfig.TinkerbellDatacenter.Spec.TinkerbellIP,
+		test.ClusterConfig.TinkerbellDatacenter.Spec.OSImageURL)
 	if err != nil {
 		t.Fatalf("Failed to get custom template config: %v", err)
 	}
