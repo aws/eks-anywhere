4bbdc5a8d71e2113678ccbed09999ac4c2215064
Amelia Lu <63176721+peirulu@users.noreply.github.com>
2025-10-09 12:24:06 -0700

    add second network interface for vsphere

--- pkg/providers/vsphere/template.go
+++ pkg/providers/vsphere/template.go
@@ -14,6 +14,7 @@ import (
 	"github.com/aws/eks-anywhere/pkg/config"
 	"github.com/aws/eks-anywhere/pkg/constants"
 	"github.com/aws/eks-anywhere/pkg/crypto"
+	"github.com/aws/eks-anywhere/pkg/logger"
 	"github.com/aws/eks-anywhere/pkg/providers"
 	"github.com/aws/eks-anywhere/pkg/providers/common"
 	"github.com/aws/eks-anywhere/pkg/registrymirror"
@@ -134,6 +135,8 @@ func (vs *VsphereTemplateBuilder) GenerateCAPISpecWorkers(
 			return nil, err
 		}
 		workerSpecs = append(workerSpecs, bytes)
+		logger.V(6).Info("worker spec is :", string(bytes))
+
 	}
 
 	return templater.AppendYamlResources(workerSpecs...), nil
@@ -464,6 +467,7 @@ func buildTemplateMapMD(
 		"workerVsphereDatastore":         workerNodeGroupMachineSpec.Datastore,
 		"workerVsphereFolder":            workerNodeGroupMachineSpec.Folder,
 		"vsphereNetwork":                 datacenterSpec.Network,
+		"vsphereMultiNetworks":           workerNodeGroupMachineSpec.Networks,
 		"workerVsphereResourcePool":      workerNodeGroupMachineSpec.ResourcePool,
 		"vsphereServer":                  datacenterSpec.Server,
 		"workerVsphereStoragePolicyName": workerNodeGroupMachineSpec.StoragePolicyName,
