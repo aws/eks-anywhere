be1e0ee84e0da9a1e6aaf3aa9d9f19e3690b3a01
Shizhao Liu <43278392+2ez4szliu@users.noreply.github.com>
2025-10-31 19:55:14 -0700

    Skip package controller install for non-package tests (#10275) (#10276)

--- test/e2e/taints.go
+++ test/e2e/taints.go
@@ -4,10 +4,7 @@
 package e2e
 
 import (
-	"time"
-
 	"github.com/aws/eks-anywhere/pkg/api/v1alpha1"
-	"github.com/aws/eks-anywhere/pkg/constants"
 	"github.com/aws/eks-anywhere/test/framework"
 )
 
@@ -16,13 +13,6 @@ func runTaintsUpgradeFlow(test *framework.ClusterE2ETest, updateVersion v1alpha1
 	test.CreateCluster()
 	test.ValidateWorkerNodes(framework.ValidateWorkerNodeTaints)
 	test.ValidateControlPlaneNodes(framework.ValidateControlPlaneTaints)
-	// Add 1-minute wait for vSphere upgrade tests
-	// where it fails during upgrade preflight validation
-	// when packages controller installs credentials provider package on the node
-	if test.Provider.Name() == constants.VSphereProviderName {
-		test.T.Log("Waiting 1 minute before starting vSphere upgrade...")
-		time.Sleep(1 * time.Minute)
-	}
 	test.UpgradeClusterWithNewConfig(clusterOpts)
 	test.ValidateCluster(updateVersion)
 	test.ValidateWorkerNodes(framework.ValidateWorkerNodeTaints)
