66ca7e427a2b0c25a834ed4ce265770ec50d3667
Subbarao Vakati <52436067+snarkychef@users.noreply.github.com>
2026-02-11 17:29:33 -0800

    Disable cgroupv1 failure for docker (#10538)

--- pkg/providers/docker/workers_test.go
+++ pkg/providers/docker/workers_test.go
@@ -146,6 +146,7 @@ func TestWorkersSpecUpgradeClusterRemoveLabels(t *testing.T) {
 		"cgroup-driver":     "cgroupfs",
 		"node-labels":       "foo=bar",
 		"eviction-hard":     "nodefs.available<0%,nodefs.inodesFree<0%,imagefs.available<0%",
+		"fail-cgroupv1":     "false",
 	}
 
 	currentGroup1 := clusterapi.WorkerGroup[*dockerv1.DockerMachineTemplate]{
@@ -186,6 +187,7 @@ func TestWorkersSpecUpgradeClusterRemoveLabels(t *testing.T) {
 		"tls-cipher-suites": "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
 		"cgroup-driver":     "cgroupfs",
 		"eviction-hard":     "nodefs.available<0%,nodefs.inodesFree<0%,imagefs.available<0%",
+		"fail-cgroupv1":     "false",
 	}
 	expectedGroup1.KubeadmConfigTemplate.Name = "test-md-0-2"
 	expectedGroup1.MachineDeployment.Spec.Template.Spec.Bootstrap.ConfigRef.Name = "test-md-0-2"
@@ -399,6 +401,7 @@ func kubeadmConfigTemplate(opts ...func(*bootstrapv1.KubeadmConfigTemplate)) *bo
 								"tls-cipher-suites": "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
 								"cgroup-driver":     "cgroupfs",
 								"eviction-hard":     "nodefs.available<0%,nodefs.inodesFree<0%,imagefs.available<0%",
+								"fail-cgroupv1":     "false",
 							},
 							Taints: []corev1.Taint{},
 						},
